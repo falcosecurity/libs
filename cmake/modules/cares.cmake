85fe13009bd65c514e2aaa904b00e60160fb90f3
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2021-03-17 20:35:31 +0100

    Move c-ares to a cmake module
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

---
+++ cmake/modules/cares.cmake
@@ -0,0 +1,32 @@
+option(USE_BUNDLED_CARES "Enable building of the bundled c-ares" ${USE_BUNDLED_DEPS})
+
+if(CARES_INCLUDE)
+	# we already have c-ares
+elseif(NOT USE_BUNDLED_CARES)
+	find_path(CARES_INCLUDE NAMES cares/ares.h ares.h)
+	find_library(CARES_LIB NAMES cares)
+	if(CARES_INCLUDE AND CARES_LIB)
+		message(STATUS "Found c-ares: include: ${CARES_INCLUDE}, lib: ${CARES_LIB}")
+	else()
+		message(FATAL_ERROR "Couldn't find system c-ares")
+	endif()
+else()
+	set(CARES_SRC "${PROJECT_BINARY_DIR}/c-ares-prefix/src/c-ares")
+	set(CARES_INCLUDE "${CARES_SRC}/target/include")
+	set(CARES_LIB "${CARES_SRC}/target/lib/libcares.a")
+
+	if(NOT TARGET c-ares)
+		message(STATUS "Using bundled c-ares in '${CARES_SRC}'")
+		ExternalProject_Add(c-ares
+			PREFIX "${PROJECT_BINARY_DIR}/c-ares-prefix"
+			URL "http://download.sysdig.com/dependencies/c-ares-1.13.0.tar.gz"
+			URL_MD5 "d2e010b43537794d8bedfb562ae6bba2"
+			CONFIGURE_COMMAND ./configure --prefix=${CARES_SRC}/target
+			BUILD_COMMAND ${CMD_MAKE}
+			BUILD_IN_SOURCE 1
+			BUILD_BYPRODUCTS ${CARES_INCLUDE} ${CARES_LIB}
+			INSTALL_COMMAND ${CMD_MAKE} install)
+	endif()
+endif()
+
+include_directories("${CARES_INCLUDE}")
