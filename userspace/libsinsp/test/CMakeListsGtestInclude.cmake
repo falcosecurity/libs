6eba10d0222a851c79e2d0069669e511a2fb37e4
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2021-03-18 14:01:42 +0100

    Move gtest to a cmake module
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

--- userspace/libsinsp/test/CMakeListsGtestInclude.cmake
+++ userspace/libsinsp/test/CMakeListsGtestInclude.cmake
@@ -24,8 +24,8 @@ include(ExternalProject)
 ExternalProject_Add(googletest
   GIT_REPOSITORY    https://github.com/google/googletest.git
   GIT_TAG           master
-  SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/googletest-src"
-  BINARY_DIR        "${CMAKE_CURRENT_BINARY_DIR}/googletest-build"
+  SOURCE_DIR        "${PROJECT_BINARY_DIR}/googletest-src"
+  BINARY_DIR        "${PROJECT_BINARY_DIR}/googletest-build"
   CONFIGURE_COMMAND ""
   BUILD_COMMAND     ""
   INSTALL_COMMAND   ""
