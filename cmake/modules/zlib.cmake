7b12c7faba32f53de78607027cfd964bc0f8d29d
Leonardo Grasso <me@leonardograsso.com>
2021-05-05 15:47:15 +0200

    chore(cmake/modules): configure zlib prefix
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- cmake/modules/zlib.cmake
+++ cmake/modules/zlib.cmake
@@ -24,7 +24,7 @@ else()
 				PREFIX "${PROJECT_BINARY_DIR}/zlib-prefix"
 				URL "https://github.com/madler/zlib/archive/v1.2.11.tar.gz"
 				URL_HASH "SHA256=629380c90a77b964d896ed37163f5c3a34f6e6d897311f1df2a7016355c45eff"
-				CONFIGURE_COMMAND "./configure"
+				CONFIGURE_COMMAND ./configure --prefix=${ZLIB_SRC}
 				BUILD_COMMAND ${CMD_MAKE}
 				BUILD_IN_SOURCE 1
 				BUILD_BYPRODUCTS ${ZLIB_LIB}
