50c3398f8b14fb509747b4c23c00a2fa74a77722
Leonardo Grasso <me@leonardograsso.com>
2021-05-05 15:46:32 +0200

    chore(cmake/modules): CARES_INSTALL_DIR
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- cmake/modules/cares.cmake
+++ cmake/modules/cares.cmake
@@ -14,6 +14,7 @@ else()
 	set(CARES_SRC "${PROJECT_BINARY_DIR}/c-ares-prefix/src/c-ares")
 	set(CARES_INCLUDE "${CARES_SRC}/target/include")
 	set(CARES_LIB "${CARES_SRC}/target/lib/libcares.a")
+	set(CARES_INSTALL_DIR "${CARES_SRC}/target")
 
 	if(NOT TARGET c-ares)
 		message(STATUS "Using bundled c-ares in '${CARES_SRC}'")
@@ -21,7 +22,7 @@ else()
 			PREFIX "${PROJECT_BINARY_DIR}/c-ares-prefix"
 			URL "https://c-ares.haxx.se/download/c-ares-1.15.0.tar.gz"
 			URL_HASH "SHA256=6cdb97871f2930530c97deb7cf5c8fa4be5a0b02c7cea6e7c7667672a39d6852"
-			CONFIGURE_COMMAND ./configure --prefix=${CARES_SRC}/target
+			CONFIGURE_COMMAND ./configure --prefix=${CARES_INSTALL_DIR}
 			BUILD_COMMAND ${CMD_MAKE}
 			BUILD_IN_SOURCE 1
 			BUILD_BYPRODUCTS ${CARES_INCLUDE} ${CARES_LIB}
