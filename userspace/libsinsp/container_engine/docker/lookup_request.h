fdfd16d458cc0c5b9c76acab2b4c29b18830f0dc
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2021-07-16 15:29:47 +0200

    Pass docker socket to the async engine separately for each request
    Rootless podman containers have multiple sockets (one per user
    account), and we don't really want to spawn a separate thread
    for each user.
    
    This means that we can no longer use a single CURL handle but need
    to create a new one for each individual request.
    
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

--- userspace/libsinsp/container_engine/docker/lookup_request.h
+++ userspace/libsinsp/container_engine/docker/lookup_request.h
@@ -10,8 +10,10 @@ struct docker_lookup_request
 	{}
 
 	docker_lookup_request(const std::string& container_id_value,
+			      const std::string& docker_socket_value,
 			      bool rw_size_value) :
 		container_id(container_id_value),
+		docker_socket(docker_socket_value),
 		request_rw_size(rw_size_value)
 	{}
 
@@ -22,16 +24,23 @@ struct docker_lookup_request
 			return container_id < rhs.container_id;
 		}
 
+		if(docker_socket != rhs.docker_socket)
+		{
+			return docker_socket < rhs.docker_socket;
+		}
+
 		return request_rw_size < rhs.request_rw_size;
 	}
 
 	bool operator==(const docker_lookup_request& rhs) const
 	{
 		return container_id == rhs.container_id &&
+		       docker_socket == rhs.docker_socket &&
 		       request_rw_size == rhs.request_rw_size;
 	}
 
 	std::string container_id;
+	std::string docker_socket;
 	bool request_rw_size;
 };
 
