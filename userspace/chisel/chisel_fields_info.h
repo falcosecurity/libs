257dd291c6581615e06798b6e5501e31bbfef736
Leonardo Grasso <me@leonardograsso.com>
2021-07-22 10:58:04 +0200

    chore(userspace): clean up comments
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- userspace/chisel/chisel_fields_info.h
+++ userspace/chisel/chisel_fields_info.h
@@ -1,7 +1,6 @@
 /*
 Copyright (C) 2021 The Falco Authors.
 
-This file is part of sysdig.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
