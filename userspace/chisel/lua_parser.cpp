b5308ce965006bfe1b79d55e4838b9357c9ca382
Mark Stemm <mark.stemm@gmail.com>
2021-08-25 14:54:41 -0700

    Update lua parser to be short-lived for a single filter
    Clean up the interface for lua_parser/lua_parser_api so it doesn't
    rely on a single object.
    
    Context--the lua_parser object simply holds some intermediate
    state (e.g. nesting level) and builds up a gen_filter as the lua side
    traverses the ast of a filter expression. The lua_parser_api class
    just has static methods that are registered into lua.
    
    lua_parser used to be a single object that was reset for each
    filter. Now, it's an object that is created as a single filter is
    parsed and deleted afterward. The callbacks always pass the lua_parser
    object as a first argument and the state/filter in the object is
    updated.
    
    Also, registering the lua callbacks is now done via a static method
    instead of in the constructor.
    
    Signed-off-by: Mark Stemm <mark.stemm@gmail.com>

--- userspace/chisel/lua_parser.cpp
+++ userspace/chisel/lua_parser.cpp
@@ -16,9 +16,8 @@ limitations under the License.
 */
 #include <iostream>
 #include <fstream>
-#include "sinsp.h"
-#include "filter.h"
-#include "sinsp_int.h"
+#include <memory>
+#include "gen_filter.h"
 
 #include "lua_parser.h"
 #include "lua_parser_api.h"
@@ -39,50 +38,29 @@ const static struct luaL_reg ll_filter [] =
 	{NULL,NULL}
 };
 
-lua_parser::lua_parser(gen_event_filter_factory &factory, lua_State *ls, const char *lua_library_name)
-	: m_factory(factory)
+lua_parser::lua_parser(std::shared_ptr<gen_event_filter_factory> factory)
+	: m_factory(factory), m_filter(m_factory->new_filter()),
+	  m_last_boolop(BO_NONE), m_have_rel_expr(false),
+	  m_nest_level(0)
 {
-	m_filter = NULL;
-
-	m_ls = ls;
-	reset();
-
-	// Register our c++ defined functions
-	luaL_openlib(m_ls, lua_library_name, ll_filter, 0);
 }
 
-void lua_parser::reset()
-{
-	m_have_rel_expr = false;
-	m_last_boolop = BO_NONE;
-	m_nest_level = 0;
-
-	m_filter = m_factory.new_filter();
-}
-
-gen_event_filter* lua_parser::get_filter(bool reset_filter)
-{
-	if (m_nest_level != 0)
-	{
-		throw sinsp_exception("Error in configured filter: unbalanced nesting");
-	}
-
-	gen_event_filter *ret = m_filter;
-
-	if (reset_filter)
-	{
-		reset();
-	}
-
-	return ret;
-}
 lua_parser::~lua_parser()
 {
-	// The lua state is not considered owned by this object, so
-	// not freeing it.
-
-	delete m_filter;
-	m_filter = NULL;
 }
 
+void lua_parser::register_callbacks(lua_State *ls, const char *lua_library_name)
+{
+	// Register our c++ defined functions
+	luaL_openlib(ls, lua_library_name, ll_filter, 0);
+}
 
+std::shared_ptr<gen_event_filter> lua_parser::filter()
+{
+	return m_filter;
+}
+
+std::shared_ptr<gen_event_filter_factory> lua_parser::factory()
+{
+	return m_factory;
+}
