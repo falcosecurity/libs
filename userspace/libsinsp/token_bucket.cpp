adf8f933d7c2059204ef5f884a01052fc5d70976
Mark Stemm <mark.stemm@gmail.com>
2021-10-15 06:45:59 -0700

    Finish moving token bucket impl from falco to libs
    It took a while, but we remembered to finish moving the token_bucket
    from falco engine to libs. There were 2 copies for a while.
    
    This brings over one change from the falco copy--to have an optional
    timer function.
    
    Signed-off-by: Mark Stemm <mark.stemm@gmail.com>

--- userspace/libsinsp/token_bucket.cpp
+++ userspace/libsinsp/token_bucket.cpp
@@ -12,17 +12,23 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-
 */
 
 #include <cstddef>
+#include <functional>
+#include <sys/time.h>
 
-#include "sinsp.h"
-#include "utils.h"
 #include "token_bucket.h"
+#include "utils.h"
 
-token_bucket::token_bucket()
+token_bucket::token_bucket():
+	token_bucket(sinsp_utils::get_current_time_ns)
 {
+}
+
+token_bucket::token_bucket(std::function<uint64_t()> timer)
+{
+	m_timer = timer;
 	init(1, 1);
 }
 
@@ -35,20 +41,12 @@ void token_bucket::init(double rate, double max_tokens, uint64_t now)
 	m_rate = rate;
 	m_max_tokens = max_tokens;
 	m_tokens = max_tokens;
-
-	if(now == 0)
-	{
-		now = sinsp_utils::get_current_time_ns();
-	}
-
-	m_last_seen = now;
+	m_last_seen = now == 0 ? m_timer() : now;
 }
 
 bool token_bucket::claim()
 {
-	uint64_t now = sinsp_utils::get_current_time_ns();
-
-	return claim(1, now);
+	return claim(1, m_timer());
 }
 
 bool token_bucket::claim(double tokens, uint64_t now)
