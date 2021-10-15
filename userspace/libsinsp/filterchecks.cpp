95f5ec22157aa8e49ca8b15f5e3045bc001e2ca9
Mark Stemm <mark.stemm@gmail.com>
2021-10-15 12:24:32 -0700

    Split sinsp_evt_filter_check_event in two, use half in plugins
    Split sinsp_evt_filter_check_event in two. (New)
    sinsp_evt_filter_check_gen_event has fields for event number and
    time. sinsp_evt_filter_check_event has fields for everything else.
    
    Add sinsp_evt_filter_check_gen_event to the filtercheck list for
    plugins so filters/conditions can have fields that work on time/event
    number.
    
    Signed-off-by: Mark Stemm <mark.stemm@gmail.com>

--- userspace/libsinsp/filterchecks.cpp
+++ userspace/libsinsp/filterchecks.cpp
@@ -2786,7 +2786,7 @@ bool sinsp_filter_check_thread::compare(sinsp_evt *evt)
 ///////////////////////////////////////////////////////////////////////////////
 // sinsp_filter_check_event implementation
 ///////////////////////////////////////////////////////////////////////////////
-const filtercheck_field_info sinsp_filter_check_event_fields[] =
+const filtercheck_field_info sinsp_filter_check_gen_event_fields[] =
 {
 	{PT_UINT64, EPF_NONE, PF_ID, "evt.num", "event number."},
 	{PT_CHARBUF, EPF_NONE, PF_NA, "evt.time", "event timestamp as a time string that includes the nanosecond part."},
@@ -2800,6 +2800,108 @@ const filtercheck_field_info sinsp_filter_check_event_fields[] =
 	{PT_RELTIME, EPF_NONE, PF_10_PADDED_DEC, "evt.reltime", "number of nanoseconds from the beginning of the capture."},
 	{PT_RELTIME, EPF_NONE, PF_DEC, "evt.reltime.s", "number of seconds from the beginning of the capture."},
 	{PT_RELTIME, EPF_NONE, PF_10_PADDED_DEC, "evt.reltime.ns", "fractional part (in ns) of the time from the beginning of the capture."},
+};
+
+sinsp_filter_check_gen_event::sinsp_filter_check_gen_event()
+{
+	m_info.m_name = "evt";
+	m_info.m_fields = sinsp_filter_check_gen_event_fields;
+	m_info.m_nfields = sizeof(sinsp_filter_check_gen_event_fields) / sizeof(sinsp_filter_check_gen_event_fields[0]);
+	m_u64val = 0;
+}
+
+sinsp_filter_check_gen_event::~sinsp_filter_check_gen_event()
+{
+}
+
+sinsp_filter_check* sinsp_filter_check_gen_event::allocate_new()
+{
+	return (sinsp_filter_check*) new sinsp_filter_check_gen_event();
+}
+
+Json::Value sinsp_filter_check_gen_event::extract_as_js(sinsp_evt *evt, OUT uint32_t* len)
+{
+	switch(m_field_id)
+	{
+	case TYPE_TIME:
+	case TYPE_TIME_S:
+	case TYPE_TIME_ISO8601:
+	case TYPE_DATETIME:
+	case TYPE_DATETIME_S:
+		return (Json::Value::Int64)evt->get_ts();
+
+	case TYPE_RAWTS:
+	case TYPE_RAWTS_S:
+	case TYPE_RAWTS_NS:
+	case TYPE_RELTS:
+	case TYPE_RELTS_S:
+	case TYPE_RELTS_NS:
+		return (Json::Value::Int64)*(uint64_t*)extract(evt, len);
+	default:
+		return Json::nullValue;
+	}
+
+	return Json::nullValue;
+}
+
+uint8_t* sinsp_filter_check_gen_event::extract(sinsp_evt *evt, OUT uint32_t* len, bool sanitize_strings)
+{
+	*len = 0;
+	switch(m_field_id)
+	{
+	case TYPE_TIME:
+		if(false)
+		{
+			m_strstorage = to_string(evt->get_ts());
+		}
+		else
+		{
+			sinsp_utils::ts_to_string(evt->get_ts(), &m_strstorage, false, true);
+		}
+		RETURN_EXTRACT_STRING(m_strstorage);
+	case TYPE_TIME_S:
+		sinsp_utils::ts_to_string(evt->get_ts(), &m_strstorage, false, false);
+		RETURN_EXTRACT_STRING(m_strstorage);
+	case TYPE_TIME_ISO8601:
+		sinsp_utils::ts_to_iso_8601(evt->get_ts(), &m_strstorage);
+		RETURN_EXTRACT_STRING(m_strstorage);
+	case TYPE_DATETIME:
+		sinsp_utils::ts_to_string(evt->get_ts(), &m_strstorage, true, true);
+		RETURN_EXTRACT_STRING(m_strstorage);
+	case TYPE_DATETIME_S:
+		sinsp_utils::ts_to_string(evt->get_ts(), &m_strstorage, true, false);
+		RETURN_EXTRACT_STRING(m_strstorage);
+	case TYPE_RAWTS:
+		m_u64val = evt->get_ts();
+		RETURN_EXTRACT_VAR(m_u64val);
+	case TYPE_RAWTS_S:
+		m_u64val = evt->get_ts() / ONE_SECOND_IN_NS;
+		RETURN_EXTRACT_VAR(m_u64val);
+	case TYPE_RAWTS_NS:
+		m_u64val = evt->get_ts() % ONE_SECOND_IN_NS;
+		RETURN_EXTRACT_VAR(m_u64val);
+	case TYPE_RELTS:
+		m_u64val = evt->get_ts() - m_inspector->m_firstevent_ts;
+		RETURN_EXTRACT_VAR(m_u64val);
+	case TYPE_RELTS_S:
+		m_u64val = (evt->get_ts() - m_inspector->m_firstevent_ts) / ONE_SECOND_IN_NS;
+		RETURN_EXTRACT_VAR(m_u64val);
+	case TYPE_RELTS_NS:
+		m_u64val = (evt->get_ts() - m_inspector->m_firstevent_ts) % ONE_SECOND_IN_NS;
+		RETURN_EXTRACT_VAR(m_u64val);
+	case TYPE_NUMBER:
+		m_u64val = evt->get_num();
+		RETURN_EXTRACT_VAR(m_u64val);
+	default:
+		ASSERT(false);
+		return NULL;
+	}
+
+	return NULL;
+}
+
+const filtercheck_field_info sinsp_filter_check_event_fields[] =
+{
 	{PT_RELTIME, EPF_NONE, PF_DEC, "evt.latency", "delta between an exit event and the correspondent enter event, in nanoseconds."},
 	{PT_RELTIME, EPF_NONE, PF_DEC, "evt.latency.s", "integer part of the event latency delta."},
 	{PT_RELTIME, EPF_NONE, PF_10_PADDED_DEC, "evt.latency.ns", "fractional part of the event latency delta."},
@@ -3340,20 +3442,9 @@ Json::Value sinsp_filter_check_event::extract_as_js(sinsp_evt *evt, OUT uint32_t
 {
 	switch(m_field_id)
 	{
-	case TYPE_TIME:
-	case TYPE_TIME_S:
-	case TYPE_TIME_ISO8601:
-	case TYPE_DATETIME:
-	case TYPE_DATETIME_S:
 	case TYPE_RUNTIME_TIME_OUTPUT_FORMAT:
 		return (Json::Value::Int64)evt->get_ts();
 
-	case TYPE_RAWTS:
-	case TYPE_RAWTS_S:
-	case TYPE_RAWTS_NS:
-	case TYPE_RELTS:
-	case TYPE_RELTS_S:
-	case TYPE_RELTS_NS:
 	case TYPE_LATENCY:
 	case TYPE_LATENCY_S:
 	case TYPE_LATENCY_NS:
@@ -3417,46 +3508,6 @@ uint8_t* sinsp_filter_check_event::extract(sinsp_evt *evt, OUT uint32_t* len, bo
 	*len = 0;
 	switch(m_field_id)
 	{
-	case TYPE_TIME:
-//		if(g_filterchecks_force_raw_times)
-		if(false)
-		{
-			m_strstorage = to_string(evt->get_ts());
-		}
-		else
-		{
-			sinsp_utils::ts_to_string(evt->get_ts(), &m_strstorage, false, true);
-		}
-		RETURN_EXTRACT_STRING(m_strstorage);
-	case TYPE_TIME_S:
-		sinsp_utils::ts_to_string(evt->get_ts(), &m_strstorage, false, false);
-		RETURN_EXTRACT_STRING(m_strstorage);
-	case TYPE_TIME_ISO8601:
-		sinsp_utils::ts_to_iso_8601(evt->get_ts(), &m_strstorage);
-		RETURN_EXTRACT_STRING(m_strstorage);
-	case TYPE_DATETIME:
-		sinsp_utils::ts_to_string(evt->get_ts(), &m_strstorage, true, true);
-		RETURN_EXTRACT_STRING(m_strstorage);
-	case TYPE_DATETIME_S:
-		sinsp_utils::ts_to_string(evt->get_ts(), &m_strstorage, true, false);
-		RETURN_EXTRACT_STRING(m_strstorage);
-	case TYPE_RAWTS:
-		RETURN_EXTRACT_VAR(evt->m_pevt->ts);
-	case TYPE_RAWTS_S:
-		m_u64val = evt->get_ts() / ONE_SECOND_IN_NS;
-		RETURN_EXTRACT_VAR(m_u64val);
-	case TYPE_RAWTS_NS:
-		m_u64val = evt->get_ts() % ONE_SECOND_IN_NS;
-		RETURN_EXTRACT_VAR(m_u64val);
-	case TYPE_RELTS:
-		m_u64val = evt->get_ts() - m_inspector->m_firstevent_ts;
-		RETURN_EXTRACT_VAR(m_u64val);
-	case TYPE_RELTS_S:
-		m_u64val = (evt->get_ts() - m_inspector->m_firstevent_ts) / ONE_SECOND_IN_NS;
-		RETURN_EXTRACT_VAR(m_u64val);
-	case TYPE_RELTS_NS:
-		m_u64val = (evt->get_ts() - m_inspector->m_firstevent_ts) % ONE_SECOND_IN_NS;
-		RETURN_EXTRACT_VAR(m_u64val);
 	case TYPE_LATENCY:
 		{
 			m_u64val = 0;
@@ -3802,8 +3853,6 @@ uint8_t* sinsp_filter_check_event::extract(sinsp_evt *evt, OUT uint32_t* len, bo
 		}
 
 		RETURN_EXTRACT_STRING(m_strstorage);
-	case TYPE_NUMBER:
-		RETURN_EXTRACT_VAR(evt->m_evtnum);
 	case TYPE_CPU:
 		RETURN_EXTRACT_VAR(evt->m_cpuid);
 	case TYPE_ARGRAW:
