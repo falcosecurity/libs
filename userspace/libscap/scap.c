66bb0f628cc5390b0c226a85d3cc78f38aaa2079
Leonardo Grasso <me@leonardograsso.com>
2022-02-03 11:13:50 +0100

    build: refactor `PROBE_` to `DRIVER_` for driver API/schema versioning
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- userspace/libscap/scap.c
+++ userspace/libscap/scap.c
@@ -555,7 +555,7 @@ scap_t* scap_open_live_int(char *error, int32_t *rc,
 		}
 	}
 
-	if(!scap_is_api_compatible(handle->m_api_version, SCAP_MINIMUM_PROBE_API_VERSION))
+	if(!scap_is_api_compatible(handle->m_api_version, SCAP_MINIMUM_DRIVER_API_VERSION))
 	{
 		snprintf(error, SCAP_LASTERR_SIZE, "Probe supports API version %lu.%lu.%lu, but running version needs %d.%d.%d",
 			PPM_API_VERSION_MAJOR(handle->m_api_version),
@@ -569,7 +569,7 @@ scap_t* scap_open_live_int(char *error, int32_t *rc,
 		return NULL;
 	}
 
-	if(!scap_is_api_compatible(handle->m_schema_version, SCAP_MINIMUM_PROBE_SCHEMA_VERSION))
+	if(!scap_is_api_compatible(handle->m_schema_version, SCAP_MINIMUM_DRIVER_SCHEMA_VERSION))
 	{
 		snprintf(error, SCAP_LASTERR_SIZE, "Probe supports schema version %lu.%lu.%lu, but running version needs %d.%d.%d",
 			PPM_API_VERSION_MAJOR(handle->m_schema_version),
