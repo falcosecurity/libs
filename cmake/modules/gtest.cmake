98a56d055d3e02e6bd9f59ab282dae7d2c129950
TPT <teryl.taylor@gmail.com>
2022-03-14 21:50:36 +0000

    build(install): updated install scripts to use GNUInstallDirs variables. removed installation of protobuf bins.
    Signed-off-by: TPT <teryl.taylor@gmail.com>

--- cmake/modules/gtest.cmake
+++ cmake/modules/gtest.cmake
@@ -36,7 +36,7 @@ else()
 
 	set(GTEST_INCLUDE_DIR "${gtest_SOURCE_DIR}/include/gtest")
 	set(GTEST_MAIN_LIB "gtest_main")
-	install(DIRECTORY "${GTEST_INCLUDE_DIR}" DESTINATION "./include/${LIBS_PACKAGE_NAME}/")
+	install(DIRECTORY "${GTEST_INCLUDE_DIR}" DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/${LIBS_PACKAGE_NAME}/")
 endif()
 
 include_directories("${GTEST_INCLUDE_DIR}")
