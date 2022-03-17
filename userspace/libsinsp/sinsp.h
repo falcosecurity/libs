ee41927a08298b9cd080e0e05a67f54bc9f637fb
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2022-03-17 14:58:31 +0100

    Restore setters used in tests
    This reverts a part of c4370e7c03ed06253a65f84d18528e1cc21dc16e

--- userspace/libsinsp/sinsp.h
+++ userspace/libsinsp/sinsp.h
@@ -1512,6 +1512,18 @@ public:
 	friend class sinsp_usergroup_manager;
 
 	template<class TKey,class THash,class TCompare> friend class sinsp_connection_manager;
+
+#ifdef SYSDIG_TEST
+protected:
+	void inject_machine_info(const scap_machine_info *value)
+	{
+		m_machine_info = value;
+	}
+	void inject_network_interfaces(sinsp_network_interfaces *value)
+	{
+		m_network_interfaces = value;
+	}
+#endif // SYSDIG_TEST
 };
 
 /*@}*/
