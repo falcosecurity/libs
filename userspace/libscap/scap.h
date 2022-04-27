631b54ff5ee3f2bf1aafccf6614b6376e4f8a80e
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2022-04-27 15:01:53 +0200

    Split a few things out of scap.h
    This is needed to prevent circular includes later on.
    
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

--- userspace/libscap/scap.h
+++ userspace/libscap/scap.h
@@ -68,6 +68,9 @@ struct iovec;
 #endif
 
 #include "plugin_info.h"
+#include "scap_limits.h"
+#include "scap_open.h"
+#include "scap_procs.h"
 
 //
 // The minimum API and schema versions the driver has to support before we can use it
@@ -131,12 +134,6 @@ typedef struct evt_param_info
 	uint32_t len; ///< The event total length.
 	char* val; ///< The event data.
 }evt_param_info;
-
-#define SCAP_MAX_PATH_SIZE 1024
-#define SCAP_MAX_ARGS_SIZE 4096
-#define SCAP_MAX_ENV_SIZE 4096
-#define SCAP_MAX_CGROUPS_SIZE 4096
-#define SCAP_MAX_SUPPRESSED_COMMS 32
 
 /*!
   \brief File Descriptor type
@@ -285,71 +282,6 @@ typedef struct {
 	uint32_t dev; ///< device number
 	UT_hash_handle hh; ///< makes this structure hashable
 } scap_mountinfo;
-
-typedef void (*proc_entry_callback)(void* context,
-									scap_t* handle,
-									int64_t tid,
-									scap_threadinfo* tinfo,
-									scap_fdinfo* fdinfo);
-
-/*!
-  \brief Arguments for scap_open
-*/
-typedef enum {
-	/*!
-	 * Default value that mostly exists so that sinsp can have a valid value
-	 * before it is initialized.
-	 */
-	SCAP_MODE_NONE = 0,
-	/*!
-	 * Read system call data from a capture file.
-	 */
-	SCAP_MODE_CAPTURE,
-	/*!
-	 * Read system call data from the underlying operating system.
-	 */
-	SCAP_MODE_LIVE,
-	/*!
-	 * Do not read system call data. If next is called, a dummy event is
-	 * returned.
-	 */
-	SCAP_MODE_NODRIVER,
-	/*!
-	 * Do not read system call data. Events come from the configured input plugin.
-	 */
-	SCAP_MODE_PLUGIN,
-} scap_mode_t;
-
-/*!
-  \brief Argument for scap_open
-  Set any PPM_SC syscall idx to true to enable its tracing at driver level,
-  otherwise syscalls are not traced (so called "uninteresting syscalls").
-*/
-typedef struct {
-	bool ppm_sc[PPM_SC_MAX];
-} interesting_ppm_sc_set;
-
-typedef struct scap_open_args
-{
-	scap_mode_t mode;
-	int fd; // If non-zero, will be used instead of fname.
-	const char* fname; ///< The name of the file to open. NULL for live captures.
-	proc_entry_callback proc_callback; ///< Callback to be invoked for each thread/fd that is extracted from /proc, or NULL if no callback is needed.
-	void* proc_callback_context; ///< Opaque pointer that will be included in the calls to proc_callback. Ignored if proc_callback is NULL.
-	bool import_users; ///< true if the user list should be created when opening the capture.
-	uint64_t start_offset; ///< Used to start reading a capture file from an arbitrary offset. This is leveraged when opening merged files.
-	const char *bpf_probe; ///< The name of the BPF probe to open. If NULL, the kernel driver will be used.
-	const char *suppressed_comms[SCAP_MAX_SUPPRESSED_COMMS]; ///< A list of processes (comm) for which no
-	                                                         // events should be returned, with a trailing NULL value.
-	                                                         // You can provide additional comm
-	                                                         // values via scap_suppress_events_comm().
-	bool udig; ///< If true, UDIG will be used for event capture. Otherwise, the kernel driver will be used.
-	interesting_ppm_sc_set ppm_sc_of_interest;
-
-	scap_source_plugin* input_plugin; ///< use this to configure a source plugin that will produce the events for this capture
-	char* input_plugin_params; ///< optional parameters string for the source plugin pointed by src_plugin
-}scap_open_args;
-
 
 //
 // The following stuff is byte aligned because we save it to disk.
