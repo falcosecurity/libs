664fa4a37fe3526ec3be620c184c3548aab9f47e
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2022-04-27 19:18:43 +0200

    Make nodriver a separate scap engine
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

---
+++ userspace/libscap/engine/nodriver/nodriver.c
@@ -0,0 +1,71 @@
+/*
+Copyright (C) 2022 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "nodriver.h"
+#include "noop.h"
+
+#include "scap.h"
+#include "scap-int.h"
+#include "../common/strlcpy.h"
+#include "gettimeofday.h"
+#include "sleep.h"
+
+static struct nodriver_engine* alloc_handle(scap_t* main_handle, char* lasterr_ptr)
+{
+	struct nodriver_engine *engine = calloc(1, sizeof(struct nodriver_engine));
+	if(engine)
+	{
+		engine->m_lasterr = lasterr_ptr;
+	}
+	return engine;
+}
+
+static int32_t next(struct scap_engine_handle handle, scap_evt** pevent, uint16_t* pcpuid)
+{
+	static scap_evt evt;
+	evt.len = 0;
+	evt.tid = -1;
+	evt.type = PPME_SCAPEVENT_X;
+	evt.nparams = 0;
+
+	sleep_ms(100);
+
+	evt.ts = get_timestamp_ns();
+	*pevent = &evt;
+	return SCAP_SUCCESS;
+}
+
+const struct scap_vtable scap_nodriver_engine = {
+	.name = "nodriver",
+	.mode = SCAP_MODE_NODRIVER,
+
+	.alloc_handle = alloc_handle,
+	.init = NULL,
+	.free_handle = noop_free_handle,
+	.close = noop_close_engine,
+	.next = next,
+	.start_capture = noop_start_capture,
+	.stop_capture = noop_stop_capture,
+	.configure = noop_configure,
+	.get_stats = noop_get_stats,
+	.get_n_tracepoint_hit = noop_get_n_tracepoint_hit,
+	.get_n_devs = noop_get_n_devs,
+	.get_max_buf_used = noop_get_max_buf_used,
+};
