b9c5c0163b234a46dddf71ac51480fcd75ad62e5
Jason Dellaluce <jasondellaluce@gmail.com>
2022-04-29 15:11:41 +0000

    refactor(userspace/libsinsp): polish plugin system implementation
    This splits the plugin.h/cpp source files by bringing out
    the plugin filtercheck implementation in its own source files.
    Code polishing involve adding virtual destructors to the plugin
    capability-interfaces, fixing-up const consistency, removing
    static object instances.
    
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- userspace/libsinsp/plugin.h
+++ userspace/libsinsp/plugin.h
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2021 The Falco Authors.
+Copyright (C) 2022 The Falco Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -24,10 +24,8 @@ limitations under the License.
 #include <string>
 #include <vector>
 #include <unordered_map>
-
 #include <plugin_info.h>
-
-#include "filter_check_list.h"
+#include "version.h"
 
 #ifdef _WIN32
 typedef HINSTANCE sinsp_plugin_handle;
@@ -47,9 +45,27 @@ typedef enum
 	CAP_EXTRACTION   = 1 << 1
 } ss_plugin_caps;
 
-class sinsp_plugin_cap_sourcing
+class sinsp_plugin_cap_common
 {
 public:
+	virtual ~sinsp_plugin_cap_common() = default;
+	virtual bool init(const char* config) = 0;
+	virtual void destroy() = 0;
+	virtual std::string get_last_error() const = 0;
+	virtual const std::string &name() const = 0;
+	virtual const std::string &description() const = 0;
+	virtual const std::string &contact() const = 0;
+	virtual const sinsp_version &plugin_version() const = 0;
+	virtual const sinsp_version &required_api_version() const = 0;
+	virtual std::string get_init_schema(ss_plugin_schema_type& schema_type) const = 0;
+	virtual ss_plugin_caps caps() const = 0;
+};
+
+class sinsp_plugin_cap_sourcing: public sinsp_plugin_cap_common
+{
+public:
+	virtual ~sinsp_plugin_cap_sourcing() = default;
+
 	// Describes a valid parameter for the open() function.
 	struct open_param {
 		std::string value;
@@ -57,33 +73,28 @@ public:
 	};
 
 	// Return a struct to be used as scap source plugin
-	virtual scap_source_plugin *as_scap_source() = 0;
+	virtual scap_source_plugin& as_scap_source() = 0;
 
-	virtual uint32_t id() = 0;
-	virtual const std::string &event_source() = 0;
+	virtual uint32_t id() const = 0;
+	virtual const std::string &event_source() const = 0;
 
-	virtual std::string get_progress(uint32_t &progress_pct) = 0;
+	virtual std::string get_progress(uint32_t &progress_pct) const = 0;
 
-	virtual std::string event_to_string(sinsp_evt* evt) = 0;
+	virtual std::string event_to_string(sinsp_evt* evt) const = 0;
 
-	virtual std::vector<open_param> list_open_params() = 0;
+	virtual std::vector<open_param> list_open_params() const = 0;
 };
 
-class sinsp_plugin_cap_extraction
+class sinsp_plugin_cap_extraction: public sinsp_plugin_cap_common
 {
 public:
-	virtual const std::set<std::string> &extract_event_sources() = 0;
+	virtual ~sinsp_plugin_cap_extraction() = default;
 
-	// Return true if the provided source is compatible with this
-	// plugin with extractor capabilities, either because it does
-	// not name any extract sources, or if the provided source is
-	// in the set of extract sources.
-	virtual bool source_compatible(const std::string &source) = 0;
+	virtual const std::set<std::string> &extract_event_sources() const = 0;
 
-	virtual bool extract_fields(ss_plugin_event &evt, uint32_t num_fields, ss_plugin_extract_field *fields) = 0;
+	virtual bool extract_fields(ss_plugin_event &evt, uint32_t num_fields, ss_plugin_extract_field *fields) const = 0;
 
-	virtual const filtercheck_field_info *fields() = 0;
-	virtual uint32_t nfields() = 0;
+	virtual const std::vector<filtercheck_field_info>& fields() const = 0;
 };
 
 // Class that holds a plugin.
@@ -92,42 +103,13 @@ public:
 class sinsp_plugin: public sinsp_plugin_cap_sourcing, public sinsp_plugin_cap_extraction
 {
 public:
-	class version {
-	public:
-		version();
-		version(const std::string &version_str);
-		virtual ~version();
-
-		std::string as_string() const;
-		bool check(version &requested) const;
-
-		bool m_valid;
-		uint32_t m_version_major;
-		uint32_t m_version_minor;
-		uint32_t m_version_patch;
-	};
-
-	// Contains important info about a plugin, suitable for
-	// printing or other checks like compatibility.
-	struct info {
-		ss_plugin_caps caps;
-		std::string name;
-		std::string description;
-		std::string contact;
-		version plugin_version;
-		version required_api_version;
-
-		// Only filled in for plugins with CAP_EVENT_SOURCE capability
-		uint32_t id;
-	};
-
 	// Create a plugin from the dynamic library at the provided
 	// path. On error, the shared_ptr will == NULL and errstr is
 	// set with an error.
-	static std::shared_ptr<sinsp_plugin> create_plugin(std::string &filepath,
-													   const char* config,
-													   std::string &errstr,
-													   filter_check_list &available_checks);
+	static std::shared_ptr<sinsp_plugin> create_plugin(
+		std::string &filepath,
+		const char* config,
+		std::string &errstr);
 
 	// Return whether a filesystem object is loaded
 	static bool is_plugin_loaded(std::string &filepath);
@@ -135,75 +117,53 @@ public:
 	sinsp_plugin(sinsp_plugin_handle handle);
 	virtual ~sinsp_plugin();
 
-	bool init(const char* config);
-	void destroy();
-
 	/** Common API **/
-	std::string get_last_error();
-	const std::string &name();
-	const std::string &description();
-	const std::string &contact();
-	const version &plugin_version();
-	const version &required_api_version();
+	bool init(const char* config) override;
+	void destroy() override;
+	std::string get_last_error() const override;
+	const std::string &name() const override;
+	const std::string &description() const override;
+	const std::string &contact() const override;
+	const sinsp_version &plugin_version() const override;
+	const sinsp_version &required_api_version() const override;
+	std::string get_init_schema(ss_plugin_schema_type& schema_type) const override;
+	ss_plugin_caps caps() const override;
 
-	std::string get_init_schema(ss_plugin_schema_type& schema_type);
-	void validate_init_config(std::string& config);
-	/** **/
+	/** Event Sourcing **/
+	scap_source_plugin& as_scap_source() override;
+	uint32_t id() const override;
+	const std::string &event_source() const override;
+	std::string get_progress(uint32_t &progress_pct) const override;
+	std::string event_to_string(sinsp_evt* evt) const override;
+	std::vector<sinsp_plugin_cap_sourcing::open_param> list_open_params() const override;
 
-	/** Sourcing API **/
-	scap_source_plugin *as_scap_source();
-
-	uint32_t id();
-	const std::string &event_source();
-
-	std::string get_progress(uint32_t &progress_pct);
-
-	std::string event_to_string(sinsp_evt* evt);
-
-	std::vector<sinsp_plugin_cap_sourcing::open_param> list_open_params();
-	/** **/
-
-	/** Extraction API **/
-	const std::set<std::string> &extract_event_sources();
-
-	// Return true if the provided source is compatible with this
-	// plugin with extractor capabilities, either because it does
-	// not name any extract sources, or if the provided source is
-	// in the set of extract sources.
-	bool source_compatible(const std::string &source);
-
-	bool extract_fields(ss_plugin_event &evt, uint32_t num_fields, ss_plugin_extract_field *fields);
-
-	const filtercheck_field_info *fields();
-	uint32_t nfields();
-	/** **/
-
-	ss_plugin_caps caps();
+	/** Field Extraction **/
+	const std::set<std::string> &extract_event_sources() const override;
+	bool extract_fields(ss_plugin_event &evt, uint32_t num_fields, ss_plugin_extract_field *fields) const override;
+	const std::vector<filtercheck_field_info>& fields() const override;
 
 private:
 	std::string m_name;
 	std::string m_description;
 	std::string m_contact;
-	version m_plugin_version;
-	version m_required_api_version;
+	sinsp_version m_plugin_version;
+	sinsp_version m_required_api_version;
 
 	plugin_api m_api;
 	ss_plugin_t* m_state;
 	ss_plugin_caps m_caps;
 	sinsp_plugin_handle m_handle;
 
-	/** Sourcing related **/
+	/** Event Sourcing **/
 	uint32_t m_id;
 	std::string m_event_source;
-	static scap_source_plugin sp;
-	/** **/
+	scap_source_plugin m_scap_source_plugin;
 
-	/** Extraction related **/
-	std::unique_ptr<filtercheck_field_info[]> m_fields;
-	int32_t m_nfields;
+	/** Field Extraction **/
+	std::vector<filtercheck_field_info> m_fields;
 	std::set<std::string> m_extract_event_sources;
-	/** **/
 
+	void validate_init_config(std::string& config);
 	bool resolve_dylib_symbols(std::string &errstr);
 	void resolve_dylib_field_arg(Json::Value root, filtercheck_field_info &tf);
 	void validate_init_config_json_schema(std::string& config, std::string &schema);
---
+++ userspace/libsinsp/plugin_filtercheck.h
@@ -0,0 +1,75 @@
+/*
+Copyright (C) 2022 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+#pragma once
+
+#include <memory>
+#include <set>
+#include <string>
+#include <vector>
+#include "sinsp_int.h"
+#include "version.h"
+#include "filter.h"
+#include "filterchecks.h"
+
+/**
+	\brief This class implements a dynamic filter check that acts as a
+	bridge to the plugin simplified field extraction implementations
+ */
+class sinsp_filter_check_plugin : public sinsp_filter_check
+{
+public:
+	sinsp_filter_check_plugin();
+
+	explicit sinsp_filter_check_plugin(std::shared_ptr<sinsp_plugin> plugin);
+
+	explicit sinsp_filter_check_plugin(const sinsp_filter_check_plugin &p);
+
+	virtual ~sinsp_filter_check_plugin();
+
+	sinsp_filter_check* allocate_new() override;
+
+	int32_t parse_field_name(
+		const char* str,
+		bool alloc_state,
+		bool needed_for_filtering) override;
+
+	bool extract(
+		sinsp_evt *evt,
+		OUT std::vector<extract_value_t>& values,
+		bool sanitize_strings = true) override;
+
+private:
+	std::string m_argstr;
+	char* m_arg_key;
+	uint64_t m_arg_index;
+	bool m_arg_present;
+	std::vector<std::string> m_res_str_storage;
+	std::vector<uint64_t> m_res_u64_storage;
+	std::set<size_t>* m_compatible_sources = NULL;
+	std::shared_ptr<sinsp_plugin_cap_extraction> m_eplugin;
+
+	// extract_arg_index() extracts a valid index from the argument if 
+	// format is valid, otherwise it throws an exception.
+	// `full_field_name` has the format "field[argument]" and it is necessary
+	// to throw an exception.
+	void extract_arg_index(const char* full_field_name);
+
+	// extract_arg_key() extracts a valid string from the argument. If we pass
+	// a numeric argument, it will be converted to string. 
+	void extract_arg_key();
+};
