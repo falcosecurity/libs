5d1029f799164d6313f617a09de9e3e5571ce768
Luca Guerra <luca@guerra.sh>
2022-05-13 12:58:27 +0000

    fix(libscap): add defines for alpine-musl/centos7 builds that don't have them
    Signed-off-by: Luca Guerra <luca@guerra.sh>

--- userspace/libscap/userspace_flag_helpers.h
+++ userspace/libscap/userspace_flag_helpers.h
@@ -9,9 +9,17 @@
 #include <sys/quota.h>
 #include <sys/ptrace.h>
 #include <sys/resource.h>
+#include <sys/file.h>
 
 #define ASSERT assert
 #define F_CANCELLK 1024 + 5
+
+#ifndef QFMT_VFS_OLD
+#define	QFMT_VFS_OLD 1
+#define	QFMT_VFS_V0 2
+#define QFMT_OCFS2 3
+#define	QFMT_VFS_V1 4
+#endif
 
 #define u8 uint8_t
 #define u16 uint16_t
