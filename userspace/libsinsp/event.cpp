146f0b46abffd8b421c89d51a9ed62278843d999
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2022-05-27 14:53:22 +0200

    fix: get_param_as_str for PT_ENUMFLAGS* fields
    Enum flags need to follow the same logic as bit flags,
    i.e. cast the field value to the correct pointer type
    before dereferencing.
    
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

--- userspace/libsinsp/event.cpp
+++ userspace/libsinsp/event.cpp
@@ -2059,8 +2059,24 @@ const char* sinsp_evt::get_param_as_str(uint32_t id, OUT const char** resolved_s
 	case PT_ENUMFLAGS16:
 	case PT_ENUMFLAGS32:
 		{
-			uint32_t val = (param_info->type == PT_FLAGS8) ? *(uint8_t *)payload :
-				(param_info->type == PT_FLAGS16) ? *(uint16_t *)payload : *(uint32_t *)payload;
+			uint32_t val = 0;
+			switch(param_info->type)
+			{
+			case PT_FLAGS8:
+			case PT_ENUMFLAGS8:
+				val = *(uint8_t *)payload;
+				break;
+			case PT_FLAGS16:
+			case PT_ENUMFLAGS16:
+				val = *(uint16_t *)payload;
+				break;
+			case PT_FLAGS32:
+			case PT_ENUMFLAGS32:
+				val = *(uint32_t *)payload;
+				break;
+			default:
+				ASSERT(false);
+			}
 			snprintf(&m_paramstr_storage[0],
 				     m_paramstr_storage.size(),
 				     "%" PRIu32, val);
