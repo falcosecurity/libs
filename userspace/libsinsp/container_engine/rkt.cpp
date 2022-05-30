39ae7d40496793cf3d3e7890c9bbdc202263836b
Federico Di Pierro <nierro92@gmail.com>
2022-05-30 16:40:08 +0200

    fix(userspace/libsinsp): added back add_container() calls for synchronous container engines.
    Old behavior for sync engines was to load informations as quick as possible, without waiting
    for the injected event to get parsed.
    
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- userspace/libsinsp/container_engine/rkt.cpp
+++ userspace/libsinsp/container_engine/rkt.cpp
@@ -189,6 +189,7 @@ bool rkt::rkt::resolve(sinsp_threadinfo* tinfo, bool query_os_for_missing_info)
 
 	if (have_rkt)
 	{
+		cache->add_container(std::make_shared<sinsp_container_info>(container), tinfo);
 		cache->notify_new_container(container, tinfo);
 		return true;
 	}
