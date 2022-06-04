7ec840a6e1a7dd1b167f1445799a3f3c8fd7b039
Andrea Terzolo <andrea.terzolo@polito.it>
2022-06-05 01:51:07 +0200

    new(libpman): add internal state
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

---
+++ userspace/libpman/src/state.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "state.h"
+
+struct internal_state g_state;
+
+void libpman__print_error(const char* error_message)
+{
+	if(!error_message)
+	{
+		fprintf(stderr, "libpman: No specific message available (errno: %d | message: %s)\n", errno, strerror(errno));
+	}
+	else
+	{
+		fprintf(stderr, "libpman: %s (errno: %d | message: %s)\n", error_message, errno, strerror(errno));
+	}
+}
---
+++ userspace/libpman/src/state.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <bpf/libbpf.h>
+#include <bpf/bpf.h>
+#include <struct_definitions.h> // contains definition used in the skeleton
+#include <bpf_probe.skel.h>
+#include <unistd.h>
+
+#define MAX_ERROR_MESSAGE_LEN 100
+
+struct internal_state
+{
+	struct bpf_probe* skel;		/* bpf skeleton with all programs and maps*/
+	struct ring_buffer* rb_manager; /* ring_buffer manager with all per-CPU ringbufs*/
+	int16_t n_cpus;			/* number of system available CPUs. */
+	int ringbuf_pos;		/* actual ringbuf we are considering. */
+	unsigned long* cons_pos;	/* every ringbuf has a consumer position. */
+	unsigned long* prod_pos;	/* every ringbuf has a producer position. */
+};
+
+extern struct internal_state g_state;
+
+extern void libpman__print_error(const char* error_message);
