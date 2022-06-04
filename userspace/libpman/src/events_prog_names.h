d9ac9c5dd07428783b1e6dd0cf186f1826c939e7
Andrea Terzolo <andrea.terzolo@polito.it>
2022-06-05 01:44:50 +0200

    new(libpman): add bpf prog names table
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

---
+++ userspace/libpman/src/events_prog_names.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <ppm_events_public.h>
+
+/* For every event here we have the name of the corresponding bpf program. */
+static const char* event_prog_names[PPM_EVENT_MAX] = {
+	[PPME_SYSCALL_MKDIR_2_E] = "mkdir_e",
+	[PPME_SYSCALL_MKDIR_2_X] = "mkdir_x",
+};
+
+/* Some events can require more than one bpf program to collect all the data. */
+static const char* extra_event_prog_names[TAIL_EXTRA_EVENT_PROG_MAX] = {
+};
