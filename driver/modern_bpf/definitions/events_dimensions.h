7f34fd60d236b8427ca39fe3ef6e7a3e75478ca6
Andrea Terzolo <andrea.terzolo@polito.it>
2022-06-05 01:07:29 +0200

    new(modern_bpf): add macro for fixed-size events
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

---
+++ driver/modern_bpf/definitions/events_dimensions.h
@@ -0,0 +1,16 @@
+#ifndef __EVENT_DIMENSIONS_H__
+#define __EVENT_DIMENSIONS_H__
+
+#include "vmlinux.h"
+
+/* Here we have all the dimensions for fixed-size events.
+ */
+
+#define PARAM_LEN 2
+#define HEADER_LEN sizeof(struct ppm_evt_hdr)
+
+/// TODO: We have to move these in the event_table.c. Right now we don't
+/// want to touch scap tables.
+#define MKDIR_E_SIZE HEADER_LEN + sizeof(uint32_t) + PARAM_LEN
+
+#endif /* __EVENT_DIMENSIONS_H__ */
