165355c17b4af9b48642cbc49d464981c0e06b61
Andrea Terzolo <andrea.terzolo@polito.it>
2022-06-07 16:25:01 +0200

    fix(libpman): keep the ringbuf inner map until the loading phase is ended
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- userspace/libpman/src/state.h
+++ userspace/libpman/src/state.h
@@ -10,12 +10,13 @@
 
 struct internal_state
 {
-	struct bpf_probe* skel;		/* bpf skeleton with all programs and maps*/
-	struct ring_buffer* rb_manager; /* ring_buffer manager with all per-CPU ringbufs*/
+	struct bpf_probe* skel;		/* bpf skeleton with all programs and maps. */
+	struct ring_buffer* rb_manager; /* ring_buffer manager with all per-CPU ringbufs. */
 	int16_t n_cpus;			/* number of system available CPUs. */
 	int ringbuf_pos;		/* actual ringbuf we are considering. */
 	unsigned long* cons_pos;	/* every ringbuf has a consumer position. */
 	unsigned long* prod_pos;	/* every ringbuf has a producer position. */
+	int32_t inner_ringbuf_map_fd;	/* inner map used to configure the ringbuf array before loading phase. */
 };
 
 extern struct internal_state g_state;
