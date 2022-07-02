7c6278d62838707cebf31300a682ce31bdb47a62
Andrea Terzolo <andrea.terzolo@polito.it>
2022-07-02 16:43:05 +0200

    cleanup(kmod): address a TODO
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- driver/main.c
+++ driver/main.c
@@ -10,18 +10,13 @@ or GPL2.txt for full copies of the license.
 #define pr_fmt(fmt)	KBUILD_MODNAME ": " fmt
 
 #include <linux/version.h>
+#include <trace/syscall.h>
+
 #if LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 20)
 #include <linux/kobject.h>
 #include <trace/sched.h>
 #include "ppm_syscall.h"
-#include <trace/syscall.h>
 #else
-
-/// TODO: @Andreagit97 @FedeDP we need to remove the ifdef, this patch should also work on x86.
-#ifdef CONFIG_ARM64
-#include <trace/syscall.h>
-#endif /* CONFIG_ARM64 */
-
 #include <asm/syscall.h>
 #endif /* LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 20) */
 
