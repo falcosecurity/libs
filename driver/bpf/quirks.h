c039e743f158781abcc876329b9daade728e155f
Andrea Terzolo <andrea.terzolo@polito.it>
2022-07-02 15:56:07 +0200

    cleanup(drivers): redefine some compilation warnings
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- driver/bpf/quirks.h
+++ driver/bpf/quirks.h
@@ -28,8 +28,8 @@ or GPL2.txt for full copies of the license.
 #define BPF_SUPPORTS_RAW_TRACEPOINTS
 #endif
 
-#if defined(CONFIG_ARM64) && !defined(BPF_SUPPORTS_RAW_TRACEPOINTS)
-    #error The BPF ARM64 support requires 'raw_tracepoints' so kernel versions greater or equal than '4.17'.
+#if CAPTURE_SCHED_PROC_FORK && !defined(BPF_SUPPORTS_RAW_TRACEPOINTS)
+    #error The CAPTURE_SCHED_PROC_FORK support requires 'raw_tracepoints' so kernel versions greater or equal than '4.17'.
 #endif
 
 /* Redefine asm_volatile_goto to work around clang not supporting it
@@ -48,4 +48,4 @@ or GPL2.txt for full copies of the license.
 #define asm_inline asm
 #endif
 
-#endif
+#endif /* __QUIRKS_H */
