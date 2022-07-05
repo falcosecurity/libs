140d470a77c0f0dca4ea43b28312cbdff6051327
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2022-07-05 16:20:04 +0200

    Make scap_udig_engine const
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

--- userspace/libscap/engine/udig/scap_udig.c
+++ userspace/libscap/engine/udig/scap_udig.c
@@ -938,7 +938,7 @@ static uint64_t get_max_buf_used(struct scap_engine_handle engine)
 
 
 
-struct scap_vtable scap_udig_engine = {
+const struct scap_vtable scap_udig_engine = {
 	.name = "udig",
 	.mode = SCAP_MODE_LIVE,
 	.savefile_ops = NULL,
