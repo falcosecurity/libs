89c562a3c652c71a9c0bf4b8070520d926eddc9b
Federico Di Pierro <nierro92@gmail.com>
2022-07-11 16:44:02 +0200

    fix(driver/bpf): accessing loginuid depends upon a config flag in the kernel.
    The config flag is either CONFIG_AUDITSYSCALL for kernels < 5.1.0, or CONFIG_AUDIT for newer ones.
    
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- driver/bpf/fillers.h
+++ driver/bpf/fillers.h
@@ -2566,6 +2566,7 @@ FILLER(proc_startupdate_3, true)
 		 * loginuid
 		 */
 		/* TODO: implement user namespace support */
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 1, 0) && CONFIG_AUDIT) || (LINUX_VERSION_CODE < KERNEL_VERSION(5, 1, 0) && CONFIG_AUDITSYSCALL)
 #ifdef COS_73_WORKAROUND
 		{
 			struct audit_task_info* audit = _READ(task->audit);
@@ -2577,7 +2578,10 @@ FILLER(proc_startupdate_3, true)
 		}
 #else
 		loginuid = _READ(task->loginuid);
-#endif
+#endif /* COS_73_WORKAROUND */
+#else
+		loginuid.val = -1;
+#endif /* CONFIG_AUDIT... */
 
 		res = bpf_val_to_ring_type(data, loginuid.val, PT_INT32);
 		if (res != PPM_SUCCESS)
@@ -6047,6 +6051,7 @@ FILLER(sched_prog_exec_3, false)
 
 	/* TODO: implement user namespace support */
 	kuid_t loginuid;
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 1, 0) && CONFIG_AUDIT) || (LINUX_VERSION_CODE < KERNEL_VERSION(5, 1, 0) && CONFIG_AUDITSYSCALL)
 #ifdef COS_73_WORKAROUND
 	{
 		struct audit_task_info *audit = _READ(task->audit);
@@ -6061,7 +6066,10 @@ FILLER(sched_prog_exec_3, false)
 	}
 #else
 	loginuid = _READ(task->loginuid);
-#endif
+#endif /* COS_73_WORKAROUND */
+#else
+	loginuid.val = -1;
+#endif /* CONFIG_AUDIT... */
 
 	/* Parameter 19: loginuid (type: PT_INT32) */
 	res = bpf_val_to_ring_type(data, loginuid.val, PT_INT32);
