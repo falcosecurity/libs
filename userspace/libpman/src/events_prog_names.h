68ff9aed850b03622c2c4c2646674c9d85910846
Andrea Terzolo <andrea.terzolo@polito.it>
2022-07-24 15:38:50 +0200

    new(modern_bpf): add `openat` syscall
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- userspace/libpman/src/events_prog_names.h
+++ userspace/libpman/src/events_prog_names.h
@@ -25,6 +25,8 @@ static const char* event_prog_names[PPM_EVENT_MAX] = {
 	[PPME_SYSCALL_MKDIR_2_X] = "mkdir_x",
 	[PPME_SYSCALL_OPEN_E] = "open_e",
 	[PPME_SYSCALL_OPEN_X] = "open_x",
+	[PPME_SYSCALL_OPENAT_2_E] = "openat_e",
+	[PPME_SYSCALL_OPENAT_2_X] = "openat_x",
 };
 
 /* Some events can require more than one bpf program to collect all the data. */
