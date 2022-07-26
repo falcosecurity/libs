cc63b276821320d04a3ab847d9964b327a34fd1f
Andrea Terzolo <andrea.terzolo@polito.it>
2022-07-26 12:40:37 +0000

    new(modern_bpf): add `open_by_handle_at` syscall
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- driver/modern_bpf/definitions/events_dimensions.h
+++ driver/modern_bpf/definitions/events_dimensions.h
@@ -19,5 +19,6 @@
 /// TODO: We have to move these in the event_table.c. Right now we don't
 /// want to touch scap tables.
 #define MKDIR_E_SIZE HEADER_LEN + sizeof(uint32_t) + PARAM_LEN
+#define OPEN_BY_HANDLE_AT_E_SIZE HEADER_LEN
 
 #endif /* __EVENT_DIMENSIONS_H__ */
