64d88ebb6a4da599c813cb9f7e1bda3b0d7ee466
Andrea Terzolo <andrea.terzolo@polito.it>
2022-07-26 17:41:12 +0200

    fix(libsinsp): check the correct len
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>
    Co-authored-by: Mauro Ezequiel Moltrasio <mmoltras@redhat.com>

--- userspace/libsinsp/parsers.cpp
+++ userspace/libsinsp/parsers.cpp
@@ -2238,8 +2238,8 @@ void sinsp_parser::parse_open_openat_creat_exit(sinsp_evt *evt)
 			enter_evt_flags = *(uint32_t *)parinfo->m_val;
 
 			if(enter_evt_namelen != 0
-			   && strncmp(enter_evt_name, "(NULL)", 6) != 0
-			   && strncmp(enter_evt_name, "<NA>", 4) != 0)
+			   && strncmp(enter_evt_name, "(NULL)", 7) != 0
+			   && strncmp(enter_evt_name, "<NA>", 5) != 0)
 			{
 				name = enter_evt_name;
 				namelen = enter_evt_namelen;
@@ -2279,8 +2279,8 @@ void sinsp_parser::parse_open_openat_creat_exit(sinsp_evt *evt)
 			enter_evt_flags = 0;
 
 			if(enter_evt_namelen != 0
-			   && strncmp(enter_evt_name, "(NULL)", 6) != 0
-			   && strncmp(enter_evt_name, "<NA>", 4) != 0)
+			   && strncmp(enter_evt_name, "(NULL)", 7) != 0
+			   && strncmp(enter_evt_name, "<NA>", 5) != 0)
 			{
 				name = enter_evt_name;
 				namelen = enter_evt_namelen;
@@ -2351,8 +2351,8 @@ void sinsp_parser::parse_open_openat_creat_exit(sinsp_evt *evt)
 			int64_t enter_evt_dirfd = *(int64_t *)parinfo->m_val;
 
 			if(enter_evt_namelen != 0
-			   && strncmp(enter_evt_name, "(NULL)", 6) != 0
-			   && strncmp(enter_evt_name, "<NA>", 4) != 0)
+			   && strncmp(enter_evt_name, "(NULL)", 7) != 0
+			   && strncmp(enter_evt_name, "<NA>", 5) != 0)
 			{
 				name = enter_evt_name;
 				namelen = enter_evt_namelen;
