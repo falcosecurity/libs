3d4c0997f42ff490b5a47d789618c9378bdc19ec
Andrea Terzolo <andrea.terzolo@polito.it>
2022-07-27 08:49:50 +0000

    update(modern_bpf): simplify a `open_by_handle_at` test
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- test/modern_bpf/test_suites/syscall_exit_suite/open_by_handle_at_x.cpp
+++ test/modern_bpf/test_suites/syscall_exit_suite/open_by_handle_at_x.cpp
@@ -54,73 +54,27 @@ TEST(SyscallExit, open_by_handle_atX_success)
 	assert_syscall_state(SYSCALL_SUCCESS, "name_to_handle_at", syscall(__NR_name_to_handle_at, dirfd, pathname, fhp, &mount_id, flags), NOT_EQUAL, -1);
 
 	/*
-	 * 4. Retrieve the `mount_fd`.
+	 * 4. Call `open_by_handle_at`.
 	 */
-	char *linep = NULL;
-	size_t lsize = 0;
-	char mount_path[4096];
-	int mi_mount_id = 0;
-	int found = 0;
-	ssize_t nread = 0;
-	FILE *fp = NULL;
-
-	fp = fopen("/proc/self/mountinfo", "r");
-	if(fp == NULL)
-	{
-		FAIL() << "Error in opening /proc/self/mountinfo" << std::endl;
-	}
-
-	while(!found)
-	{
-		nread = getline(&linep, &lsize, fp);
-		if(nread == -1)
-		{
-			break;
-		}
-
-		nread = sscanf(linep, "%d %*d %*s %*s %s", &mi_mount_id, mount_path);
-		if(nread != 2)
-		{
-			FAIL() << "Bad sscanf()" << std::endl;
-		}
-
-		if(mi_mount_id == mount_id)
-		{
-			found = 1;
-		}
-	}
-
-	if(!found)
-	{
-		FAIL() << "Could not find mount point" << std::endl;
-	}
-
-	/*
-	 * 5. Call `open_by_handle_at`.
-	 */
-	int mount_fd = open(mount_path, O_RDONLY);
 	flags = O_RDONLY;
-	int open_by_handle_fd = syscall(__NR_open_by_handle_at, mount_fd, fhp, flags);
+	int open_by_handle_fd = syscall(__NR_open_by_handle_at, AT_FDCWD, fhp, flags);
 	assert_syscall_state(SYSCALL_SUCCESS, "open_by_handle_at", open_by_handle_fd, NOT_EQUAL, -1);
 
 	/*
-	 * 6. Get the current working directory.
+	 * 5. Get the current working directory.
 	 */
 	char tmp[4096];
-	char* err = getcwd(tmp, 4096);
+	char *err = getcwd(tmp, 4096);
 	if(!err)
 	{
 		FAIL() << "Could not get the current working directory" << std::endl;
 	}
 
 	/*
-	 * 7. Cleaning phase.
+	 * 6. Cleaning phase.
 	 */
 	close(open_by_handle_fd);
-	close(mount_fd);
-	fclose(fp);
 	close(fd);
-	free(linep);
 	free(fhp);
 
 	/*=============================== TRIGGER SYSCALL  ===========================*/
@@ -144,7 +98,7 @@ TEST(SyscallExit, open_by_handle_atX_success)
 	evt_test->assert_numeric_param(1, (int64_t)open_by_handle_fd);
 
 	/* Parameter 2: mountfd (type: PT_FD) */
-	evt_test->assert_numeric_param(2, (int64_t)mount_fd);
+	evt_test->assert_numeric_param(2, (int64_t)PPM_AT_FDCWD);
 
 	/* Parameter 3: flags (type: PT_FLAGS32) */
 	evt_test->assert_numeric_param(3, (uint32_t)PPM_O_RDONLY);
