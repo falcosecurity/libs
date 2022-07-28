ce3078a37a30797a7a4891e0e208fded046e3cb5
Andrea Terzolo <andrea.terzolo@polito.it>
2022-07-28 23:58:37 +0200

    new(modern_bpf): add `dup3` syscalls
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

---
+++ test/modern_bpf/test_suites/syscall_exit_suite/dup3_x.cpp
@@ -0,0 +1,59 @@
+#include "../../event_class/event_class.h"
+
+#if defined(__NR_dup3) && defined(__NR_openat)
+TEST(SyscallExit, dup3X)
+{
+	auto evt_test = new event_test(__NR_dup3, EXIT_EVENT);
+
+	evt_test->enable_capture();
+
+	/*=============================== TRIGGER SYSCALL ===========================*/
+
+	int32_t old_fd = syscall(__NR_openat, AT_FDCWD, ".", O_RDWR | O_TMPFILE, 0);
+	assert_syscall_state(SYSCALL_SUCCESS, "openat", old_fd, NOT_EQUAL, -1);
+
+	/* If `oldfd` equals `newfd`, then dup3() fails with the error `EINVAL`. */
+	int32_t new_fd = old_fd;
+	uint32_t flags = O_CLOEXEC;
+	int32_t res = syscall(__NR_dup3, old_fd, new_fd, flags);
+	assert_syscall_state(SYSCALL_FAILURE, "dup3", res);
+	int64_t errno_value = -errno;
+
+	close(old_fd);
+	close(new_fd);
+	close(res);
+
+	/*=============================== TRIGGER SYSCALL ===========================*/
+
+	evt_test->disable_capture();
+
+	evt_test->assert_event_presence();
+
+	if(HasFatalFailure())
+	{
+		return;
+	}
+
+	evt_test->parse_event();
+
+	evt_test->assert_header();
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	/* Parameter 1: res (type: PT_FD) */
+	evt_test->assert_numeric_param(1, (int64_t)errno_value);
+
+	/* Parameter 2: oldfd (type: PT_FD) */
+	evt_test->assert_numeric_param(2, (int64_t)old_fd);
+
+	/* Parameter 3: newfd (type: PT_FD) */
+	evt_test->assert_numeric_param(3, (int64_t)new_fd);
+
+	/* Parameter 4: flags (type: PT_FLAGS32) */
+	evt_test->assert_numeric_param(4, (uint32_t)PPM_O_CLOEXEC);
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	evt_test->assert_num_params_pushed(4);
+}
+#endif
