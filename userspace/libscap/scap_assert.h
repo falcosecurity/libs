4ca7c85303001ee3b9905b775663d1b1225e8b38
Jason Dellaluce <jasondellaluce@gmail.com>
2022-08-01 17:37:48 +0000

    refactor(userspace/libscap): isolate assert definitions
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

---
+++ userspace/libscap/scap_assert.h
@@ -0,0 +1,34 @@
+/*
+Copyright (C) 2021 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+#pragma once
+
+#ifdef _WIN32
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#endif
+#include <assert.h>
+
+#ifdef ASSERT
+#undef ASSERT
+#endif // ASSERT
+#ifdef _DEBUG
+#define ASSERT(X) assert(X)
+#else // _DEBUG
+#define ASSERT(X)
+#endif // _DEBUG
