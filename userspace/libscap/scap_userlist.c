b89969e2b99dc31c969a3801a070400b0abcdaf6
Angelo Puglisi <angelopuglisi86@gmail.com>
2022-08-04 13:07:30 +0200

    new(userspace): consider scap host root for passwd and group lookup
    Take users and groups considering scap host root.
    When no host root is set, fall back using `getpwent`, `getgrent`, `getpwuid`, and `getgrgid`.
    
    This way, when running on the host we will preserve the lookup with NSS,
    when running as a container we will show the host users and groups
    instead of the container ones (from files).
    
    This applies to scap_create_userlist and sinsp_usergroup_manager
    
    Signed-off-by: Angelo Puglisi <angelopuglisi86@gmail.com>

--- userspace/libscap/scap_userlist.c
+++ userspace/libscap/scap_userlist.c
@@ -31,6 +31,9 @@ limitations under the License.
 //
 int32_t scap_create_userlist(scap_t* handle)
 {
+	bool file_lookup = false;
+	FILE *f = NULL;
+	char filename[SCAP_MAX_PATH_SIZE];
 	uint32_t usercnt, useridx;
 	uint32_t grpcnt, grpidx;
 	struct passwd *p;
@@ -76,11 +79,38 @@ int32_t scap_create_userlist(scap_t* handle)
 		return SCAP_FAILURE;		
 	}
 
-	// users
-	setpwent();
-	p = getpwent();
+	// check for host root
+	const char *host_root = scap_get_host_root();
+	if(host_root[0] == '\0')
+	{
+		file_lookup = false;
+	}
+	else
+	{
+		file_lookup = true;
+	}
 
-	for(useridx = 0; p; p = getpwent(), useridx++)
+	// users
+	if(file_lookup)
+	{
+		snprintf(filename, sizeof(filename), "%s/etc/passwd", host_root);
+		f = fopen(filename, "r");
+		if(f == NULL)
+		{
+			// if we don't have it inside the host root, we'll proceed without a list
+			free(handle->m_userlist->users);
+			free(handle->m_userlist->groups);
+			free(handle->m_userlist);
+			handle->m_userlist = NULL;
+			return SCAP_SUCCESS;
+		}
+	}
+	else
+	{
+		setpwent();
+	}
+
+	for(useridx = 0; file_lookup ? (p = fgetpwent(f)) : (p = getpwent()); useridx++)
 	{
 		if (useridx == usercnt)
 		{
@@ -96,6 +126,14 @@ int32_t scap_create_userlist(scap_t* handle)
 				free(handle->m_userlist->users);
 				free(handle->m_userlist->groups);
 				free(handle->m_userlist);
+				if(file_lookup)
+				{
+					fclose(f);
+				}
+				else
+				{
+					endpwent();
+				}
 				return SCAP_FAILURE;
 			}
 		}
@@ -140,7 +178,15 @@ int32_t scap_create_userlist(scap_t* handle)
 			strlen(user->shell) + 2;
 	}
 
-	endpwent();
+	if(file_lookup)
+	{
+		fclose(f);
+	}
+	else
+	{
+		endpwent();
+	}
+
 	handle->m_userlist->nusers = useridx;
 	if (useridx < usercnt)
 	{
@@ -149,10 +195,26 @@ int32_t scap_create_userlist(scap_t* handle)
 	}
 
 	// groups
-	setgrent();
-	g = getgrent();
+	if(file_lookup)
+	{
+		snprintf(filename, sizeof(filename), "%s/etc/group", host_root);
+		f = fopen(filename, "r");
+		if(f == NULL)
+		{
+			// if we reached this point we had passwd but we don't have group
+			snprintf(handle->m_lasterr, SCAP_LASTERR_SIZE, "failed to open %s", filename);
+			free(handle->m_userlist->users);
+			free(handle->m_userlist->groups);
+			free(handle->m_userlist);
+			return SCAP_FAILURE;
+		}
+	}
+	else
+	{
+		setgrent();
+	}
 
-	for(grpidx = 0; g; g = getgrent(), grpidx++)
+	for(grpidx = 0; file_lookup ? (g = fgetgrent(f)) : (g = getgrent()); grpidx++)
 	{
 		if (grpidx == grpcnt)
 		{
@@ -168,6 +230,14 @@ int32_t scap_create_userlist(scap_t* handle)
 				free(handle->m_userlist->users);
 				free(handle->m_userlist->groups);
 				free(handle->m_userlist);
+				if(file_lookup)
+				{
+					fclose(f);
+				}
+				else
+				{
+					endgrent();
+				}
 				return SCAP_FAILURE;
 			}
 		}
@@ -189,7 +259,15 @@ int32_t scap_create_userlist(scap_t* handle)
 			strlen(group->name) + 2;
 	}
 
-	endgrent();
+	if(file_lookup)
+	{
+		fclose(f);
+	}
+	else
+	{
+		endgrent();
+	}
+
 	handle->m_userlist->ngroups = grpidx;
 	if (grpidx < grpcnt)
 	{
