e0eb7514c6e67763af0b27f2b5fb37f7bf97bfce
Andrea Terzolo <andrea.terzolo@polito.it>
2022-08-12 16:21:55 +0200

    new(modern_bpf): add `flock` syscall
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- userspace/libpman/src/events_prog_names.h
+++ userspace/libpman/src/events_prog_names.h
@@ -117,6 +117,8 @@ static const char* event_prog_names[PPM_EVENT_MAX] = {
 	[PPME_SYSCALL_PIPE_X] = "pipe_x",
 	[PPME_SYSCALL_BPF_2_E] = "bpf_e",
 	[PPME_SYSCALL_BPF_2_X] = "bpf_x",
+	[PPME_SYSCALL_FLOCK_E] = "flock_e",
+	[PPME_SYSCALL_FLOCK_X] = "flock_x",
 };
 
 /* Some events can require more than one bpf program to collect all the data. */
