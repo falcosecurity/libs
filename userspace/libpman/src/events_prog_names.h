b73437ff29047f9d6e23705a602410ee295b782c
Andrea Terzolo <andrea.terzolo@polito.it>
2022-08-12 19:32:14 +0200

    new(modern_bpf): add `mount` syscall
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- userspace/libpman/src/events_prog_names.h
+++ userspace/libpman/src/events_prog_names.h
@@ -125,6 +125,8 @@ static const char* event_prog_names[PPM_EVENT_MAX] = {
 	[PPME_SYSCALL_QUOTACTL_X] = "quotactl_x",
 	[PPME_SYSCALL_UNSHARE_E] = "unshare_e",
 	[PPME_SYSCALL_UNSHARE_X] = "unshare_x",
+	[PPME_SYSCALL_MOUNT_E] = "mount_e",
+	[PPME_SYSCALL_MOUNT_X] = "mount_x",
 };
 
 /* Some events can require more than one bpf program to collect all the data. */
