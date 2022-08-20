abb830202f385b6a190e1149587bbd176d392190
Andrea Terzolo <andrea.terzolo@polito.it>
2022-08-20 16:03:53 +0000

    new(modern_bpf): add `clone3` syscall
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

---
+++ test/modern_bpf/test_suites/syscall_enter_suite/clone3_e.cpp
@@ -0,0 +1,43 @@
+#include "../../event_class/event_class.h"
+
+#ifdef __NR_clone3
+
+#include <linux/sched.h>
+
+TEST(SyscallEnter, clone3E)
+{
+	auto evt_test = new event_test(__NR_clone3, ENTER_EVENT);
+
+	evt_test->enable_capture();
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	/* flags are invalid so the syscall will fail. */
+	struct clone_args cl_args = {0};
+	cl_args.flags = (unsigned long)-1;
+	assert_syscall_state(SYSCALL_FAILURE, "clone3", syscall(__NR_clone3, &cl_args, sizeof(cl_args)));
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	evt_test->disable_capture();
+
+	evt_test->assert_event_presence();
+
+	if(HasFatalFailure())
+	{
+		return;
+	}
+
+	evt_test->parse_event();
+
+	evt_test->assert_header();
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	// Here we have no parameters to assert.
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	evt_test->assert_num_params_pushed(0);
+}
+#endif
--- userspace/libpman/src/events_prog_names.h
+++ userspace/libpman/src/events_prog_names.h
@@ -101,6 +101,8 @@ static const char* event_prog_names[PPM_EVENT_MAX] = {
 	[PPME_SYSCALL_EXECVEAT_X] = "execveat_x",
 	[PPME_SYSCALL_CLONE_20_E] = "clone_e",
 	[PPME_SYSCALL_CLONE_20_X] = "clone_x",
+	[PPME_SYSCALL_CLONE3_E] = "clone3_e",
+	[PPME_SYSCALL_CLONE3_X] = "clone3_x",
 };
 
 /* Some events can require more than one bpf program to collect all the data. */
@@ -108,4 +110,5 @@ static const char* extra_event_prog_names[TAIL_EXTRA_EVENT_PROG_MAX] = {
 	[T1_EXECVE_X] = "t1_execve_x",
 	[T1_EXECVEAT_X] = "t1_execveat_x",
 	[T1_CLONE_X] = "t1_clone_x",
+	[T1_CLONE3_X] = "t1_clone3_x",
 };
