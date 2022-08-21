0f92414f2d2eb29096f2ef0ad5d0bbc01e342e3a
Andrea Terzolo <andrea.terzolo@polito.it>
2022-08-21 22:27:09 +0000

    new(modern_bpf): add `prlimit64` syscall
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- userspace/libpman/src/events_prog_names.h
+++ userspace/libpman/src/events_prog_names.h
@@ -158,6 +158,8 @@ static const char* event_prog_names[PPM_EVENT_MAX] = {
 	[PPME_SYSCALL_SETSID_X] = "setsid_x",
 	[PPME_SYSCALL_SETRLIMIT_E] = "setrlimit_e",
 	[PPME_SYSCALL_SETRLIMIT_X] = "setrlimit_x",
+	[PPME_SYSCALL_PRLIMIT_E] = "prlimit64_e",
+	[PPME_SYSCALL_PRLIMIT_X] = "prlimit64_x",
 };
 
 /* Some events can require more than one bpf program to collect all the data. */
