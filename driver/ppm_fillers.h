9dcd4204a0883195de804b17015657c6a2dd8627
Federico Di Pierro <nierro92@gmail.com>
2022-09-13 09:59:09 +0200

    chore(driver): added new filler to fillers_table.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- driver/ppm_fillers.h
+++ driver/ppm_fillers.h
@@ -132,7 +132,8 @@ or GPL2.txt for full copies of the license.
 	FN(sched_prog_fork)			\
 	FN(sched_prog_fork_2)		\
 	FN(sched_prog_fork_3)		\
-	FN(sys_mlock2_x)			\
+	FN(sys_mlock2_x)                \
+        FN(sys_fsconfig_x)               \
 	FN(terminate_filler)
 
 #define FILLER_ENUM_FN(x) PPM_FILLER_##x,
