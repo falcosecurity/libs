df8cf0c44a2c004a47cb65a0975a8aee2e4fd698
Luca Guerra <luca@guerra.sh>
2022-09-22 15:02:24 +0000

    update(tests): add ipv6 connection, client/server field test
    Signed-off-by: Luca Guerra <luca@guerra.sh>

--- userspace/libsinsp/test/sinsp.ut.cpp
+++ userspace/libsinsp/test/sinsp.ut.cpp
@@ -720,13 +720,16 @@ TEST_F(sinsp_with_test_input, bind_listen_accept_ipv4)
 	serv.sin_port = htons(8080);
 	inet_aton("0.0.0.0", &serv.sin_addr);
 
+	const char* local_server = "192.168.0.2";
+	const char* remote_client = "192.168.0.3";
+
 	dest.sin_family = AF_INET;
 	dest.sin_port = htons(8080);
-	inet_aton("127.0.0.1", &dest.sin_addr);
+	inet_aton(local_server, &dest.sin_addr);
 
 	src.sin_family = AF_INET;
 	src.sin_port = htons(40556);
-	inet_aton("127.0.0.1", &src.sin_addr);
+	inet_aton(remote_client, &src.sin_addr);
 
 	std::vector<uint8_t> sa = test_utils::pack_sockaddr(reinterpret_cast<sockaddr*>(&serv));
 
@@ -744,5 +747,68 @@ TEST_F(sinsp_with_test_input, bind_listen_accept_ipv4)
 	add_event_advance_ts(increasing_ts(), 1, PPME_SOCKET_ACCEPT_5_E, 0);
 	add_event_advance_ts(increasing_ts(), 1, PPME_SOCKET_ACCEPT_5_X, 5, 4, scap_const_sized_buffer{st.data(), st.size()}, 0, 0, 5);
 
-	ASSERT_EQ(get_field_as_string(evt, "fd.name"), "127.0.0.1:40556->127.0.0.1:8080");
+	ASSERT_EQ(get_field_as_string(evt, "fd.name"), "192.168.0.3:40556->192.168.0.2:8080");
+	ASSERT_EQ(get_field_as_string(evt, "fd.sip"), local_server);
+	ASSERT_EQ(get_field_as_string(evt, "fd.cip"), remote_client);
+	ASSERT_EQ(get_field_as_string(evt, "fd.rip"), remote_client);
+	ASSERT_EQ(get_field_as_string(evt, "fd.lip"), local_server);
+	ASSERT_EQ(get_field_as_string(evt, "fd.cport"), "40556");
+	ASSERT_EQ(get_field_as_string(evt, "fd.sport"), "8080");
+	ASSERT_EQ(get_field_as_string(evt, "fd.lport"), "8080");
+	ASSERT_EQ(get_field_as_string(evt, "fd.rport"), "40556");
+}
+
+// test a basic server connection with ipv6
+TEST_F(sinsp_with_test_input, bind_listen_accept_ipv6)
+{
+	add_default_init_thread();
+	open_inspector();
+	sinsp_evt* evt;
+
+	add_event_advance_ts(increasing_ts(), 1, PPME_SOCKET_SOCKET_E, 3, PPM_AF_INET6, SOCK_STREAM, 0);
+	add_event_advance_ts(increasing_ts(), 1, PPME_SOCKET_SOCKET_X, 1, 3);
+
+	sockaddr_in6 src, dest, serv;
+	serv.sin6_family = AF_INET6;
+	serv.sin6_port = htons(8080);
+	inet_pton(AF_INET6, "::", &serv.sin6_addr);
+
+	const char* local_server = "2001:db8:3333:4444:5555:6666:7777:8888";
+	const char* remote_client = "2001:db8:3333:4444:5555:6666:7777:1111";
+
+	dest.sin6_family = AF_INET6;
+	dest.sin6_port = htons(8080);
+	inet_pton(AF_INET6, local_server, &dest.sin6_addr);
+
+	src.sin6_family = AF_INET6;
+	src.sin6_port = htons(40556);
+	inet_pton(AF_INET6, remote_client, &src.sin6_addr);
+
+	std::vector<uint8_t> sa = test_utils::pack_sockaddr(reinterpret_cast<sockaddr*>(&serv));
+
+	add_event_advance_ts(increasing_ts(), 1, PPME_SOCKET_BIND_E, 1, 3);
+
+	evt = add_event_advance_ts(increasing_ts(), 1, PPME_SOCKET_BIND_X, 2, 0, scap_const_sized_buffer{sa.data(), sa.size()});
+	ASSERT_EQ(get_field_as_string(evt, "fd.name"), ":::8080");
+	ASSERT_EQ(get_field_as_string(evt, "fd.is_server"), "true");
+
+	add_event_advance_ts(increasing_ts(), 1, PPME_SOCKET_LISTEN_E, 2, 3, 5);
+	add_event_advance_ts(increasing_ts(), 1, PPME_SOCKET_LISTEN_X, 1, 0);
+
+	std::vector<uint8_t> st = test_utils::pack_socktuple(reinterpret_cast<sockaddr*>(&src), reinterpret_cast<sockaddr*>(&dest));
+
+	add_event_advance_ts(increasing_ts(), 1, PPME_SOCKET_ACCEPT_5_E, 0);
+	add_event_advance_ts(increasing_ts(), 1, PPME_SOCKET_ACCEPT_5_X, 5, 4, scap_const_sized_buffer{st.data(), st.size()}, 0, 0, 5);
+
+	std::string fdname = std::string(remote_client) + ":40556->" + std::string(local_server) + ":8080";
+
+	ASSERT_EQ(get_field_as_string(evt, "fd.name"), fdname);
+	ASSERT_EQ(get_field_as_string(evt, "fd.sip"), local_server);
+	ASSERT_EQ(get_field_as_string(evt, "fd.cip"), remote_client);
+	ASSERT_EQ(get_field_as_string(evt, "fd.rip"), remote_client);
+	ASSERT_EQ(get_field_as_string(evt, "fd.lip"), local_server);
+	ASSERT_EQ(get_field_as_string(evt, "fd.cport"), "40556");
+	ASSERT_EQ(get_field_as_string(evt, "fd.sport"), "8080");
+	ASSERT_EQ(get_field_as_string(evt, "fd.lport"), "8080");
+	ASSERT_EQ(get_field_as_string(evt, "fd.rport"), "40556");
 }
