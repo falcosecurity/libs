d28a68626c77c6a906e86043a2893e0adee58f03
Luca Guerra <luca@guerra.sh>
2022-09-28 17:05:44 +0000

    update(libsinsp/tests): add test for <NA> PID
    Signed-off-by: Luca Guerra <luca@guerra.sh>

--- userspace/libsinsp/test/sinsp_with_test_input.h
+++ userspace/libsinsp/test/sinsp_with_test_input.h
@@ -244,6 +244,14 @@ protected:
 		return ret;
 	}
 
+	bool field_exists(sinsp_evt *evt, const std::string& field_name)
+	{
+		std::unique_ptr<sinsp_filter_check> chk(g_filterlist.new_filter_check_from_fldname(field_name, &m_inspector, false));
+		chk->parse_field_name(field_name.c_str(), true, false);
+		std::vector<extract_value_t> values;
+		return chk->extract(evt, values);
+	}
+
 	std::string get_field_as_string(sinsp_evt *evt, const std::string& field_name)
 	{
 		std::unique_ptr<sinsp_filter_check> chk(g_filterlist.new_filter_check_from_fldname(field_name, &m_inspector, false));
