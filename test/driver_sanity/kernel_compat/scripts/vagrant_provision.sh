b22f6dff80f68376917176048ac431e88b554df6
Melissa Kilby <melissa.kilby.oss@gmail.com>
2022-11-04 18:31:10 -0700

    new(test): driver_sanity - add helper scripts run in VM
    Signed-off-by: Melissa Kilby <melissa.kilby.oss@gmail.com>

---
+++ test/driver_sanity/kernel_compat/scripts/vagrant_provision.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+if [[ "${EUID}" -ne 0 ]]; then
+  echo "Usage: sudo bash vagrant_provision.sh"
+  echo "Run as root user"
+  exit 1
+fi
+
+UNAME_R=$(uname -r);
+echo "Running vagrant_provision.sh in VM";
+
+if [[ "${UNAME_R}" == *"el7"* ]]; then
+    yum install -y /home/vagrant/kernels/*.el7.elrepo.x86_64.rpm;
+    sed -i "s/GRUB_DEFAULT=.*/GRUB_DEFAULT=saved/" /etc/default/grub;
+    ID=$(awk -F\' '$1=="menuentry " {print i++ " : " $2}' /etc/grub2.cfg | grep -v "3.10" | grep -v rescue | tail -1 | cut -c1-2);
+    grub2-set-default "${ID}";
+    grub2-mkconfig -o /boot/grub2/grub.cfg;
+    cat /boot/grub2/grub.cfg;
+fi
+
+if [[ "${UNAME_R}" == *"generic"* ]]; then
+    apt-get install /home/vagrant/kernels/linux*amd64.deb -y;
+fi
+
+reboot
---
+++ test/driver_sanity/kernel_compat/scripts/vagrant_scap_open_test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [[ $# -ne 1 || "${EUID}" -ne 0 ]]; then
+  echo "Usage: sudo bash vagrant_scap_open_test.sh COMPILER"
+  echo "Run as root user"
+  exit 1
+fi
+
+COMPILER="${1}";
+echo "Test run for compiler/kernel ${COMPILER}/$(uname -r)"
+
+rm -f /tmp/o;
+# background scap-open
+if [[ ${COMPILER} == *"clang"* ]]; then
+  /home/vagrant/scap-open --bpf "/home/vagrant/driver/${COMPILER}/$(uname -r).o" > /tmp/o &
+elif [[ ${COMPILER} == *"gcc"* ]]; then
+  cp -f "/home/vagrant/driver/${COMPILER}/$(uname -r).ko" /home/vagrant/scap.ko;
+  insmod /home/vagrant/scap.ko; lsmod | grep scap;
+  /home/vagrant/scap-open --kmod > /tmp/o &
+  # we don't remove the kernel module as we reboot to recover from possible buggy kmod attempts
+else
+  exit 0
+fi
+
+sleep 3;
+# SIGINT to force STDOUT be captured in file for verification
+pkill -SIGINT scap-open;
+cat /tmp/o;
+sleep 1;
+
+TEST=$(cat /tmp/o | grep 'captured' | sed "s/[^0-9]//g");
+
+if [[ ${TEST} -ge "10" ]]; then
+    exit 0
+else
+    exit 1
+fi
