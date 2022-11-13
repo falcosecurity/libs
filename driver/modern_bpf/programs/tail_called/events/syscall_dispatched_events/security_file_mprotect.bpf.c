582d1d1af1f90c4cdb99a84731c90a415129a1c4
David Windsor <dwindsor@secureworks.com>
2022-11-13 15:33:19 -0500

    Whitespace
    Signed-off-by: David Windsor <dwindsor@secureworks.com>

--- driver/modern_bpf/programs/tail_called/events/syscall_dispatched_events/security_file_mprotect.bpf.c
+++ driver/modern_bpf/programs/tail_called/events/syscall_dispatched_events/security_file_mprotect.bpf.c
@@ -22,57 +22,57 @@ int BPF_PROG(security_file_mprotect_e,
 
 	auxmap__preload_event_header(auxmap, PPME_SYSCALL_SECURITY_FILE_MPROTECT_E);
 
-    /*=============================== COLLECT PARAMETERS  ===========================*/
+	/*=============================== COLLECT PARAMETERS  ===========================*/
 
-    /* Copy struct vm_area_struct */
-    unsigned long vma_pointer = extract__syscall_argument(regs, 1);
-    struct vm_area_struct vma;
-    bpf_probe_read_user((void *)&vma, sizeof(struct vm_area_struct), (void *)vma_pointer);
-
+	/* Copy struct vm_area_struct */
+	unsigned long vma_pointer = extract__syscall_argument(regs, 1);
+	struct vm_area_struct vma;
+	bpf_probe_read_user((void *)&vma, sizeof(struct vm_area_struct), (void *)vma_pointer);
 
 	/* Parameter 1: vm_start (type: PT_UINT64) */
-    auxmap__store_u64_param(auxmap, vma.vm_start);
+	auxmap__store_u64_param(auxmap, vma.vm_start);
 
-    /* Parameter 2: vm_end (type: PT_UINT64) */
-    auxmap__store_u64_param(auxmap, vma.vm_end);
+	/* Parameter 2: vm_end (type: PT_UINT64) */
+	auxmap__store_u64_param(auxmap, vma.vm_end);
 
-    unsigned long start_code = 0;
-    unsigned long end_code = 0;
-    unsigned long start_data = 0;
-    unsigned long end_data = 0;
-    unsigned long start_brk = 0;
-    unsigned long end_brk = 0;
-    unsigned long start_stack = 0;
-    if (vma.vm_mm) {
-        start_code = vma.vm_mm->start_code;
-        end_code = vma.vm_mm->end_code;
-        start_data = vma.vm_mm->start_data;
-        end_data = vma.vm_mm->end_data;
-        start_brk = vma.vm_mm->start_brk;
-        end_brk = vma.vm_mm->brk;
-        start_stack = vma.vm_mm->start_stack;
-    }
+	unsigned long start_code = 0;
+	unsigned long end_code = 0;
+	unsigned long start_data = 0;
+	unsigned long end_data = 0;
+	unsigned long start_brk = 0;
+	unsigned long end_brk = 0;
+	unsigned long start_stack = 0;
+	if(vma.vm_mm)
+	{
+		start_code = vma.vm_mm->start_code;
+		end_code = vma.vm_mm->end_code;
+		start_data = vma.vm_mm->start_data;
+		end_data = vma.vm_mm->end_data;
+		start_brk = vma.vm_mm->start_brk;
+		end_brk = vma.vm_mm->brk;
+		start_stack = vma.vm_mm->start_stack;
+	}
 
-    /* Parameter 3: start_code (type: PT_UINT64) */
-    auxmap__store_u64_param(auxmap, start_code);
+	/* Parameter 3: start_code (type: PT_UINT64) */
+	auxmap__store_u64_param(auxmap, start_code);
 
-    /* Parameter 4: end_code (type: PT_UINT64) */
-    auxmap__store_u64_param(auxmap, end_code)
+	/* Parameter 4: end_code (type: PT_UINT64) */
+	auxmap__store_u64_param(auxmap, end_code)
 
-    /* Parameter 5: start_data (type: PT_UINT64) */
-    auxmap__store_u64_param(auxmap, start_data);
+		/* Parameter 5: start_data (type: PT_UINT64) */
+		auxmap__store_u64_param(auxmap, start_data);
 
-    /* Parameter 6: end_data (type: PT_UINT64) */
-    auxmap__store_u64_param(auxmap, end_data);
+	/* Parameter 6: end_data (type: PT_UINT64) */
+	auxmap__store_u64_param(auxmap, end_data);
 
-    /* Parameter 7: start_brk (type: PT_UINT64) */
-    auxmap__store_u64_param(auxmap, start_brk);
+	/* Parameter 7: start_brk (type: PT_UINT64) */
+	auxmap__store_u64_param(auxmap, start_brk);
 
-    /* Parameter 8: end_brk (type: PT_UINT64) */
-    auxmap__store_u64_param(auxmap, end_brk);
+	/* Parameter 8: end_brk (type: PT_UINT64) */
+	auxmap__store_u64_param(auxmap, end_brk);
 
-    /* Parameter 9: start_stack (type: PT_UINT64) */
-    auxmap__store_u64_param(auxmap, start_stack);
+	/* Parameter 9: start_stack (type: PT_UINT64) */
+	auxmap__store_u64_param(auxmap, start_stack);
 
 	/*=============================== COLLECT PARAMETERS  ===========================*/
 
