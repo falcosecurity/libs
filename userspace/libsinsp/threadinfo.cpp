1036c6de336391806be4b2423c371fe9b04fc3f5
Angelo Puglisi <angelo.puglisi@sysdig.com>
2022-11-25 11:21:59 +0100

    [falcosecurity#734] Fix user and group data for uid/gid 0 being reported as <NA> (#130)
    Port https://github.com/falcosecurity/libs/pull/734
    Also port https://github.com/falcosecurity/libs/commit/b52ca996 to avoid conflict at sync time

--- userspace/libsinsp/threadinfo.cpp
+++ userspace/libsinsp/threadinfo.cpp
@@ -530,9 +530,9 @@ void sinsp_threadinfo::set_user(uint32_t uid)
 	{
 		m_user.uid = uid;
 		m_user.gid = m_group.gid;
-		strcpy(m_user.name, "<NA>");
-		strcpy(m_user.homedir, "<NA>");
-		strcpy(m_user.shell, "<NA>");
+		strlcpy(m_user.name, (uid == 0) ? "root" : "<NA>", sizeof(m_user.name));
+		strlcpy(m_user.homedir, (uid == 0) ? "/root" : "<NA>", sizeof(m_user.homedir));
+		strlcpy(m_user.shell, "<NA>", sizeof(m_user.shell));
 	}
 }
 
@@ -565,7 +565,7 @@ void sinsp_threadinfo::set_group(uint32_t gid)
 	else
 	{
 		m_group.gid = gid;
-		strcpy(m_group.name, "<NA>");
+		strlcpy(m_group.name, (gid == 0) ? "root" : "<NA>", sizeof(m_group.name));
 	}
 	// Force-sync user.gid and group id
 	m_user.gid = m_group.gid;
@@ -588,9 +588,9 @@ void sinsp_threadinfo::set_loginuser(uint32_t loginuid)
 	{
 		m_loginuser.uid = loginuid;
 		m_loginuser.gid = m_group.gid;
-		strcpy(m_loginuser.name, "<NA>");
-		strcpy(m_loginuser.homedir, "<NA>");
-		strcpy(m_loginuser.shell, "<NA>");
+		strlcpy(m_loginuser.name, "<NA>", sizeof(m_loginuser.name));
+		strlcpy(m_loginuser.homedir, "<NA>", sizeof(m_loginuser.homedir));
+		strlcpy(m_loginuser.shell, "<NA>", sizeof(m_loginuser.shell));
 	}
 }
 
