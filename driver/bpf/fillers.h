ca039114590623bb481636193bf382b5be599cb4
Andrea Terzolo <andrea.terzolo@polito.it>
2022-11-26 19:59:34 +0100

    fix: fchmod, fchmodat syscalls
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- driver/bpf/fillers.h
+++ driver/bpf/fillers.h
@@ -5859,43 +5859,28 @@ FILLER(sys_autofill, true)
 
 FILLER(sys_fchmodat_x, true)
 {
-	unsigned long val;
-	int res;
-	long retval;
-	unsigned int mode;
+	/* Parameter 1: res (type: PT_ERRNO) */
+	long retval = bpf_syscall_get_retval(data->ctx);
+	int res = bpf_val_to_ring(data, retval);
+	CHECK_RES(res);
 
-	retval = bpf_syscall_get_retval(data->ctx);
-	res = bpf_val_to_ring(data, retval);
-	if (res != PPM_SUCCESS)
-		return res;
+	/* Parameter 2: dirfd (type: PT_FD) */
+	s32 dirfd = (s32)bpf_syscall_get_argument(data, 0);
+	if(dirfd == AT_FDCWD)
+	{
+		dirfd = PPM_AT_FDCWD;
+	}
+	res = bpf_val_to_ring(data, (s64)dirfd);
+	CHECK_RES(res);
 
-	/*
-	 * dirfd
-	 */
-	val = bpf_syscall_get_argument(data, 0);
-	if ((int)val == AT_FDCWD)
-		val = PPM_AT_FDCWD;
+	/* Parameter 3: filename (type: PT_FSRELPATH) */
+	unsigned long path_pointer = bpf_syscall_get_argument(data, 1);
+	res = bpf_val_to_ring(data, path_pointer);
+	CHECK_RES(res);
 
-	res = bpf_val_to_ring(data, val);
-	if (res != PPM_SUCCESS)
-		return res;
-
-	/*
-	 * filename
-	 */
-	val = bpf_syscall_get_argument(data, 1);
-	res = bpf_val_to_ring(data, val);
-	if (res != PPM_SUCCESS)
-		return res;
-
-	/*
-	 * mode
-	 */
-	mode = bpf_syscall_get_argument(data, 2);
-	mode = chmod_mode_to_scap(mode);
-	res = bpf_val_to_ring(data, mode);
-
-	return res;
+	/* Parameter 4: mode (type: PT_MODE) */
+	unsigned long mode = bpf_syscall_get_argument(data, 2);
+	return bpf_val_to_ring(data, chmod_mode_to_scap(mode));
 }
 
 FILLER(sys_chmod_x, true)
@@ -5928,30 +5913,19 @@ FILLER(sys_chmod_x, true)
 
 FILLER(sys_fchmod_x, true)
 {
-	unsigned long val;
-	int res;
-	long retval;
+	/* Parameter 1: res (type: PT_ERRNO) */
+	long retval = bpf_syscall_get_retval(data->ctx);
+	int res = bpf_val_to_ring(data, retval);
+	CHECK_RES(res);
 
-	retval = bpf_syscall_get_retval(data->ctx);
-	res = bpf_val_to_ring(data, retval);
-	if (res != PPM_SUCCESS)
-		return res;
+	/* Parameter 2: fd (type: PT_FD) */
+	s32 fd = (s32)bpf_syscall_get_argument(data, 0);
+	res = bpf_val_to_ring(data, (s64)fd);
+	CHECK_RES(res);
 
-	/*
-	 * fd
-	 */
-	val = bpf_syscall_get_argument(data, 0);
-	res = bpf_val_to_ring(data, val);
-	if (res != PPM_SUCCESS)
-		return res;
-
-	/*
-	 * mode
-	 */
-	val = bpf_syscall_get_argument(data, 1);
-	res = bpf_val_to_ring(data, val);
-
-	return res;
+	/* Parameter 3: mode (type: PT_MODE) */
+	unsigned long mode = bpf_syscall_get_argument(data, 1);
+	return bpf_val_to_ring(data, chmod_mode_to_scap(mode));
 }
 
 FILLER(sys_copy_file_range_e, true)
--- driver/ppm_fillers.c
+++ driver/ppm_fillers.c
@@ -6636,42 +6636,35 @@ int f_sys_mkdirat_x(struct event_filler_arguments *args)
 
 int f_sys_fchmodat_x(struct event_filler_arguments *args)
 {
-	unsigned long val;
-	int res;
-	int64_t retval;
+	unsigned long val = 0;
+	int res = 0;
+	int64_t retval = 0;
+	s32 dirfd = 0;
 
+	/* Parameter 1: res (type: PT_ERRNO) */
 	retval = (int64_t)syscall_get_return_value(current, args->regs);
 	res = val_to_ring(args, retval, 0, false, 0);
-	if (unlikely(res != PPM_SUCCESS))
-		return res;
+	CHECK_RES(res);
 
-	/*
-	 * dirfd
-	 */
+	/* Parameter 2: dirfd (type: PT_FD) */
 	syscall_get_arguments_deprecated(current, args->regs, 0, 1, &val);
+	dirfd = (s32)val;
+	if(dirfd == AT_FDCWD)
+	{
+		dirfd = PPM_AT_FDCWD;
+	}
+	res = val_to_ring(args, (s64)dirfd, 0, false, 0);
+	CHECK_RES(res);
 
-	if ((int)val == AT_FDCWD)
-		val = PPM_AT_FDCWD;
-
-	res = val_to_ring(args, val, 0, false, 0);
-	if (unlikely(res != PPM_SUCCESS))
-		return res;
-
-	/*
-	 * filename
-	 */
+	/* Parameter 3: filename (type: PT_FSRELPATH) */
 	syscall_get_arguments_deprecated(current, args->regs, 1, 1, &val);
 	res = val_to_ring(args, val, 0, true, 0);
-	if (unlikely(res != PPM_SUCCESS))
-		return res;
+	CHECK_RES(res);
 
-	/*
-	 * mode
-	 */
+	/* Parameter 4: mode (type: PT_MODE) */
 	syscall_get_arguments_deprecated(current, args->regs, 2, 1, &val);
 	res = val_to_ring(args, chmod_mode_to_scap(val), 0, false, 0);
-	if (unlikely(res != PPM_SUCCESS))
-		return res;
+	CHECK_RES(res);
 
 	return add_sentinel(args);
 }
@@ -6708,31 +6701,26 @@ int f_sys_chmod_x(struct event_filler_arguments *args)
 
 int f_sys_fchmod_x(struct event_filler_arguments *args)
 {
-	unsigned long val;
-	int res;
-	int64_t retval;
+	unsigned long val = 0;
+	int res = 0;
+	int64_t retval = 0;
+	s32 fd = 0;
 
+	/* Parameter 1: res (type: PT_ERRNO) */
 	retval = (int64_t)syscall_get_return_value(current, args->regs);
 	res = val_to_ring(args, retval, 0, false, 0);
-	if (unlikely(res != PPM_SUCCESS))
-		return res;
+	CHECK_RES(res);
 
-	/*
-	 * fd
-	 */
+	/* Parameter 2: fd (type: PT_FD) */
 	syscall_get_arguments_deprecated(current, args->regs, 0, 1, &val);
+	fd = (s32)val;
+	res = val_to_ring(args, (s64)fd, 0, false, 0);
+	CHECK_RES(res);
 
-	res = val_to_ring(args, val, 0, false, 0);
-	if (unlikely(res != PPM_SUCCESS))
-		return res;
-
-	/*
-	 * mode
-	 */
+	/* Parameter 3: mode (type: PT_MODE) */
 	syscall_get_arguments_deprecated(current, args->regs, 1, 1, &val);
 	res = val_to_ring(args, chmod_mode_to_scap(val), 0, false, 0);
-	if (unlikely(res != PPM_SUCCESS))
-		return res;
+	CHECK_RES(res);
 
 	return add_sentinel(args);
 }
