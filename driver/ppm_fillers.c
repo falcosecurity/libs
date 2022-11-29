b392ee9b4cb3bc057aa7210acecad5b2a7206d20
Andrea Terzolo <andrea.terzolo@polito.it>
2022-11-29 23:44:32 +0100

    fix: mkdirat exit event
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- driver/ppm_fillers.c
+++ driver/ppm_fillers.c
@@ -6566,42 +6566,35 @@ int f_sys_bpf_x(struct event_filler_arguments *args)
 
 int f_sys_mkdirat_x(struct event_filler_arguments *args)
 {
-	unsigned long val;
-	int res;
-	int64_t retval;
+	unsigned long val = 0;
+	int res = 0;
+	int64_t retval = 0;
+	s32 dirfd = 0;
 
+	/* Parameter 1: res (type: PT_ERRNO) */
 	retval = (int64_t)syscall_get_return_value(current, args->regs);
 	res = val_to_ring(args, retval, 0, false, 0);
-	if (unlikely(res != PPM_SUCCESS))
-		return res;
+	CHECK_RES(res);
 
-	/*
-	 * dirfd
-	 */
+	/* Parameter 2: dirfd (type: PT_FD) */
 	syscall_get_arguments_deprecated(current, args->regs, 0, 1, &val);
+	dirfd = (s32)val;
+	if(dirfd == AT_FDCWD)
+	{
+		dirfd = PPM_AT_FDCWD;
+	}
+	res = val_to_ring(args, (s64)dirfd, 0, false, 0);
+	CHECK_RES(res);
 
-	if ((int)val == AT_FDCWD)
-		val = PPM_AT_FDCWD;
-
-	res = val_to_ring(args, val, 0, false, 0);
-	if (unlikely(res != PPM_SUCCESS))
-		return res;
-
-	/*
-	 * path
-	 */
+	/* Parameter 3: path (type: PT_FSRELPATH) */
 	syscall_get_arguments_deprecated(current, args->regs, 1, 1, &val);
 	res = val_to_ring(args, val, 0, true, 0);
-	if (unlikely(res != PPM_SUCCESS))
-		return res;
+	CHECK_RES(res);
 
-	/*
-	 * mode
-	 */
+	/* Parameter 4: mode (type: PT_UINT32) */
 	syscall_get_arguments_deprecated(current, args->regs, 2, 1, &val);
 	res = val_to_ring(args, val, 0, false, 0);
-	if (unlikely(res != PPM_SUCCESS))
-		return res;
+	CHECK_RES(res);
 
 	return add_sentinel(args);
 }
