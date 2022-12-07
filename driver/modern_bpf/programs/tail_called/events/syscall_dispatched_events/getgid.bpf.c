3418a2c895bafaa0188b0bed182a43766c0921c8
David Windsor <dwindsor@secureworks.com>
2022-12-07 11:59:59 -0500

    Add modern probes for getegid, geteuid, getresuid, getresgid, getgid
    Signed-off-by: David Windsor <dwindsor@secureworks.com>

---
+++ driver/modern_bpf/programs/tail_called/events/syscall_dispatched_events/getgid.bpf.c
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2022 The Falco Authors.
+ *
+ * This file is dual licensed under either the MIT or GPL 2. See MIT.txt
+ * or GPL2.txt for full copies of the license.
+ */
+
+#include <helpers/interfaces/fixed_size_event.h>
+#include <helpers/interfaces/variable_size_event.h>
+
+/*=============================== ENTER EVENT ===========================*/
+
+SEC("tp_btf/sys_enter")
+int BPF_PROG(getgid_e,
+	     struct pt_regs *regs,
+	     long id)
+{
+	struct auxiliary_map *auxmap = auxmap__get();
+	if(!auxmap)
+	{
+		return 0;
+	}
+
+	auxmap__preload_event_header(auxmap, PPME_SYSCALL_GETGID_E);
+
+	/*=============================== COLLECT PARAMETERS  ===========================*/
+
+	/*=============================== COLLECT PARAMETERS  ===========================*/
+
+	auxmap__submit_event(auxmap);
+
+	return 0;
+}
+
+/*=============================== ENTER EVENT ===========================*/
+
+/*=============================== EXIT EVENT ===========================*/
+
+SEC("tp_btf/sys_exit")
+int BPF_PROG(getgid_x,
+	     struct pt_regs *regs,
+	     long ret)
+{
+	struct auxiliary_map *auxmap = auxmap__get();
+	if(!auxmap)
+	{
+		return 0;
+	}
+
+	auxmap__preload_event_header(auxmap, PPME_SYSCALL_GETGID_X);
+
+	/*=============================== COLLECT PARAMETERS  ===========================*/
+
+        /* Parameter 1: res (type: PT_UID) */
+        auxmap__store_u32_param(auxmap, (u32)ret);
+
+	/*=============================== COLLECT PARAMETERS  ===========================*/
+
+	auxmap__submit_event(auxmap);
+
+	return 0;
+}
+
+/*=============================== EXIT EVENT ===========================*/
