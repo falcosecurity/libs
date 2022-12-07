f86748d6cb7eb276b1180590aac7eb6d42fff481
David Windsor <dwindsor@secureworks.com>
2022-12-07 10:57:09 -0500

    Use auxmap API rather than ringbuf
    Signed-off-by: David Windsor <dwindsor@secureworks.com>

--- driver/modern_bpf/programs/tail_called/events/syscall_dispatched_events/getuid.bpf.c
+++ driver/modern_bpf/programs/tail_called/events/syscall_dispatched_events/getuid.bpf.c
@@ -6,6 +6,7 @@
  */
 
 #include <helpers/interfaces/fixed_size_event.h>
+#include <helpers/interfaces/variable_size_event.h>
 
 /*=============================== ENTER EVENT ===========================*/
 
@@ -14,19 +15,19 @@ int BPF_PROG(getuid_e,
 	     struct pt_regs *regs,
 	     long id)
 {
-	struct ringbuf_struct ringbuf;
-	if(!ringbuf__reserve_space(&ringbuf, GETUID_E_SIZE))
+	struct auxiliary_map *auxmap = auxmap__get();
+	if(!auxmap)
 	{
 		return 0;
 	}
 
-	ringbuf__store_event_header(&ringbuf, PPME_SYSCALL_GETUID_E);
+	auxmap__preload_event_header(auxmap, PPME_SYSCALL_GETUID_E);
 
 	/*=============================== COLLECT PARAMETERS  ===========================*/
 
 	/*=============================== COLLECT PARAMETERS  ===========================*/
 
-	ringbuf__submit_event(&ringbuf);
+	auxmap__submit_event(auxmap);
 
 	return 0;
 }
@@ -40,22 +41,22 @@ int BPF_PROG(getuid_x,
 	     struct pt_regs *regs,
 	     long ret)
 {
-	struct ringbuf_struct ringbuf;
-	if(!ringbuf__reserve_space(&ringbuf, GETUID_X_SIZE))
+	struct auxiliary_map *auxmap = auxmap__get();
+	if(!auxmap)
 	{
 		return 0;
 	}
 
-	ringbuf__store_event_header(&ringbuf, PPME_SYSCALL_GETUID_X);
+	auxmap__preload_event_header(auxmap, PPME_SYSCALL_GETUID_X);
 
 	/*=============================== COLLECT PARAMETERS  ===========================*/
 
-	/* Parameter 1: res (type: PT_UID) */
-	ringbuf__store_u32(&ringbuf, (u32)ret);
+        /* Parameter 1: res (type: PT_UID) */
+        auxmap__store_u32_param(auxmap, (u32)ret);
 
 	/*=============================== COLLECT PARAMETERS  ===========================*/
 
-	ringbuf__submit_event(&ringbuf);
+	auxmap__submit_event(auxmap);
 
 	return 0;
 }
