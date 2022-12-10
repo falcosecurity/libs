2b6a65ce621768a48c491f916002fe80a85bf671
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2022-12-11 00:09:48 +0100

    chore(scap): move public savefile functions to scap_savefile_api.h
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

--- userspace/libscap/scap.h
+++ userspace/libscap/scap.h
@@ -18,6 +18,7 @@ limitations under the License.
 #pragma once
 
 #include "scap_const.h"
+#include "scap_savefile_api.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -496,15 +497,6 @@ typedef enum event_direction
 }event_direction;
 
 /*!
-  \brief Indicates the compression type used when writing a tracefile
-*/
-typedef enum compression_mode
-{
-	SCAP_COMPRESSION_NONE = 0,
-	SCAP_COMPRESSION_GZIP = 1
-}compression_mode;
-
-/*!
   \brief Flags for scap_dump
 */
 typedef enum scap_dump_flags
@@ -691,72 +683,6 @@ uint32_t scap_event_get_dump_flags(scap_t* handle);
   \param handle Handle to the capture instance.
 */
 int64_t scap_get_readfile_offset(scap_t* handle);
-
-/*!
-  \brief Open a trace file for writing
-
-  \param handle Handle to the capture instance.
-  \param fname The name of the trace file.
-
-  \return Dump handle that can be used to identify this specific dump instance.
-*/
-scap_dumper_t* scap_dump_open(scap_t *handle, const char *fname, compression_mode compress, bool skip_proc_scan);
-
-/*!
-  \brief Open a trace file for writing, using the provided fd.
-
-  \param handle Handle to the capture instance.
-  \param fd A file descriptor to which the dumper will write
-
-  \return Dump handle that can be used to identify this specific dump instance.
-*/
-scap_dumper_t* scap_dump_open_fd(scap_t *handle, int fd, compression_mode compress, bool skip_proc_scan);
-
-/*!
-  \brief Close a trace file.
-
-  \param d The dump handle, returned by \ref scap_dump_open
-*/
-void scap_dump_close(scap_dumper_t *d);
-
-/*!
-  \brief Return the current size of a trace file.
-
-  \param d The dump handle, returned by \ref scap_dump_open
-  \return The current size of the dump file pointed by d.
-*/
-int64_t scap_dump_get_offset(scap_dumper_t *d);
-
-/*!
-  \brief Return the position for the next write to a trace file.
-         This uses gztell, while scap_dump_get_offset uses gzoffset.
-
-  \param d The dump handle, returned by \ref scap_dump_open
-  \return The next write position.
-*/
-int64_t scap_dump_ftell(scap_dumper_t *d);
-
-/*!
-  \brief Flush all pending output into the file.
-
-  \param d The dump handle, returned by \ref scap_dump_open
-*/
-void scap_dump_flush(scap_dumper_t *d);
-
-/*!
-  \brief Write an event to a trace file
-
-  \param handle Handle to the capture instance.
-  \param d The dump handle, returned by \ref scap_dump_open
-  \param e pointer to an event returned by \ref scap_next.
-  \param cpuid The cpu from which the event was captured. Returned by \ref scap_next.
-  \param flags The event flags. 0 means no flags.
-
-  \return SCAP_SUCCESS if the call is successful.
-   On Failure, SCAP_FAILURE is returned and scap_getlasterr() can be used to obtain
-   the cause of the error.
-*/
-int32_t scap_dump(scap_t *handle, scap_dumper_t *d, scap_evt* e, uint16_t cpuid, uint32_t flags);
 
 /*!
   \brief Get the process list for the given capture instance
@@ -1065,12 +991,6 @@ void scap_fseek(scap_t *handle, uint64_t off);
 int32_t scap_enable_tracers_capture(scap_t* handle);
 int32_t scap_proc_add(scap_t* handle, uint64_t tid, scap_threadinfo* tinfo);
 int32_t scap_fd_add(scap_t *handle, scap_threadinfo* tinfo, uint64_t fd, scap_fdinfo* fdinfo);
-scap_dumper_t *scap_memory_dump_open(scap_t *handle, uint8_t* targetbuf, uint64_t targetbufsize);
-scap_dumper_t *scap_managedbuf_dump_create(scap_t *handle);
-uint8_t* scap_get_memorydumper_curpos(scap_dumper_t *d);
-int32_t scap_write_proc_fds(scap_t *handle, struct scap_threadinfo *tinfo, scap_dumper_t *d);
-scap_dumper_t* scap_write_proclist_begin(scap_t* handle);
-int scap_write_proclist_end(scap_t *handle, scap_dumper_t *d, scap_dumper_t *proclist_dumper, uint32_t totlen);
 // Variant of scap_write_proclist_entry where array-backed information
 // about the thread is provided separate from the scap_threadinfo
 // struct.
