a2367687d3e57286a57e0624d1c8770bc4dabada
David Windsor <dwindsor@secureworks.com>
2022-12-16 13:19:00 -0500

    Add munmap
    Signed-off-by: David Windsor <dwindsor@secureworks.com>

--- driver/modern_bpf/definitions/events_dimensions.h
+++ driver/modern_bpf/definitions/events_dimensions.h
@@ -25,6 +25,8 @@
 #define MKDIR_E_SIZE HEADER_LEN + sizeof(uint32_t) + PARAM_LEN
 #define MMAP_E_SIZE HEADER_LEN + sizeof(uint64_t) * 4 + sizeof(uint32_t) * 2 + PARAM_LEN * 6
 #define MMAP_X_SIZE HEADER_LEN + sizeof(int64_t) + sizeof(uint32_t) * 3 + PARAM_LEN * 4
+#define MUNMAP_E_SIZE HEADER_LEN + sizeof(uint64_t) * 2 + PARAM_LEN * 2
+#define MUNMAP_X_SIZE HEADER_LEN + sizeof(int64_t) + sizeof(uint32_t) * 3 + PARAM_LEN * 4
 #define OPEN_BY_HANDLE_AT_E_SIZE HEADER_LEN
 #define CLOSE_E_SIZE HEADER_LEN + sizeof(int64_t) + PARAM_LEN
 #define CLOSE_X_SIZE HEADER_LEN + sizeof(int64_t) + PARAM_LEN
