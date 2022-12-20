ef1bcc311400e06d050de4afe27916b49115b08e
David Windsor <dwindsor@secureworks.com>
2022-12-20 16:36:29 -0500

    Add modern probe for mmap
    Signed-off-by: David Windsor <dwindsor@secureworks.com>

--- driver/modern_bpf/definitions/events_dimensions.h
+++ driver/modern_bpf/definitions/events_dimensions.h
@@ -23,6 +23,8 @@
 #define GENERIC_E_SIZE HEADER_LEN + sizeof(uint16_t) * 2 + PARAM_LEN * 2
 #define GENERIC_X_SIZE HEADER_LEN + sizeof(uint16_t) + PARAM_LEN
 #define MKDIR_E_SIZE HEADER_LEN + sizeof(uint32_t) + PARAM_LEN
+#define MMAP_E_SIZE HEADER_LEN + sizeof(uint64_t) * 4 + sizeof(uint32_t) * 2 + PARAM_LEN * 6
+#define MMAP_X_SIZE HEADER_LEN + sizeof(int64_t) + sizeof(uint32_t) * 3 + PARAM_LEN * 4
 #define OPEN_BY_HANDLE_AT_E_SIZE HEADER_LEN
 #define CLOSE_E_SIZE HEADER_LEN + sizeof(int64_t) + PARAM_LEN
 #define CLOSE_X_SIZE HEADER_LEN + sizeof(int64_t) + PARAM_LEN
