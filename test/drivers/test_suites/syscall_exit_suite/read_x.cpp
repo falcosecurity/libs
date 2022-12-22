bf3f875f2eb5d0e402b439659ee3a87fd3e6f146
David Windsor <dwindsor@secureworks.com>
2022-12-22 13:51:54 -0500

    Add test for read(2) failure case
    Signed-off-by: David Windsor <dwindsor@secureworks.com>

--- test/drivers/test_suites/syscall_exit_suite/read_x.cpp
+++ test/drivers/test_suites/syscall_exit_suite/read_x.cpp
@@ -52,5 +52,49 @@ TEST(SyscallExit, readX)
 
 	evt_test->assert_num_params_pushed(2);
 }
+
+TEST(SyscallExit, readXfail)
+{
+	auto evt_test = get_syscall_event_test(__NR_read, EXIT_EVENT);
+
+	evt_test->enable_capture();
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	/* Read data from an invalid fd */
+	const unsigned data_len = 64;
+	char buf[data_len];
+	ssize_t read_bytes = syscall(__NR_read, -1, (void *)buf, data_len);
+	int errno_value = -errno;
+	assert_syscall_state(SYSCALL_FAILURE, "read", read_bytes);
+
+	/*=============================== TRIGGER SYSCALL ===========================*/
+
+	evt_test->disable_capture();
+
+	evt_test->assert_event_presence();
+
+	if(HasFatalFailure())
+	{
+		return;
+	}
+
+	evt_test->parse_event();
+
+	evt_test->assert_header();
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	/* Parameter 1: res (type: PT_ERRNO) */
+	evt_test->assert_numeric_param(1, (int64_t)errno_value);
+
+	/* Parameter 2: data (type: PT_BYTEBUF) */
+	evt_test->assert_empty_param(2);
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	evt_test->assert_num_params_pushed(2);
+}
+
 #endif
 #endif
