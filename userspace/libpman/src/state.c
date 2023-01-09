cea60788cfd7dee0ec2c8d220b4e1e42d7c07c23
Andrea Terzolo <andrea.terzolo@polito.it>
2023-01-09 22:04:59 +0100

    fix: correctly free the state in modern bpf probe
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- userspace/libpman/src/state.c
+++ userspace/libpman/src/state.c
@@ -22,16 +22,21 @@ limitations under the License.
 #include <unistd.h>
 #include "state.h"
 
-struct internal_state g_state;
+struct internal_state g_state = {};
 
 void pman_print_error(const char* error_message)
 {
 	if(!error_message)
 	{
-		fprintf(stderr, "libpman: No specific message available (errno: %d | message: %s)\n", errno, strerror(errno));
+		return;
+	}
+
+	if(errno != 0)
+	{
+		fprintf(stderr, "libpman: %s (errno: %d | message: %s)\n", error_message, errno, strerror(errno));
 	}
 	else
 	{
-		fprintf(stderr, "libpman: %s (errno: %d | message: %s)\n", error_message, errno, strerror(errno));
+		fprintf(stderr, "libpman: %s\n", error_message);
 	}
 }
