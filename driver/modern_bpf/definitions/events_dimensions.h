cbf3f359773cdffcaea3b923ddf9251d73d41759
Andrea Terzolo <andrea.terzolo@polito.it>
2023-02-01 00:42:08 +0100

    new: support `mmap2` syscall
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- driver/modern_bpf/definitions/events_dimensions.h
+++ driver/modern_bpf/definitions/events_dimensions.h
@@ -158,6 +158,8 @@
 #define IO_URING_REGISTER_X_SIZE HEADER_LEN + sizeof(int64_t) * 2 + sizeof(uint16_t) + sizeof(uint64_t) + sizeof(uint32_t) + PARAM_LEN * 5
 #define IO_URING_SETUP_E_SIZE HEADER_LEN
 #define IO_URING_SETUP_X_SIZE HEADER_LEN + sizeof(int64_t) + sizeof(uint32_t) * 7 + PARAM_LEN * 8
+#define MMAP2_E_SIZE HEADER_LEN + sizeof(uint64_t) * 3 + sizeof(int64_t) + sizeof(uint32_t) * 2 + PARAM_LEN * 6
+#define MMAP2_X_SIZE HEADER_LEN + sizeof(int64_t) + sizeof(uint32_t) * 3 + PARAM_LEN * 4
 
 /* Generic tracepoints events. */
 #define PROC_EXIT_SIZE HEADER_LEN + sizeof(int64_t) * 2 + sizeof(uint8_t) * 2 + PARAM_LEN * 4
---
+++ test/drivers/test_suites/syscall_enter_suite/mmap2_e.cpp
@@ -0,0 +1,64 @@
+
+#include "../../event_class/event_class.h"
+
+#ifdef __NR_mmap2
+
+#include <sys/mman.h>
+
+TEST(SyscallEnter, mmap2E)
+{
+	auto evt_test = get_syscall_event_test(__NR_mmap2, ENTER_EVENT);
+
+	evt_test->enable_capture();
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	void *mock_addr = (void *)87;
+	size_t mock_length = 4096;
+	int mock_prot = PROT_EXEC | PROT_READ;
+	int mock_flags = MAP_SHARED;
+	int mock_fd = -1;
+	off_t mock_offset = 1023;
+
+	assert_syscall_state(SYSCALL_FAILURE, "mmap2", syscall(__NR_mmap2, mock_addr, mock_length, mock_prot, mock_flags, mock_fd, mock_offset));
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	evt_test->disable_capture();
+
+	evt_test->assert_event_presence();
+
+	if(HasFatalFailure())
+	{
+		return;
+	}
+
+	evt_test->parse_event();
+
+	evt_test->assert_header();
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	/* Parameter 1: addr (type: PT_UINT64) */
+	evt_test->assert_numeric_param(1, (uint64_t)mock_addr);
+
+	/* Parameter 2: length (type: PT_UINT64) */
+	evt_test->assert_numeric_param(2, (uint64_t)mock_length);
+
+	/* Parameter 3: prot (type: PT_FLAGS32) */
+	evt_test->assert_numeric_param(3, (uint32_t)PPM_PROT_EXEC | PPM_PROT_READ);
+
+	/* Parameter 4: flags (type: PT_FLAGS32) */
+	evt_test->assert_numeric_param(4, (uint32_t)PPM_MAP_SHARED);
+
+	/* Parameter 5: fd (type: PT_FD) */
+	evt_test->assert_numeric_param(5, (int64_t)mock_fd);
+
+	/* Parameter 6: pgoffset (type: PT_UINT64) */
+	evt_test->assert_numeric_param(6, (uint64_t)mock_offset);
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	evt_test->assert_num_params_pushed(6);
+}
+#endif
