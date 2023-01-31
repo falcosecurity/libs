110c1cb2cc52472596c34ef278bcd712130e794c
Melissa Kilby <melissa.kilby.oss@gmail.com>
2023-01-31 21:36:15 +0000

    cleanup(libsinsp): move filter_evttype_resolver to libsinsp
    * move filter_evttype_resolver from falco to libsinsp in order to support sinsp-example e2e tests for configuring interesting sets of syscalls based on ruleset evttypes
    * falco_event_types renamed to sinsp_event_types
    
    Co-authored-by: Jason Dellaluce <jasondellaluce@gmail.com>
    Signed-off-by: Melissa Kilby <melissa.kilby.oss@gmail.com>

---
+++ userspace/libsinsp/filter_evttype_resolver.cpp
@@ -0,0 +1,164 @@
+/*
+Copyright (C) 2023 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include "filter_evttype_resolver.h"
+#include <sinsp.h>
+
+using namespace libsinsp::filter;
+
+extern sinsp_evttables g_infotables;
+
+static bool is_evttype_operator(const std::string& op)
+{
+	return op == "==" || op == "=" || op == "!=" || op == "in";
+}
+
+
+size_t sinsp_event_types::get_ppm_event_max()
+{
+	return PPM_EVENT_MAX;
+}
+
+void filter_evttype_resolver::visitor::inversion(sinsp_event_types& types)
+{
+	sinsp_event_types all_types;
+	evttypes("", all_types);
+	if (types != all_types) // we don't invert the "all types" set
+	{
+		types = all_types.diff(types);
+	}
+}
+
+void filter_evttype_resolver::visitor::evttypes(const std::string& evtname, sinsp_event_types& out)
+{
+	// Fill in from 2 to PPM_EVENT_MAX-1. 0 and 1 are excluded as
+	// those are PPM_GENERIC_E/PPME_GENERIC_X
+	const struct ppm_event_info* etable = g_infotables.m_event_info;
+	for(uint16_t i = 2; i < PPM_EVENT_MAX; i++)
+	{
+		// Skip unused events or events not matching the requested evtname
+		if(!sinsp::is_unused_event(i) && (evtname.empty() || std::string(etable[i].name) == evtname))
+		{
+			out.insert(i);
+		}
+	}
+}
+
+void filter_evttype_resolver::evttypes(
+	libsinsp::filter::ast::expr* filter,
+	std::set<uint16_t>& out) const
+{
+	visitor v;
+	v.m_expect_value = false;
+	v.m_last_node_evttypes.clear();
+	filter->accept(&v);
+	v.m_last_node_evttypes.for_each([&out](uint16_t val){out.insert(val); return true;});
+}
+
+void filter_evttype_resolver::evttypes(
+	std::shared_ptr<libsinsp::filter::ast::expr> filter,
+	std::set<uint16_t>& out) const
+{
+	visitor v;
+	v.m_expect_value = false;
+	v.m_last_node_evttypes.clear();
+	filter.get()->accept(&v);
+	v.m_last_node_evttypes.for_each([&out](uint16_t val){out.insert(val); return true;} );
+}
+
+// "and" nodes evttypes are the intersection of the evttypes of their children.
+// we initialize the set with "all event types"
+void filter_evttype_resolver::visitor::visit(ast::and_expr* e)
+{
+	sinsp_event_types types;
+	evttypes("", types);
+	m_last_node_evttypes.clear();
+	for (auto &c : e->children)
+	{
+		sinsp_event_types inters;
+		c->accept(this);
+		types = types.intersect(m_last_node_evttypes);
+	}
+	m_last_node_evttypes = types;
+}
+
+// "or" nodes evttypes are the union of the evttypes their children
+void filter_evttype_resolver::visitor::visit(ast::or_expr* e)
+{
+	sinsp_event_types types;
+	m_last_node_evttypes.clear();
+	for (auto &c : e->children)
+	{
+		c->accept(this);
+		types.merge(m_last_node_evttypes);
+	}
+	m_last_node_evttypes = types;
+}
+
+void filter_evttype_resolver::visitor::visit(ast::not_expr* e)
+{
+	m_last_node_evttypes.clear();
+	e->child->accept(this);
+	inversion(m_last_node_evttypes);
+}
+
+void filter_evttype_resolver::visitor::visit(ast::binary_check_expr* e)
+{
+	m_last_node_evttypes.clear();
+	if (e->field == "evt.type" && is_evttype_operator(e->op))
+	{
+		m_expect_value = true;
+		e->value->accept(this);
+		m_expect_value = false;
+		if (e->op == "!=")
+		{
+			inversion(m_last_node_evttypes);
+		}
+		return;
+	}
+	evttypes("", m_last_node_evttypes);
+}
+
+void filter_evttype_resolver::visitor::visit(ast::unary_check_expr* e)
+{
+	m_last_node_evttypes.clear();
+	evttypes("", m_last_node_evttypes);
+}
+
+void filter_evttype_resolver::visitor::visit(ast::value_expr* e)
+{
+	m_last_node_evttypes.clear();
+	if (m_expect_value)
+	{
+		evttypes(e->value, m_last_node_evttypes);
+		return;
+	}
+	evttypes("", m_last_node_evttypes);
+}
+
+void filter_evttype_resolver::visitor::visit(ast::list_expr* e)
+{
+	m_last_node_evttypes.clear();
+	if (m_expect_value)
+	{
+		for (auto &v : e->values)
+		{
+			evttypes(v, m_last_node_evttypes);
+		}	
+		return;
+	}
+	evttypes("", m_last_node_evttypes);
+}
---
+++ userspace/libsinsp/filter_evttype_resolver.h
@@ -0,0 +1,210 @@
+/*
+Copyright (C) 2023 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#pragma once
+
+#include <filter/parser.h>
+#include <string>
+#include <set>
+#include <memory>
+#include <functional>
+#include <stdexcept>
+
+class sinsp_event_types
+{
+private:
+	using vec_t = std::vector<uint8_t>;
+	vec_t m_types{};
+
+	static inline void check_range(uint16_t e)
+	{
+	    	static const auto enum_max = get_ppm_event_max();
+		if(e > enum_max)
+		{
+			throw std::range_error("invalid event type");
+		}
+	}
+
+public:
+	sinsp_event_types(sinsp_event_types&&) = default;
+	sinsp_event_types(const sinsp_event_types&) = default;
+	sinsp_event_types& operator=(sinsp_event_types&&) = default;
+	sinsp_event_types& operator=(const sinsp_event_types&) = default;
+
+	static size_t get_ppm_event_max();
+
+	inline sinsp_event_types():
+		m_types(get_ppm_event_max() + 1, 0)
+	{
+	}
+
+	inline void insert(uint16_t e)
+	{
+		check_range(e);
+		m_types[e] = 1;
+	}
+
+	void merge(const sinsp_event_types& other)
+	{
+		for(size_t i = 0; i <= get_ppm_event_max(); ++i)
+		{
+			m_types[i] |= other.m_types[i];
+		}
+	}
+
+	void merge(const std::set<uint16_t>& other)
+	{
+		for(const auto& e : other)
+		{
+			insert(e);
+		}
+	}
+
+	inline bool contains(uint16_t e) const
+	{
+		check_range(e);
+		return m_types[e] != 0;
+	}
+
+	void clear()
+	{
+		for(auto& v : m_types)
+		{
+			v = 0;
+		}
+	}
+
+	bool equals(const sinsp_event_types& other) const
+	{
+		return m_types == other.m_types;
+	}
+
+	sinsp_event_types diff(const sinsp_event_types& other)
+	{
+		sinsp_event_types ret;
+		for(size_t i = 0; i <= get_ppm_event_max(); ++i)
+		{
+			if(m_types[i] == 1 && other.m_types[i] == 0)
+			{
+				ret.m_types[i] = 1;
+			}
+		}
+		return ret;
+	}
+
+	sinsp_event_types intersect(const sinsp_event_types& other)
+	{
+		sinsp_event_types ret;
+		for(size_t i = 0; i <= get_ppm_event_max(); ++i)
+		{
+			if(m_types[i] == 1 && other.m_types[i] == 1)
+			{
+				ret.m_types[i] = 1;
+			}
+		}
+		return ret;
+	}
+
+	void for_each(std::function<bool(uint16_t)> consumer) const
+	{
+		for(uint16_t i = 0; i < m_types.size(); ++i)
+		{
+			if(m_types[i] != 0)
+			{
+				if(!consumer(i))
+				{
+					return;
+				}
+			}
+		}
+	}
+};
+
+inline bool operator==(const sinsp_event_types& lhs, const sinsp_event_types& rhs)
+{
+	return lhs.equals(rhs);
+}
+
+inline bool operator!=(const sinsp_event_types& lhs, const sinsp_event_types& rhs)
+{
+	return !(lhs == rhs);
+}
+
+/*!
+	\brief Helper class for finding event types
+*/
+class filter_evttype_resolver
+{
+public:
+	/*!
+		\brief Collects the evttypes related to the provided event name.
+		The event types are inserted in the set provided as parameter.
+		The set is not cleared before inserting the elements.
+		\param evtname The event name used to search event types. If an empty
+		string is passed, all the available evttypes are collected
+		\param out The set to be filled with the evttypes
+	*/
+	inline void evttypes(const std::string& evtname, sinsp_event_types& out) const
+	{
+		sinsp_event_types evt_types;
+		visitor().evttypes(evtname, evt_types);
+		evt_types.for_each([&out](uint16_t val)
+				   {out.insert(val); return true; });
+	}
+
+	/*!
+		\brief Visits a filter AST and collects all the evttypes for which
+		the filter expression can be evaluated as true. The event types are
+		inserted in the set provided as parameter. The set is not cleared before
+		inserting the elements.
+		\param filter The filter AST to be explored
+		\param out The set to be filled with the evttypes
+	*/
+	void evttypes(
+		libsinsp::filter::ast::expr* filter,
+		std::set<uint16_t>& out) const;
+
+	/*!
+		\brief Overloaded version of evttypes() that supports filters wrapped
+		in shared pointers
+	*/
+	void evttypes(
+		std::shared_ptr<libsinsp::filter::ast::expr> filter,
+		std::set<uint16_t>& out) const;
+
+private:
+	struct visitor : public libsinsp::filter::ast::expr_visitor
+	{
+		visitor(): m_expect_value(false) {}
+		visitor(visitor&&) = default;
+		visitor& operator = (visitor&&) = default;
+		visitor(const visitor&) = default;
+		visitor& operator = (const visitor&) = default;
+
+		bool m_expect_value;
+		sinsp_event_types m_last_node_evttypes;
+
+		void visit(libsinsp::filter::ast::and_expr* e) override;
+		void visit(libsinsp::filter::ast::or_expr* e) override;
+		void visit(libsinsp::filter::ast::not_expr* e) override;
+		void visit(libsinsp::filter::ast::value_expr* e) override;
+		void visit(libsinsp::filter::ast::list_expr* e) override;
+		void visit(libsinsp::filter::ast::unary_check_expr* e) override;
+		void visit(libsinsp::filter::ast::binary_check_expr* e) override;
+		void inversion(sinsp_event_types& types);
+		void evttypes(const std::string& evtname, sinsp_event_types& out);
+	};
+};
