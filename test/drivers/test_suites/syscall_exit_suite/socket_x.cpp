bc36a04e6823b29173367e5113a2836fe3df821d
Andrea Terzolo <andrea.terzolo@polito.it>
2023-02-09 00:34:52 +0100

    update(tests): correct socket tests
    Perform socket call tests in child process to not interfere
    with scap socket calibration.
    
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>
    Co-authored-by: Hendrik Brueckner <brueckner@de.ibm.com>

--- test/drivers/test_suites/syscall_exit_suite/socket_x.cpp
+++ test/drivers/test_suites/syscall_exit_suite/socket_x.cpp
@@ -1,7 +1,8 @@
 #include "../../event_class/event_class.h"
 
-#if defined(__NR_socket) && defined(__NR_close)
+#if defined(__NR_socket) && defined(__NR_clone3) && defined(__NR_wait4)
 
+#include <linux/sched.h>
 #include <sys/socket.h>
 
 TEST(SyscallExit, socketX)
@@ -12,18 +13,50 @@ TEST(SyscallExit, socketX)
 
 	/*=============================== TRIGGER SYSCALL  ===========================*/
 
-	int domain = AF_INET;
-	int type = SOCK_RAW;
-	int protocol = PF_INET;
-	int32_t socket_fd = syscall(__NR_socket, domain, type, protocol);
-	assert_syscall_state(SYSCALL_SUCCESS, "socket_fd", socket_fd, NOT_EQUAL, -1);
-	syscall(__NR_close, socket_fd);
+	int domain = -1;
+	int type = -1;
+	int protocol = -1;
+
+	/* Here we need to call the `socket` from a child because the main process throws a `socket`
+	 * syscall to calibrate the socket file options if we are using the bpf probe.
+	 */
+	struct clone_args cl_args = {0};
+	cl_args.flags = CLONE_FILES;
+	cl_args.exit_signal = SIGCHLD;
+	pid_t ret_pid = syscall(__NR_clone3, &cl_args, sizeof(cl_args));
+
+	if(ret_pid == 0)
+	{
+		/* In this way in the father we know if the call was successful or not. */
+		if(syscall(__NR_socket, domain, type, protocol) == -1)
+		{
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	assert_syscall_state(SYSCALL_SUCCESS, "clone3", ret_pid, NOT_EQUAL, -1);
+	/* Catch the child before doing anything else. */
+	int status = 0;
+	int options = 0;
+	assert_syscall_state(SYSCALL_SUCCESS, "wait4", syscall(__NR_wait4, ret_pid, &status, options, NULL), NOT_EQUAL, -1);
+
+	if(__WEXITSTATUS(status) == EXIT_FAILURE || __WIFSIGNALED(status) != 0)
+	{
+		FAIL() << "The socket call is successful while it should fail..." << std::endl;
+	}
+
+	/* This is the errno value we expect from the `socket` call. */
+	int64_t errno_value = -EINVAL;
 
 	/*=============================== TRIGGER SYSCALL ===========================*/
 
 	evt_test->disable_capture();
 
-	evt_test->assert_event_presence();
+	evt_test->assert_event_presence(ret_pid);
 
 	if(HasFatalFailure())
 	{
@@ -37,7 +70,7 @@ TEST(SyscallExit, socketX)
 	/*=============================== ASSERT PARAMETERS  ===========================*/
 
 	/* Parameter 1: res (type: PT_ERRNO)*/
-	evt_test->assert_numeric_param(1, (int64_t)socket_fd);
+	evt_test->assert_numeric_param(1, (int64_t)errno_value);
 
 	/*=============================== ASSERT PARAMETERS  ===========================*/
 
