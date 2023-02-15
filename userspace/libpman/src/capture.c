c7632b94bbce7e740744ca7284890b2b3280ec06
Andrea Terzolo <andrea.terzolo@polito.it>
2023-02-15 17:41:57 +0100

    cleanup: remove unused API in modern bpf probe
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- userspace/libpman/src/capture.c
+++ userspace/libpman/src/capture.c
@@ -48,47 +48,6 @@ int pman_disable_capture()
 	return 0;
 }
 
-#ifdef TEST_HELPERS
-
-/* Not used right now */
-int pman_print_stats()
-{
-	char error_message[MAX_ERROR_MESSAGE_LEN];
-	long overall_n_drops_buffer = 0;
-	long overall_n_drops_max_event_size = 0;
-	long overall_n_evts = 0;
-	struct counter_map cnt_map;
-
-	int counter_maps_fd = bpf_map__fd(g_state.skel->maps.counter_maps);
-	if(counter_maps_fd <= 0)
-	{
-		pman_print_error("unable to get counter maps");
-		return errno;
-	}
-
-	for(int index = 0; index < g_state.n_possible_cpus; index++)
-	{
-		if(bpf_map_lookup_elem(counter_maps_fd, &index, &cnt_map) < 0)
-		{
-			snprintf(error_message, MAX_ERROR_MESSAGE_LEN, "unbale to get the counter map for CPU %d", index);
-			pman_print_error((const char *)error_message);
-			goto clean_print_stats;
-		}
-		overall_n_evts += cnt_map.n_evts;
-		overall_n_drops_buffer += cnt_map.n_drops_buffer;
-		overall_n_drops_max_event_size += cnt_map.n_drops_max_event_size;
-	}
-	printf("\noverall_n_evts: %ld\n", overall_n_evts);
-	printf("overall_n_drops_max_event_size: %ld\n", overall_n_drops_max_event_size);
-	printf("overall_n_drops_buffer: %ld\n", overall_n_drops_buffer);
-	return 0;
-
-clean_print_stats:
-	close(counter_maps_fd);
-	return errno;
-}
-#endif
-
 int pman_get_scap_stats(void *scap_stats_struct)
 {
 	struct scap_stats *stats = (struct scap_stats *)scap_stats_struct;
