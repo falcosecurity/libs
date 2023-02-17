f09a55e88316df21d60c6f6c0273189e5261634c
David Windsor <dwindsor@secureworks.com>
2023-02-16 22:19:04 -0500

    Add modern probe for write
    Signed-off-by: David Windsor <dwindsor@secureworks.com>

--- driver/modern_bpf/definitions/events_dimensions.h
+++ driver/modern_bpf/definitions/events_dimensions.h
@@ -172,6 +172,7 @@
 #define SEMOP_X_SIZE HEADER_LEN + sizeof(int64_t) + sizeof(uint32_t) + sizeof(uint16_t) * 4 + sizeof(int16_t) * 2 + PARAM_LEN * 8
 #define SENDFILE_E_SIZE HEADER_LEN + sizeof(int64_t) * 2 + sizeof(uint64_t) * 2 + PARAM_LEN * 4
 #define SENDFILE_X_SIZE HEADER_LEN + sizeof(int64_t) + sizeof(uint64_t) + PARAM_LEN * 2
+#define WRITE_E_SIZE HEADER_LEN + sizeof(int64_t) + sizeof(uint32_t) + PARAM_LEN * 2
 
 /* Generic tracepoints events. */
 #define PROC_EXIT_SIZE HEADER_LEN + sizeof(int64_t) * 2 + sizeof(uint8_t) * 2 + PARAM_LEN * 4
