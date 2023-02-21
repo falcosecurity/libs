d3f50f2b5c6d61ce08fa4613e554c2723c139070
David Windsor <dwindsor@secureworks.com>
2023-02-21 11:06:40 -0500

    Use fixed_size api
    Signed-off-by: David Windsor <dwindsor@secureworks.com>

--- driver/modern_bpf/programs/tail_called/events/syscall_dispatched_events/nanosleep.bpf.c
+++ driver/modern_bpf/programs/tail_called/events/syscall_dispatched_events/nanosleep.bpf.c
@@ -1,12 +1,11 @@
 /*
- * Copyright (C) 2022 The Falco Authors.
+ * Copyright (C) 2023 The Falco Authors.
  *
  * This file is dual licensed under either the MIT or GPL 2. See MIT.txt
  * or GPL2.txt for full copies of the license.
  */
 
 #include <helpers/interfaces/fixed_size_event.h>
-#include <helpers/interfaces/variable_size_event.h>
 
 /*=============================== ENTER EVENT ===========================*/
 
@@ -15,13 +14,13 @@ int BPF_PROG(nanosleep_e,
 	     struct pt_regs *regs,
 	     long id)
 {
-        struct auxiliary_map *auxmap = auxmap__get();
-        if(!auxmap)
+	struct ringbuf_struct ringbuf;
+        if(!ringbuf__reserve_space(&ringbuf, NANOSLEEP_E_SIZE))
         {
                 return 0;
         }
 
-        auxmap__preload_event_header(auxmap, PPME_SYSCALL_NANOSLEEP_E);
+        ringbuf__store_event_header(&ringbuf, PPME_SYSCALL_NANOSLEEP_E);
 
 	/*=============================== COLLECT PARAMETERS  ===========================*/
 
@@ -31,18 +30,16 @@ int BPF_PROG(nanosleep_e,
         if(bpf_probe_read_user(&ts, bpf_core_type_size(struct __kernel_timespec), (void *)ts_pointer))
         {
                 /* In case of invalid pointer, like in the other drivers */
-                auxmap__store_u64_param(auxmap, (u64)-1);
+                ringbuf__store_u64(&ringbuf, (u64)-1);
         }
         else
         {
-                auxmap__store_u64_param(auxmap, ((u64)ts.tv_sec) * 1000000000 + ts.tv_nsec);
+                ringbuf__store_u64(&ringbuf, ((u64)ts.tv_sec) * 1000000000 + ts.tv_nsec);
         }
 
 	/*=============================== COLLECT PARAMETERS  ===========================*/
 
-	auxmap__finalize_event_header(auxmap);
-
-	auxmap__submit_event(auxmap);
+	ringbuf__submit_event(&ringbuf);
 
 	return 0;
 }
@@ -56,24 +53,22 @@ int BPF_PROG(nanosleep_x,
 	     struct pt_regs *regs,
 	     long ret)
 {
-	struct auxiliary_map *auxmap = auxmap__get();
-	if(!auxmap)
-	{
-		return 0;
-	}
+        struct ringbuf_struct ringbuf;
+        if(!ringbuf__reserve_space(&ringbuf, NANOSLEEP_X_SIZE))
+        {
+                return 0;
+        }
 
-	auxmap__preload_event_header(auxmap, PPME_SYSCALL_NANOSLEEP_X);
+        ringbuf__store_event_header(&ringbuf, PPME_SYSCALL_NANOSLEEP_X);
 
 	/*=============================== COLLECT PARAMETERS  ===========================*/
 
 	/* Parameter 1: res (type: PT_ERRNO) */
-	auxmap__store_s64_param(auxmap, ret);
+	ringbuf__store_s64(&ringbuf, ret);
 
 	/*=============================== COLLECT PARAMETERS  ===========================*/
 
-	auxmap__finalize_event_header(auxmap);
-
-	auxmap__submit_event(auxmap);
+	ringbuf__submit_event(&ringbuf);
 
 	return 0;
 }
