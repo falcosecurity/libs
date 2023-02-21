da9b795db953165a04b5752d07cb940d07ac48a8
Wiktor Gołgowski <wiktor.golgowski@sysdig.com>
2023-02-21 19:46:04 +0100

    new(test): add unit tests for stat events
    Signed-off-by: Wiktor Gołgowski <wiktor.golgowski@sysdig.com>

---
+++ test/drivers/test_suites/syscall_enter_suite/stat_e.cpp
@@ -0,0 +1,39 @@
+#include "../../event_class/event_class.h"
+
+#ifdef __NR_stat
+
+TEST(SyscallEnter, statE)
+{
+	auto evt_test = get_syscall_event_test(__NR_stat, ENTER_EVENT);
+
+	evt_test->enable_capture();
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	char pathname[] = "//**null-file-path**//";
+	struct stat statbuf = { 0 };
+	assert_syscall_state(SYSCALL_FAILURE, "stat", syscall(__NR_stat, pathname, &statbuf));
+
+	/*=============================== TRIGGER SYSCALL ===========================*/
+
+	evt_test->disable_capture();
+
+	evt_test->assert_event_presence();
+
+	if(HasFatalFailure())
+	{
+		return;
+	}
+
+	evt_test->parse_event();
+
+	evt_test->assert_header();
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	evt_test->assert_num_params_pushed(0);
+}
+#endif
