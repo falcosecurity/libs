5263c4024b4fbd06116bb64103d45078e74b3218
Andrea Terzolo <andrea.terzolo@polito.it>
2023-03-05 16:52:31 +0100

    test: signal_deliver test caused some failures on some machines
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- test/drivers/test_suites/generic_tracepoints_suite/signal_deliver.cpp
+++ test/drivers/test_suites/generic_tracepoints_suite/signal_deliver.cpp
@@ -1,9 +1,12 @@
 #include "../../event_class/event_class.h"
 
-#if defined(__NR_clone3) && defined(__NR_kill)
+#ifdef __NR_kill
 
 #include <signal.h>
-#include <linux/sched.h>
+
+static void signal_deliver_callback(int signal)
+{
+}
 
 TEST(GenericTracepoints, signal_deliver)
 {
@@ -13,36 +16,19 @@ TEST(GenericTracepoints, signal_deliver)
 
 	/*=============================== TRIGGER SYSCALL  ===========================*/
 
-	struct clone_args cl_args = {0};
-	cl_args.exit_signal = SIGCHLD;
-	pid_t ret_pid = syscall(__NR_clone3, &cl_args, sizeof(cl_args));
-
-	if(ret_pid == 0)
+	if(signal(SIGUSR1, signal_deliver_callback) == SIG_ERR)
 	{
-		sleep(10);
-		exit(EXIT_SUCCESS);
+		FAIL() << "An error occurred while setting SIGUSR1 signal handler.\n";
 	}
 
-	assert_syscall_state(SYSCALL_SUCCESS, "clone3", ret_pid, NOT_EQUAL, -1);
-
-	/* Kill the child */
-	assert_syscall_state(SYSCALL_SUCCESS, "kill", syscall(__NR_kill, ret_pid, SIGKILL), NOT_EQUAL, -1);
-	
-	/* Wait for the child to be killed */
-	int status = 0;
-	int options = 0;
-	assert_syscall_state(SYSCALL_SUCCESS, "wait4", syscall(__NR_wait4, ret_pid, &status, options, NULL), NOT_EQUAL, -1);
-
-	if(__WTERMSIG(status) != SIGKILL)
-	{
-		FAIL() << "The child is not killed by the signal." << std::endl;
-	}
+	/* Send a signal to the caller */
+	assert_syscall_state(SYSCALL_SUCCESS, "kill", syscall(__NR_kill, getpid(), SIGUSR1), NOT_EQUAL, -1);
 
 	/*=============================== TRIGGER SYSCALL ===========================*/
 
 	evt_test->disable_capture();
 
-	evt_test->assert_event_presence(ret_pid);
+	evt_test->assert_event_presence();
 
 	if(HasFatalFailure())
 	{
@@ -60,10 +46,10 @@ TEST(GenericTracepoints, signal_deliver)
 	evt_test->assert_numeric_param(1, (int64_t)0);
 
 	/* Parameter 2: dpid (type: PT_PID) */
-	evt_test->assert_numeric_param(2, (int64_t)ret_pid);
+	evt_test->assert_numeric_param(2, (int64_t)getpid());
 
 	/* Parameter 3: sig (type: PT_SIGTYPE) */
-	evt_test->assert_numeric_param(3, (uint8_t)SIGKILL);
+	evt_test->assert_numeric_param(3, (uint8_t)SIGUSR1);
 
 	/*=============================== ASSERT PARAMETERS  ===========================*/
 
