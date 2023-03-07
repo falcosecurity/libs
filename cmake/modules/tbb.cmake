488a9568d00b0b4260c22811e39400d0ffe90485
Luca Guerra <luca@guerra.sh>
2023-03-07 16:23:52 +0000

    update(cmake): upgrade to libtbb v2021.8.0
    Signed-off-by: Luca Guerra <luca@guerra.sh>

--- cmake/modules/tbb.cmake
+++ cmake/modules/tbb.cmake
@@ -16,24 +16,25 @@ elseif(NOT USE_BUNDLED_TBB)
 else()
 	set(TBB_SRC "${PROJECT_BINARY_DIR}/tbb-prefix/src/tbb")
 	set(TBB_INCLUDE_DIR "${TBB_SRC}/include/")
-	set(TBB_LIB "${TBB_SRC}/build/lib_release/libtbb.a")
+	set(TBB_LIB "${TBB_SRC}/lib_release/libtbb.a")
 	if(NOT TARGET tbb)
 		message(STATUS "Using bundled tbb in '${TBB_SRC}'")
 
 		ExternalProject_Add(tbb
 			PREFIX "${PROJECT_BINARY_DIR}/tbb-prefix"
-			URL "https://github.com/oneapi-src/oneTBB/archive/2020_U3.tar.gz"
-			URL_HASH "SHA256=2103cc6238c935664f87680618f6684d57501d4a2fa8ea8f6c97ad6ff7dc722a"
-			CONFIGURE_COMMAND ""
-			BUILD_COMMAND ${CMD_MAKE} tbb_build_dir=${TBB_SRC}/build tbb_build_prefix=lib extra_inc=big_iron.inc
+			URL "https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.8.0.tar.gz"
+			URL_HASH "SHA256=eee380323bb7ce864355ed9431f85c43955faaae9e9bce35c62b372d7ffd9f8b"
 			BUILD_IN_SOURCE 1
+			BUILD_COMMAND ${CMD_MAKE} tbb
+			CMAKE_ARGS
+				-DBUILD_SHARED_LIBS=Off
+				-DCMAKE_BUILD_TYPE=release
+				-DTBB_OUTPUT_DIR_BASE=lib
 			BUILD_BYPRODUCTS ${TBB_LIB}
 			INSTALL_COMMAND "")
 		install(FILES "${TBB_LIB}" DESTINATION "${CMAKE_INSTALL_LIBDIR}/${LIBS_PACKAGE_NAME}"
 				COMPONENT "libs-deps")
 		install(DIRECTORY "${TBB_INCLUDE_DIR}/tbb" DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/${LIBS_PACKAGE_NAME}"
-				COMPONENT "libs-deps")
-		install(DIRECTORY "${TBB_INCLUDE_DIR}/serial" DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/${LIBS_PACKAGE_NAME}"
 				COMPONENT "libs-deps")
 	endif()
 endif()
