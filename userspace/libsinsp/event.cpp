c6c6335c59f9ace14282760ec334dc9d65e5ad8f
Luca Guerra <luca@guerra.sh>
2023-03-10 14:23:09 +0000

    fix(libsinsp,libscap): remove legacy references to is_windows
    Signed-off-by: Luca Guerra <luca@guerra.sh>

--- userspace/libsinsp/event.cpp
+++ userspace/libsinsp/event.cpp
@@ -1725,8 +1725,7 @@ const char* sinsp_evt::get_param_as_str(uint32_t id, OUT const char** resolved_s
 					(char*)cwd.c_str(),
 					(uint32_t)cwd.length(),
 					payload,
-					payload_len,
-					m_inspector->m_is_windows))
+					payload_len))
 				{
 					m_resolved_paramstr_storage[0] = 0;
 				}
