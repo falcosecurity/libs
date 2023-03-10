add9a61084f7cf038c3fc8d6f7953113432d293f
Andrea Terzolo <andrea.terzolo@polito.it>
2023-03-10 13:57:22 +0100

    new: support `recv` syscall
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- test/drivers/test_suites/syscall_exit_suite/socketcall_x.cpp
+++ test/drivers/test_suites/syscall_exit_suite/socketcall_x.cpp
@@ -2952,4 +2952,55 @@ TEST(SyscallExit, socketcall_sendX)
 }
 #endif
 
+#ifdef __NR_recv
+TEST(SyscallExit, socketcall_recvX_fail)
+{
+	auto evt_test = get_syscall_event_test(__NR_recv, EXIT_EVENT);
+
+	evt_test->enable_capture();
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	int32_t mock_fd = -1;
+	char *mock_buf = NULL;
+	size_t mock_count = DEFAULT_SNAPLEN;
+	int flags = 0;
+
+	unsigned long args[4] = {0};
+	args[0] = mock_fd;
+	args[1] = (unsigned long)mock_buf;
+	args[2] = mock_count;
+	args[3] = (unsigned long)flags;
+	assert_syscall_state(SYSCALL_FAILURE, "recv", syscall(__NR_socketcall, SYS_RECV, args));
+	int errno_value = -errno;
+
+	/*=============================== TRIGGER SYSCALL ===========================*/
+
+	evt_test->disable_capture();
+
+	evt_test->assert_event_presence();
+
+	if(HasFatalFailure())
+	{
+		return;
+	}
+
+	evt_test->parse_event();
+
+	evt_test->assert_header();
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	/* Parameter 1: res (type: PT_ERRNO) */
+	evt_test->assert_numeric_param(1, (int64_t)errno_value);
+
+	/* Parameter 2: data (type: PT_BYTEBUF) */
+	evt_test->assert_empty_param(2);
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	evt_test->assert_num_params_pushed(2);
+}
+#endif
+
 #endif /* __NR_socketcall */
