6106b1c15c4cd9515a12b63f59fdb36b105f6581
Andrea Terzolo <andrea.terzolo@polito.it>
2023-03-10 10:04:14 +0100

    new: support `getrlimit` syscall
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

---
+++ test/drivers/test_suites/syscall_exit_suite/getrlimit_x.cpp
@@ -0,0 +1,93 @@
+#include "../../event_class/event_class.h"
+
+#ifdef __NR_getrlimit
+
+#include <sys/resource.h>
+
+TEST(SyscallExit, getrlimitX_success)
+{
+	auto evt_test = get_syscall_event_test(__NR_getrlimit, EXIT_EVENT);
+
+	evt_test->enable_capture();
+
+	/*=============================== TRIGGER SYSCALL ===========================*/
+
+	int resource = RLIMIT_NPROC;
+	struct rlimit rlim = {0};
+	assert_syscall_state(SYSCALL_SUCCESS, "getrlimit", syscall(__NR_getrlimit, resource, &rlim), NOT_EQUAL, -1);
+
+	/*=============================== TRIGGER SYSCALL ===========================*/
+
+	evt_test->disable_capture();
+
+	evt_test->assert_event_presence();
+
+	if(HasFatalFailure())
+	{
+		return;
+	}
+
+	evt_test->parse_event();
+
+	evt_test->assert_header();
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	/* Parameter 1: res (type: PT_ERRNO) */
+	evt_test->assert_numeric_param(1, (int64_t)0);
+
+	/* Parameter 2: cur (type: PT_INT64) */
+	evt_test->assert_numeric_param(2, (int64_t)rlim.rlim_cur);
+
+	/* Parameter 3: max (type: PT_INT64) */
+	evt_test->assert_numeric_param(3, (int64_t)rlim.rlim_max);
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	evt_test->assert_num_params_pushed(3);
+}
+
+TEST(SyscallExit, getrlimitX_failure)
+{
+	auto evt_test = get_syscall_event_test(__NR_getrlimit, EXIT_EVENT);
+
+	evt_test->enable_capture();
+
+	/*=============================== TRIGGER SYSCALL ===========================*/
+
+	int resource = -1;
+	struct rlimit rlim = {0};
+	assert_syscall_state(SYSCALL_FAILURE, "getrlimit", syscall(__NR_getrlimit, resource, &rlim));
+	int64_t errno_value = -errno;
+
+	/*=============================== TRIGGER SYSCALL ===========================*/
+
+	evt_test->disable_capture();
+
+	evt_test->assert_event_presence();
+
+	if(HasFatalFailure())
+	{
+		return;
+	}
+
+	evt_test->parse_event();
+
+	evt_test->assert_header();
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	/* Parameter 1: res (type: PT_ERRNO) */
+	evt_test->assert_numeric_param(1, (int64_t)errno_value);
+
+	/* Parameter 2: cur (type: PT_INT64) */
+	evt_test->assert_numeric_param(2, (int64_t)-1);
+
+	/* Parameter 3: max (type: PT_INT64) */
+	evt_test->assert_numeric_param(3, (int64_t)-1);
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	evt_test->assert_num_params_pushed(3);
+}
+#endif
