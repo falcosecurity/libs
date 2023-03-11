2c64de714c6c83b8eb2e7e3f02b168945deff820
Andrea Terzolo <andrea.terzolo@polito.it>
2023-03-11 15:05:32 +0100

    update: collect data when the syscall fails (sendto)
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- test/drivers/test_suites/syscall_exit_suite/sendto_x.cpp
+++ test/drivers/test_suites/syscall_exit_suite/sendto_x.cpp
@@ -126,11 +126,58 @@ TEST(SyscallExit, sendtoX_fail)
 	/*=============================== TRIGGER SYSCALL ===========================*/
 
 	int32_t mock_fd = -1;
+	char sent_data[DEFAULT_SNAPLEN / 2] = "some-data";
+	size_t len = DEFAULT_SNAPLEN / 2;
+	uint32_t sendto_flags = 0;
+	struct sockaddr* dest_addr = NULL;
+	socklen_t addrlen = 0;
+
+	assert_syscall_state(SYSCALL_FAILURE, "sendto", syscall(__NR_sendto, mock_fd, sent_data, len, sendto_flags, dest_addr, addrlen));
+	int64_t errno_value = -errno;
+
+	/*=============================== TRIGGER SYSCALL ===========================*/
+
+	evt_test->disable_capture();
+
+	evt_test->assert_event_presence();
+
+	if(HasFatalFailure())
+	{
+		return;
+	}
+
+	evt_test->parse_event();
+
+	evt_test->assert_header();
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	/* Parameter 1: res (type: PT_ERRNO) */
+	evt_test->assert_numeric_param(1, (int64_t)errno_value);
+
+	/* Parameter 2: data (type: PT_BYTEBUF)*/
+	evt_test->assert_bytebuf_param(2, sent_data, DEFAULT_SNAPLEN / 2);
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	evt_test->assert_num_params_pushed(2);
+}
+
+TEST(SyscallExit, sendtoX_empty)
+{
+	auto evt_test = get_syscall_event_test(__NR_sendto, EXIT_EVENT);
+
+	evt_test->enable_capture();
+
+	/*=============================== TRIGGER SYSCALL ===========================*/
+
+	int32_t mock_fd = -1;
 	char* sent_data = NULL;
 	size_t len = 0;
 	uint32_t sendto_flags = 0;
 	struct sockaddr* dest_addr = NULL;
 	socklen_t addrlen = 0;
+
 	assert_syscall_state(SYSCALL_FAILURE, "sendto", syscall(__NR_sendto, mock_fd, sent_data, len, sendto_flags, dest_addr, addrlen));
 	int64_t errno_value = -errno;
 
