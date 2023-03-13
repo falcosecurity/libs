a055187ac908f04d94a5232d869d7d31eed5852d
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-03-13 10:43:23 +0100

    fix(e2e-tests): prevent out of index access on systems without BTF
    Signed-off-by: Mauro Ezequiel Moltrasio <mmoltras@redhat.com>

--- test/e2e/tests/commons/sinspqa/__init__.py
+++ test/e2e/tests/commons/sinspqa/__init__.py
@@ -14,3 +14,11 @@ else:
     LOGS_PATH = tempfile.mkdtemp()
 
 SINSP_LOG_PATH = os.path.join(LOGS_PATH, 'sinsp.log')
+
+
+kernel = os.uname().release.split('.')
+kernel_major = int(kernel[0])
+kernel_minor = int(kernel[1])
+
+BTF_IS_AVAILABLE = kernel_major > 5 or (
+    kernel_major == 5 and kernel_minor >= 8)
