e8647bc944c7e678df40a5ffa1002212cce77173
Andrea Terzolo <andrea.terzolo@polito.it>
2023-03-18 22:35:14 +0100

    new: support `getpeername` syscall
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

---
+++ test/drivers/test_suites/syscall_enter_suite/getpeername_e.cpp
@@ -0,0 +1,40 @@
+#include "../../event_class/event_class.h"
+
+#ifdef __NR_getpeername
+TEST(SyscallEnter, getpeernameE)
+{
+	auto evt_test = get_syscall_event_test(__NR_getpeername, ENTER_EVENT);
+
+	evt_test->enable_capture();
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	int32_t mock_fd = -1;
+	void* usockaddr = NULL;
+	int* usockaddr_len = NULL;
+	assert_syscall_state(SYSCALL_FAILURE, "getpeername", syscall(__NR_getpeername, mock_fd, usockaddr, usockaddr_len));
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	evt_test->disable_capture();
+
+	evt_test->assert_event_presence();
+
+	if(HasFatalFailure())
+	{
+		return;
+	}
+
+	evt_test->parse_event();
+
+	evt_test->assert_header();
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	// Here we have no parameters to assert.
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	evt_test->assert_num_params_pushed(0);
+}
+#endif
--- test/drivers/test_suites/syscall_exit_suite/socketcall_x.cpp
+++ test/drivers/test_suites/syscall_exit_suite/socketcall_x.cpp
@@ -3185,4 +3185,48 @@ TEST(SyscallExit, socketcall_recvX_fail)
 }
 #endif
 
+#ifdef __NR_getpeername
+TEST(SyscallExit, socketcall_getpeernameX)
+{
+	auto evt_test = get_syscall_event_test(__NR_getpeername, EXIT_EVENT);
+
+	evt_test->enable_capture();
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	int32_t mock_fd = -1;
+	void *usockaddr = NULL;
+	int *usockaddr_len = NULL;
+
+	unsigned long args[3] = {0};
+	args[0] = mock_fd;
+	args[1] = (unsigned long)usockaddr;
+	args[2] = (unsigned long)usockaddr_len;
+	assert_syscall_state(SYSCALL_FAILURE, "getpeername", syscall(__NR_socketcall, SYS_GETPEERNAME, args));
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	evt_test->disable_capture();
+
+	evt_test->assert_event_presence();
+
+	if(HasFatalFailure())
+	{
+		return;
+	}
+
+	evt_test->parse_event();
+
+	evt_test->assert_header();
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	// Here we have no parameters to assert.
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	evt_test->assert_num_params_pushed(0);
+}
+#endif
+
 #endif /* __NR_socketcall */
