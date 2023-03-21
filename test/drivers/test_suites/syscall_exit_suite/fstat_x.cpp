2288cde46166566af8cd07af1f3ee55816045cd5
Wiktor Golgowski <wiktor.golgowski@sysdig.com>
2023-03-21 13:43:15 +0000

    new(test): add unit tests for fstat events
    Signed-off-by: Wiktor Golgowski <wiktor.golgowski@sysdig.com>

---
+++ test/drivers/test_suites/syscall_exit_suite/fstat_x.cpp
@@ -0,0 +1,41 @@
+#include "../../event_class/event_class.h"
+
+#ifdef __NR_fstat
+TEST(SyscallExit, fstatX)
+{
+	auto evt_test = get_syscall_event_test(__NR_fstat, EXIT_EVENT);
+
+	evt_test->enable_capture();
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	int f_desc = -1;
+	struct stat statbuf = { 0 };
+	assert_syscall_state(SYSCALL_FAILURE, "fstat", syscall(__NR_fstat, f_desc, &statbuf));
+	int64_t errno_value = -errno;
+
+	/*=============================== TRIGGER SYSCALL ===========================*/
+
+	evt_test->disable_capture();
+
+	evt_test->assert_event_presence();
+
+	if(HasFatalFailure())
+	{
+		return;
+	}
+
+	evt_test->parse_event();
+
+	evt_test->assert_header();
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	/* Parameter 1: res (type: PT_ERRNO)*/
+	evt_test->assert_numeric_param(1, (int64_t)errno_value);
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	evt_test->assert_num_params_pushed(1);
+}
+#endif
