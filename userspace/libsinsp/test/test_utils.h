cd476d05c1b5a17a28ea8527859aa3f76694a49c
Andrea Terzolo <andrea.terzolo@polito.it>
2023-03-24 17:05:54 +0100

    cleanup: move event_table tests to scap
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- userspace/libsinsp/test/test_utils.h
+++ userspace/libsinsp/test/test_utils.h
@@ -22,6 +22,7 @@ limitations under the License.
 #include <set>
 #include <unordered_set>
 #include <netinet/in.h>
+#include <event_stats.h>
 
 #define ASSERT_NAMES_EQ(a, b)                                                                                        \
 	{                                                                                                            \
@@ -74,16 +75,6 @@ limitations under the License.
 		ASSERT_TRUE(a1.equals(b1));                                                                                                    \
 		ASSERT_EQ(a1.size(), b1.size());                                                                                               \
 	}
-
-/* These numbers must be updated when we add new events in the event table */
-#define SYSCALL_EVENTS_NUM 354
-#define TRACEPOINT_EVENTS_NUM 6
-#define METAEVENTS_NUM 19
-#define PLUGIN_EVENTS_NUM 1
-#define UNKNOWN_EVENTS_NUM 20
-
-/* This number should be increased when we add/remove a generic syscall */
-#define GENERIC_SYSCALLS_NUM 235
 
 namespace test_utils {
 
