5fd46f0bf0d4916f89edfcebbaf809bbcff1eb5e
Andrea Terzolo <andrea.terzolo@polito.it>
2023-03-27 18:11:53 +0200

    new: introduce dedicated dropping logic for some syscalls
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- driver/modern_bpf/programs/tail_called/events/syscall_dispatched_events/close.bpf.c
+++ driver/modern_bpf/programs/tail_called/events/syscall_dispatched_events/close.bpf.c
@@ -14,6 +14,40 @@ int BPF_PROG(close_e,
 	     struct pt_regs *regs,
 	     long id)
 {
+	if(maps__get_dropping_mode())
+	{
+		s32 fd = (s32)extract__syscall_argument(regs, 0);
+		/* We drop the event if we are closing a negative file descriptor */
+		if(fd < 0)
+		{
+			return 0;
+		}
+
+		struct task_struct *task = get_current_task();
+		u32 max_fds = 0;
+		READ_TASK_FIELD_INTO(&max_fds, task, files, fdt, max_fds);
+		/* We drop the event if the fd is >= than `max_fds` */
+		if(fd >= max_fds)
+		{
+			return 0;
+		}
+
+		/* We drop the event if the fd is not open */
+		long unsigned int entry = 0;
+		long unsigned int *open_fds = READ_TASK_FIELD(task, files, fdt, open_fds);
+		if(open_fds == NULL)
+		{
+			return 0;
+		}
+		if(bpf_probe_read_kernel(&entry, sizeof(entry), (const void *)&(open_fds[BIT_WORD(fd)])) == 0)
+		{
+			if(!(1UL & (entry >> (fd & (BITS_PER_LONG - 1)))))
+			{
+				return 0;
+			}
+		}
+	}
+
 	struct ringbuf_struct ringbuf;
 	if(!ringbuf__reserve_space(&ringbuf, CLOSE_E_SIZE))
 	{
@@ -44,6 +78,11 @@ int BPF_PROG(close_x,
 	     struct pt_regs *regs,
 	     long ret)
 {
+	if(maps__get_dropping_mode() && ret < 0)
+	{
+		return 0;
+	}
+
 	struct ringbuf_struct ringbuf;
 	if(!ringbuf__reserve_space(&ringbuf, CLOSE_X_SIZE))
 	{
