468f90b6aeceb13bc10ca77587e7abd27784483d
Jason Dellaluce <jasondellaluce@gmail.com>
2023-04-03 17:34:17 +0000

    fix(userspace/libsinsp): adjust failing tests
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- userspace/libsinsp/test/public_sinsp_API/interesting_syscalls.cpp
+++ userspace/libsinsp/test/public_sinsp_API/interesting_syscalls.cpp
@@ -114,12 +114,12 @@ TEST(interesting_syscalls, names_sc_set_names_corner_cases)
 	/* INCONSISTENCY: `event_names_to_sc_set` is converting event names to ppm_sc, but this was not its original scope, the original scope was to convert sc_names -> to sc_set  */
 	std::unordered_set<std::string> event_names{"accept", "execve", "syncfs", "eventfd", "umount", "pipe", "signalfd", "umount2", "procexit"};
 	auto sc_set = libsinsp::events::event_names_to_sc_set(event_names);
-	libsinsp::events::set<ppm_sc_code> expected_sc_set{PPM_SC_ACCEPT, PPM_SC_EXECVE, PPM_SC_SYNCFS, PPM_SC_EVENTFD, PPM_SC_UMOUNT, PPM_SC_PIPE, PPM_SC_SIGNALFD, PPM_SC_UMOUNT2, PPM_SC_SCHED_PROCESS_EXIT};
+	libsinsp::events::set<ppm_sc_code> expected_sc_set{PPM_SC_ACCEPT, PPM_SC_ACCEPT4, PPM_SC_EXECVE, PPM_SC_SYNCFS, PPM_SC_EVENTFD, PPM_SC_UMOUNT, PPM_SC_PIPE, PPM_SC_SIGNALFD, PPM_SC_UMOUNT2, PPM_SC_SCHED_PROCESS_EXIT};
 	ASSERT_PPM_SC_CODES_EQ(sc_set, expected_sc_set);
 
 	/* Please note that here we are converting sc_set to sc_names not event_names! */
 	auto sc_names = libsinsp::events::sc_set_to_event_names(sc_set);	
-	static std::unordered_set<std::string> expected_sc_names = {"accept", "execve", "syncfs", "eventfd", "umount", "pipe", "signalfd", "umount2", "procexit"};
+	static std::unordered_set<std::string> expected_sc_names = {"accept", "accept4", "execve", "syncfs", "eventfd", "umount", "pipe", "signalfd", "umount2", "procexit"};
 	ASSERT_NAMES_EQ(expected_sc_names, sc_names);
 }
 
