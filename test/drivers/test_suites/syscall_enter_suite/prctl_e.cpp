00af2d6c304c6087b9bfb2c3b7d338726fa5868f
Roberto Scolaro <roberto.scolaro21@gmail.com>
2023-04-12 13:57:05 +0000

    chore(test/drivers): added new tests
    Signed-off-by: Roberto Scolaro <roberto.scolaro21@gmail.com>

--- test/drivers/test_suites/syscall_enter_suite/prctl_e.cpp
+++ test/drivers/test_suites/syscall_enter_suite/prctl_e.cpp
@@ -1,59 +1,25 @@
 #include "../../event_class/event_class.h"
-#include "../../flags/flags_definitions.h"
-#include "../../helpers/proc_parsing.h"
-
-#if defined(__NR_prctl) && defined(__NR_clone3) && defined(__NR_wait4)
-
-#include <linux/sched.h>
-
+#if defined(__NR_prctl)
 TEST(SyscallEnter, prctlE)
 {
 	auto evt_test = get_syscall_event_test(__NR_prctl, ENTER_EVENT);
-
 	evt_test->enable_capture();
 
 	/*=============================== TRIGGER SYSCALL  ===========================*/
 
-	int option = 36; //PR_SET_CHILD_SUBREAPER
-	unsigned long arg2 = 1;
+	int option = -2;
+	unsigned long arg2 = 0;
 	unsigned long arg3 = 0;
 	unsigned long arg4 = 0;
 	unsigned long arg5 = 0;
 
-	/* We need to use `SIGCHLD` otherwise the parent won't receive any signal
-	 * when the child terminates.
-	 */
-	struct clone_args cl_args = {0};
-	cl_args.exit_signal = SIGCHLD;
-	pid_t ret_pid = syscall(__NR_clone3, &cl_args, sizeof(cl_args));
-	assert_syscall_state(SYSCALL_SUCCESS, "clone3", ret_pid, NOT_EQUAL, -1);
-
-	if (ret_pid == 0)
-	{
-		/*
-		 * Call the `prctl`
-		 */
-		int res = syscall(__NR_prctl, option, arg2, arg3, arg4, arg5);
-		assert_syscall_state(SYSCALL_SUCCESS, "prctl", res,EQUAL,0);
-		exit(EXIT_SUCCESS);
-
-	}
-
-	/* Catch the child before doing anything else. */
-	int status = 0;
-	int options = 0;
-	assert_syscall_state(SYSCALL_SUCCESS, "wait4", syscall(__NR_wait4, ret_pid, &status, options, NULL), NOT_EQUAL, -1);
-
-	if(__WEXITSTATUS(status) == EXIT_FAILURE || __WIFSIGNALED(status) != 0)
-	{
-		FAIL() << "The prctl call is successful while it should fail..." << std::endl;
-	}
+	assert_syscall_state(SYSCALL_SUCCESS, "prctl", syscall(__NR_prctl, option, arg2, arg3, arg4, arg5));
 
 	/*=============================== TRIGGER SYSCALL  ===========================*/
 
 	evt_test->disable_capture();
 
-	evt_test->assert_event_presence(ret_pid);
+	evt_test->assert_event_presence();
 
 	if(HasFatalFailure())
 	{
