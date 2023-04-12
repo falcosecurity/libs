14dc3450f3ff10a21e4e223d2fe90288ce8ae046
Roberto Scolaro <roberto.scolaro21@gmail.com>
2023-04-12 14:34:14 +0000

    fix(driver/modern_bpf): fix func args
    Signed-off-by: Roberto Scolaro <roberto.scolaro21@gmail.com>

--- driver/modern_bpf/programs/tail_called/events/syscall_dispatched_events/prctl.bpf.c
+++ driver/modern_bpf/programs/tail_called/events/syscall_dispatched_events/prctl.bpf.c
@@ -16,7 +16,7 @@ int BPF_PROG(prctl_e,
 	     long id)
 {
 	struct ringbuf_struct ringbuf;
-	if(!ringbuf__reserve_space(&ringbuf, PRCTL_E_SIZE))
+	if(!ringbuf__reserve_space(&ringbuf, ctx, PRCTL_E_SIZE))
 	{
 		return 0;
 	}
@@ -99,7 +99,7 @@ int BPF_PROG(prctl_x,
 
 	auxmap__finalize_event_header(auxmap);
 
-	auxmap__submit_event(auxmap);
+	auxmap__submit_event(auxmap, ctx);
 
 	return 0;
 }
