12c03dfc941ba64929d86f830176252ebba4a700
Roberto Scolaro <roberto.scolaro21@gmail.com>
2023-04-29 22:31:29 +0000

    fix(driver/kmod): fix typo
    Signed-off-by: Roberto Scolaro <roberto.scolaro21@gmail.com>

--- driver/ppm_fillers.c
+++ driver/ppm_fillers.c
@@ -1418,7 +1418,7 @@ cgroups_error:
 	#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 16, 0)
 							unsigned long has_upper = oe->has_upper;
 	#else
-							unsigned long has_upper = test_bit(flags, &(oe->flags));
+							unsigned long has_upper = test_bit(OVL_E_UPPER_ALIAS, &(oe->flags));
 	#endif
 
 							if(*upper_dentry && (has_upper || disconnected))
@@ -7677,8 +7677,7 @@ cgroups_error:
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 16, 0)
 							unsigned long has_upper = oe->has_upper;
 #else
-							unsigned long flags = oe->flags;
-							unsigned long has_upper = test_bit(flags, OVL_E_UPPER_ALIAS);
+							unsigned long has_upper = test_bit(OVL_E_UPPER_ALIAS, &(oe->flags));
 #endif
 
 							if(*upper_dentry && (has_upper || disconnected))
