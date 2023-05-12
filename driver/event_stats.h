d01d4bade26ed8b70881aaef354ec65da127cc3e
Jason Dellaluce <jasondellaluce@gmail.com>
2023-05-12 16:20:30 +0000

    update(driver): add new event type for async meta-event
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- driver/event_stats.h
+++ driver/event_stats.h
@@ -3,6 +3,6 @@
 /* These numbers must be updated when we add new events in the event table */
 #define SYSCALL_EVENTS_NUM 356
 #define TRACEPOINT_EVENTS_NUM 6
-#define METAEVENTS_NUM 19
-#define PLUGIN_EVENTS_NUM 1
-#define UNKNOWN_EVENTS_NUM 20
+#define METAEVENTS_NUM 20
+#define PLUGIN_EVENTS_NUM 2
+#define UNKNOWN_EVENTS_NUM 21
