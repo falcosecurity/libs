8404e45677b1bfe63d18c48fa6e09a16f72257f3
Andrea Terzolo <andrea.terzolo@polito.it>
2023-05-26 14:37:34 +0200

    fix: correctly convert socketcall codes on 32 bits
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

---
+++ driver/socketcall_to_syscall.h
@@ -0,0 +1,15 @@
+/*
+
+Copyright (C) 2023 The Falco Authors.
+
+This file is dual licensed under either the MIT or GPL 2. See MIT.txt
+or GPL2.txt for full copies of the license.
+
+*/
+
+#ifndef SOCKETCALL_TO_SYSCALL_H
+#define SOCKETCALL_TO_SYSCALL_H
+
+int socketcall_code_to_syscall_code(int socketcall_code);
+
+#endif /* SOCKETCALL_TO_SYSCALL_H */
