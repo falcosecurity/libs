6f2b4a64c9b59b5414300bf5678c00a847a608dc
Andrea Terzolo <andrea.terzolo@polito.it>
2023-05-31 11:24:01 +0200

    cleanup
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- driver/socketcall_to_syscall.h
+++ driver/socketcall_to_syscall.h
@@ -10,6 +10,6 @@ or GPL2.txt for full copies of the license.
 #ifndef SOCKETCALL_TO_SYSCALL_H
 #define SOCKETCALL_TO_SYSCALL_H
 
-int socketcall_code_to_syscall_code(int socketcall_code, bool* is_syscall_id);
+int socketcall_code_to_syscall_code(int socketcall_code, bool* is_syscall_return);
 
 #endif /* SOCKETCALL_TO_SYSCALL_H */
