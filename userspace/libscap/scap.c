82e6c7f5eb64395e2f1170f155c93188b657515c
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2023-06-20 13:21:41 +0200

    cleanup(scap): use scap_open_live_int for source_plugin
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

--- userspace/libscap/scap.c
+++ userspace/libscap/scap.c
@@ -48,7 +48,7 @@ const char* scap_getlasterr(scap_t* handle)
 	return handle ? handle->m_lasterr : "null scap handle";
 }
 
-#if defined(HAS_ENGINE_KMOD) || defined(HAS_ENGINE_BPF) || defined(HAS_ENGINE_MODERN_BPF) || defined(HAS_ENGINE_UDIG) || defined(HAS_ENGINE_TEST_INPUT) || defined(HAS_ENGINE_GVISOR) || defined(HAS_ENGINE_SAVEFILE) || defined(HAS_ENGINE_NODRIVER)
+#if defined(HAS_ENGINE_KMOD) || defined(HAS_ENGINE_BPF) || defined(HAS_ENGINE_MODERN_BPF) || defined(HAS_ENGINE_UDIG) || defined(HAS_ENGINE_TEST_INPUT) || defined(HAS_ENGINE_GVISOR) || defined(HAS_ENGINE_SAVEFILE) || defined(HAS_ENGINE_NODRIVER) || defined(HAS_ENGINE_SOURCE_PLUGIN)
 int32_t scap_init_live_int(scap_t* handle, scap_open_args* oargs, const struct scap_vtable* vtable, struct scap_platform* platform)
 {
 	int32_t rc;
@@ -90,41 +90,6 @@ int32_t scap_init_live_int(scap_t* handle, scap_open_args* oargs, const struct s
 	return SCAP_SUCCESS;
 }
 #endif // HAS_LIVE_CAPTURE
-
-#ifdef HAS_ENGINE_SOURCE_PLUGIN
-int32_t scap_init_plugin_int(scap_t* handle, scap_open_args* oargs, struct scap_platform* platform)
-{
-	int32_t rc;
-
-	//
-	// Preliminary initializations
-	//
-	handle->m_mode = SCAP_MODE_PLUGIN;
-	handle->m_vtable = &scap_source_plugin_engine;
-	handle->m_platform = platform;
-
-	handle->m_engine.m_handle = handle->m_vtable->alloc_handle(handle, handle->m_lasterr);
-	if(!handle->m_engine.m_handle)
-	{
-		snprintf(handle->m_lasterr, SCAP_LASTERR_SIZE, "error allocating the engine structure");
-		return SCAP_FAILURE;
-	}
-
-	handle->m_debug_log_fn = oargs->debug_log_fn;
-
-	if((rc = handle->m_vtable->init(handle, oargs)) != SCAP_SUCCESS)
-	{
-		return rc;
-	}
-
-	if((rc = scap_platform_init(handle->m_platform, handle->m_lasterr, handle->m_engine, oargs)) != SCAP_SUCCESS)
-	{
-		return rc;
-	}
-
-	return SCAP_SUCCESS;
-}
-#endif
 
 scap_t* scap_alloc(void)
 {
@@ -263,7 +228,7 @@ int32_t scap_init(scap_t* handle, scap_open_args* oargs)
 			return scap_errprintf(handle->m_lasterr, 0, "failed to allocate platform struct");
 		}
 
-		return scap_init_plugin_int(handle, oargs, platform);
+		return scap_init_live_int(handle, oargs, &scap_source_plugin_engine, platform);
 	}
 #endif
 
