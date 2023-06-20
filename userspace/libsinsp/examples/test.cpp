588cdce7dd91bf4d78abacbf8f5c46e6755ac7f3
rohith-raju <rohithraju488@gmail.com>
2023-06-20 13:46:54 +0000

    new(ci): run tests for emscripten build
    Signed-off-by: rohith-raju <rohithraju488@gmail.com>
    Co-authored-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- userspace/libsinsp/examples/test.cpp
+++ userspace/libsinsp/examples/test.cpp
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include <emscripten/bind.h>
 #include <cstdlib>
 #include <iostream>
 #include <chrono>
@@ -577,8 +576,4 @@ void json_dump(sinsp& inspector)
 	}
 
 	cout << output << std::endl;
-}
-
-EMSCRIPTEN_BINDINGS(my_module){
-	emscripten::function("usage", &usage);
 }
