eadc315ef8752afd46ba3f71bdefe03898f416dd
Melissa Kilby <melissa.kilby.oss@gmail.com>
2023-06-22 16:00:30 +0000

    fix(test): add null checks for libsinsp test assertion methods
    Signed-off-by: Melissa Kilby <melissa.kilby.oss@gmail.com>

--- userspace/libsinsp/test/sinsp_with_test_input.h
+++ userspace/libsinsp/test/sinsp_with_test_input.h
@@ -268,6 +268,10 @@ protected:
 
 	bool field_exists(sinsp_evt *evt, const std::string& field_name, filter_check_list& flist = g_filterlist)
 	{
+		if (evt == nullptr) {
+			throw sinsp_exception("The event class is NULL");
+		}
+
 		std::unique_ptr<sinsp_filter_check> chk(flist.new_filter_check_from_fldname(field_name, &m_inspector, false));
 		int32_t parse_result = chk->parse_field_name(field_name.c_str(), true, false);
 		if (parse_result <= 0) {
@@ -280,6 +284,10 @@ protected:
 
 	std::string get_field_as_string(sinsp_evt *evt, const std::string& field_name, filter_check_list& flist = g_filterlist)
 	{
+		if (evt == nullptr) {
+			throw sinsp_exception("The event class is NULL");
+		}
+
 		std::unique_ptr<sinsp_filter_check> chk(flist.new_filter_check_from_fldname(field_name, &m_inspector, false));
 		int32_t parse_result = chk->parse_field_name(field_name.c_str(), true, false);
 		if (parse_result <= 0) {
