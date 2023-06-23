f1db22749a880b56ad48de6dc927c8b5a3233c3f
Andrea Terzolo <andrea.terzolo@polito.it>
2023-06-23 11:59:16 +0200

    tests
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- test/drivers/test_suites/generic_tracepoints_suite/sched_process_exec.cpp
+++ test/drivers/test_suites/generic_tracepoints_suite/sched_process_exec.cpp
@@ -115,4 +115,111 @@ TEST(GenericTracepoints, sched_proc_exec)
 
 	evt_test->assert_num_params_pushed(27);
 }
+
+#if defined(__NR_memfd_create) && defined(__NR_openat) && defined(__NR_read) && defined(__NR_write)
+#include <sys/mman.h>
+TEST(GenericTracepoints, sched_proc_exec_success_memfd)
+{
+	auto evt_test = get_syscall_event_test(__NR_execve, EXIT_EVENT);
+
+	evt_test->enable_capture();
+
+	/*=============================== TRIGGER SYSCALL  ===========================*/
+
+	int mem_fd = syscall(__NR_memfd_create, "malware", MFD_CLOEXEC);
+	assert_syscall_state(SYSCALL_SUCCESS, "memfd_create", mem_fd, NOT_EQUAL, -1);
+
+	/* Open the executable to copy */
+	int fd_to_read = syscall(__NR_openat, 0, "/usr/bin/echo", O_RDWR);
+	if(fd_to_read < 0)
+	{
+		FAIL() << "failed to open the file to read\n";
+	}
+
+	char buf[200];
+	ssize_t bytes_read = 200;
+	while(bytes_read != 0)
+	{
+		bytes_read = syscall(__NR_read, fd_to_read, buf, sizeof(buf));
+		if(bytes_read < 0)
+		{
+			syscall(__NR_close, fd_to_read);
+			syscall(__NR_close, mem_fd);
+			FAIL() << "unable to read from file\n";
+		}
+
+		bytes_read = syscall(__NR_write, mem_fd, buf, bytes_read);
+		if(bytes_read < 0)
+		{
+			syscall(__NR_close, fd_to_read);
+			syscall(__NR_close, mem_fd);
+			FAIL() << "unable to write to file\n";
+		}
+	}
+	syscall(__NR_close, fd_to_read);
+
+	/* We need to use `SIGCHLD` otherwise the parent won't receive any signal
+	 * when the child terminates.
+	 */
+	struct clone_args cl_args = {0};
+	cl_args.exit_signal = SIGCHLD;
+	pid_t ret_pid = syscall(__NR_clone3, &cl_args, sizeof(cl_args));
+
+	if(ret_pid == 0)
+	{
+		char pathname[200];
+		snprintf(pathname, sizeof(pathname), "/proc/%d/fd/%d", getpid(), mem_fd);
+		const char *newargv[] = {pathname, "[OUTPUT] SyscallExit.execveX_success_memfd", NULL};
+		const char *newenviron[] = {"IN_TEST=yes", "3_ARGUMENT=yes", "2_ARGUMENT=no", NULL};
+		syscall(__NR_execve, pathname, newargv, newenviron);
+		exit(EXIT_FAILURE);
+	}
+	syscall(__NR_close, mem_fd);
+
+	assert_syscall_state(SYSCALL_SUCCESS, "clone3", ret_pid, NOT_EQUAL, -1);
+
+	/* Catch the child before doing anything else. */
+	int status = 0;
+	int options = 0;
+	assert_syscall_state(SYSCALL_SUCCESS, "wait4", syscall(__NR_wait4, ret_pid, &status, options, NULL), NOT_EQUAL,
+			     -1);
+
+	if(__WEXITSTATUS(status) == EXIT_FAILURE || __WIFSIGNALED(status) != 0)
+	{
+		FAIL() << "The child execve failed." << std::endl;
+	}
+
+	/*=============================== TRIGGER SYSCALL ===========================*/
+
+	evt_test->disable_capture();
+
+	/* We search for a child event. */
+	evt_test->assert_event_presence(ret_pid);
+
+	if(HasFatalFailure())
+	{
+		return;
+	}
+
+	evt_test->parse_event();
+
+	evt_test->assert_header();
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	/* Please note here we cannot assert all the params, we check only the possible ones. */
+
+	/* Parameter 1: res (type: PT_ERRNO)*/
+	evt_test->assert_numeric_param(1, (int64_t)0);
+
+	/* PPM_EXE_WRITABLE is set when the user that executed a process can also write to the executable
+	 * file that is used to spawn it or is its owner or otherwise capable.
+	 */
+	evt_test->assert_numeric_param(20, (uint32_t)PPM_EXE_WRITABLE | PPM_EXE_FROM_MEMFD);
+
+	/*=============================== ASSERT PARAMETERS  ===========================*/
+
+	evt_test->assert_num_params_pushed(27);
+}
+#endif
 #endif
