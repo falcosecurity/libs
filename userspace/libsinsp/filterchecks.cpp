0999e682cadfe03a0abbbe8d0d11e512bc454985
Melissa Kilby <melissa.kilby.oss@gmail.com>
2023-06-28 02:36:48 +0000

    new(libsinsp): add proc.vpgid.name, proc.vpgid.exe, proc.vpgid.exepath
    Signed-off-by: Melissa Kilby <melissa.kilby.oss@gmail.com>

--- userspace/libsinsp/filterchecks.cpp
+++ userspace/libsinsp/filterchecks.cpp
@@ -2372,6 +2372,9 @@ const filtercheck_field_info sinsp_filter_check_thread_fields[] =
 	{PT_CHARBUF, EPF_NONE, PF_NA, "proc.sid.exe", "Process Session First Argument", "The first command line argument argv[0] (usually the executable name or a custom one) of the current process's session leader. This is either the process with pid=proc.sid or the eldest ancestor that has the same sid as the current process."},
 	{PT_CHARBUF, EPF_NONE, PF_NA, "proc.sid.exepath", "Process Session Executable Path", "The full executable path of the current process's session leader. This is either the process with pid=proc.sid or the eldest ancestor that has the same sid as the current process."},
 	{PT_INT64, EPF_NONE, PF_ID, "proc.vpgid", "Process Virtual Group ID", "The process group id of the process generating the event, as seen from its current PID namespace."},
+	{PT_CHARBUF, EPF_NONE, PF_NA, "proc.vpgid.name", "Process Group Name", "The name of the current process's process group leader. This is either the process with proc.vpgid == proc.vpid or the eldest ancestor that has the same vpgid as the current process."},
+	{PT_CHARBUF, EPF_NONE, PF_NA, "proc.vpgid.exe", "Process Group First Argument", "The first command line argument argv[0] (usually the executable name or a custom one) of the current process's process group leader. This is either the process with proc.vpgid == proc.vpid or the eldest ancestor that has the same vpgid as the current process."},
+	{PT_CHARBUF, EPF_NONE, PF_NA, "proc.vpgid.exepath", "Process Group Executable Path", "The full executable path of the current process's process group leader. This is either the process with proc.vpgid == proc.vpid or the eldest ancestor that has the same vpgid as the current process."},
 	{PT_RELTIME, EPF_NONE, PF_DEC, "proc.duration", "Process Duration", "Number of nanoseconds since the process started."},
 	{PT_RELTIME, EPF_NONE, PF_DEC, "proc.ppid.duration", "Parent Process Duration", "Number of nanoseconds since the parent process started."},
 	{PT_RELTIME, EPF_NONE, PF_DEC, "proc.pid.ts", "Process start ts", "Start of process as epoch timestamp in nanoseconds."},
@@ -2892,6 +2895,138 @@ uint8_t* sinsp_filter_check_thread::extract(sinsp_evt *evt, OUT uint32_t* len, b
 
 				// mt has been updated to the highest process that has the same session id.
 				// mt's comm is considered the session leader.
+				m_tstr = mt->get_exepath();
+				RETURN_EXTRACT_STRING(m_tstr);
+			}
+		}
+		case TYPE_VPGID_NAME:
+		{
+			//
+			// Relying on the convention that a process group id is the process id of the process group leader
+			//
+			sinsp_threadinfo* vpgidinfo =
+				m_inspector->get_thread_ref(tinfo->m_vpgid, false, true).get();
+
+			if(vpgidinfo != NULL)
+			{
+				m_tstr = vpgidinfo->get_comm();
+				RETURN_EXTRACT_STRING(m_tstr);
+			}
+			else
+			{
+				// This can occur when the process group leader process has exited.
+				// Find the highest ancestor process that has the same process group id and
+				// declare it to be the process group leader.
+				sinsp_threadinfo* mt = tinfo->get_main_thread();
+
+				if(mt == NULL)
+				{
+					return NULL;
+				}
+
+				int64_t vpgid = mt->m_vpgid;
+				sinsp_threadinfo::visitor_func_t visitor = [vpgid, &mt] (sinsp_threadinfo *pt)
+				{
+					if(pt->m_vpgid != vpgid)
+					{
+						return false;
+					}
+					mt = pt;
+					return true;
+				};
+
+				mt->traverse_parent_state(visitor);
+
+				// mt has been updated to the highest process that has the same process group id.
+				// mt's comm is considered the process group leader.
+				m_tstr = mt->get_comm();
+				RETURN_EXTRACT_STRING(m_tstr);
+			}
+		}
+		case TYPE_VPGID_EXE:
+		{
+			//
+			// Relying on the convention that a process group id is the process id of the process group leader
+			//
+			sinsp_threadinfo* vpgidinfo =
+				m_inspector->get_thread_ref(tinfo->m_vpgid, false, true).get();
+
+			if(vpgidinfo != NULL)
+			{
+				m_tstr = vpgidinfo->get_exe();
+				RETURN_EXTRACT_STRING(m_tstr);
+			}
+			else
+			{
+				// This can occur when the process group leader process has exited.
+				// Find the highest ancestor process that has the same process group id and
+				// declare it to be the process group leader.
+				sinsp_threadinfo* mt = tinfo->get_main_thread();
+
+				if(mt == NULL)
+				{
+					return NULL;
+				}
+
+				int64_t vpgid = mt->m_vpgid;
+				sinsp_threadinfo::visitor_func_t visitor = [vpgid, &mt] (sinsp_threadinfo *pt)
+				{
+					if(pt->m_vpgid != vpgid)
+					{
+						return false;
+					}
+					mt = pt;
+					return true;
+				};
+
+				mt->traverse_parent_state(visitor);
+
+				// mt has been updated to the highest process that has the same process group id.
+				// mt's comm is considered the process group leader.
+				m_tstr = mt->get_exe();
+				RETURN_EXTRACT_STRING(m_tstr);
+			}
+		}
+		case TYPE_VPGID_EXEPATH:
+		{
+			//
+			// Relying on the convention that a process group id is the process id of the process group leader
+			//
+			sinsp_threadinfo* vpgidinfo =
+				m_inspector->get_thread_ref(tinfo->m_vpgid, false, true).get();
+
+			if(vpgidinfo != NULL)
+			{
+				m_tstr = vpgidinfo->get_exepath();
+				RETURN_EXTRACT_STRING(m_tstr);
+			}
+			else
+			{
+				// This can occur when the process group leader process has exited.
+				// Find the highest ancestor process that has the same process group id and
+				// declare it to be the process group leader.
+				sinsp_threadinfo* mt = tinfo->get_main_thread();
+
+				if(mt == NULL)
+				{
+					return NULL;
+				}
+
+				int64_t vpgid = mt->m_vpgid;
+				sinsp_threadinfo::visitor_func_t visitor = [vpgid, &mt] (sinsp_threadinfo *pt)
+				{
+					if(pt->m_vpgid != vpgid)
+					{
+						return false;
+					}
+					mt = pt;
+					return true;
+				};
+
+				mt->traverse_parent_state(visitor);
+
+				// mt has been updated to the highest process that has the same process group id.
+				// mt's comm is considered the process group leader.
 				m_tstr = mt->get_exepath();
 				RETURN_EXTRACT_STRING(m_tstr);
 			}
