3b5666f9df290dbf5a07d47da7c9ad10826f9481
Joseph Pittman <joseph.pittman@sysdig.com>
2023-07-18 13:42:32 -0400

    Remove ALWAYS_DROP setting for setsid system call
    Libsinsp contains a parser for the SETSID_X event, and updates the m_sid
    for the threadinfo object.  But the current ALWAYS_DROP setting for the
    SETSID syscall_table entry, means this parsing never happens, in the
    normal case where dropping mode is enabled in the kernel module/BPF probe.
    
    This change removes the ALWAYS_DROP setting for SETSID, so applications
    which rely on thread session IDs will have access to accurate information.
    
    Signed-off-by: Joseph Pittman <joseph.pittman@sysdig.com>

--- driver/syscall_table.c
+++ driver/syscall_table.c
@@ -320,7 +320,7 @@ const struct syscall_evt_pair g_syscall_table[SYSCALL_TABLE_SIZE] = {
 #ifdef __NR_chroot
 	[__NR_chroot - SYSCALL_TABLE_ID0] =                     {UF_USED | UF_NEVER_DROP, PPME_SYSCALL_CHROOT_E, PPME_SYSCALL_CHROOT_X, PPM_SC_CHROOT},
 #endif
-	[__NR_setsid - SYSCALL_TABLE_ID0] =                     {UF_USED | UF_ALWAYS_DROP, PPME_SYSCALL_SETSID_E, PPME_SYSCALL_SETSID_X, PPM_SC_SETSID},
+	[__NR_setsid - SYSCALL_TABLE_ID0] =                     {UF_USED, PPME_SYSCALL_SETSID_E, PPME_SYSCALL_SETSID_X, PPM_SC_SETSID},
 	[__NR_setpgid - SYSCALL_TABLE_ID0] =                    {UF_USED | UF_ALWAYS_DROP, PPME_SYSCALL_SETPGID_E, PPME_SYSCALL_SETPGID_X, PPM_SC_SETPGID},
 #ifdef __NR_bpf
 	[__NR_bpf - SYSCALL_TABLE_ID0] =                        {UF_USED, PPME_SYSCALL_BPF_2_E, PPME_SYSCALL_BPF_2_X, PPM_SC_BPF},
