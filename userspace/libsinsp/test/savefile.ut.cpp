c4e14939246f01fb8d94bd77c8617bc6f1f339ee
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2023-07-21 17:38:17 +0200

    new(tests): basic savefile test
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

---
+++ userspace/libsinsp/test/savefile.ut.cpp
@@ -0,0 +1,30 @@
+/*
+Copyright (C) 2023 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+#include "sinsp.h"
+
+#include <gtest/gtest.h>
+
+using namespace std;
+
+TEST(savefile, proclist)
+{
+	sinsp inspector;
+	inspector.open_savefile(RESOURCE_DIR "/sample.scap");
+
+	ASSERT_EQ(inspector.m_thread_manager->get_thread_count(), 94);
+}
