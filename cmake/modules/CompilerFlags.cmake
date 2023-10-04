205a5d7c57b653806e992ab90c6bbea199332d51
Leonardo Grasso <me@leonardograsso.com>
2023-10-04 18:31:45 +0200

    docs(cmake): add license header and SPDX identifier
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- cmake/modules/CompilerFlags.cmake
+++ cmake/modules/CompilerFlags.cmake
@@ -1,3 +1,17 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# Copyright (C) 2023 The Falco Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+
 option(BUILD_WARNINGS_AS_ERRORS "Enable building with -Wextra -Werror flags")
 
 option(ENABLE_PIC "Build position independent libraries and executables" ${BUILD_SHARED_LIBS})
