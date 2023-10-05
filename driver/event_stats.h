c042300bb7f401e8b9660270385c727af31a7e95
Leonardo Grasso <me@leonardograsso.com>
2023-10-05 12:27:07 +0200

    docs(driver): add license header and SPDX identifier
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- driver/event_stats.h
+++ driver/event_stats.h
@@ -1,3 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0-only OR MIT
+/*
+
+Copyright (C) 2023 The Falco Authors.
+
+This file is dual licensed under either the MIT or GPL 2. See MIT.txt
+or GPL2.txt for full copies of the license.
+
+*/
 #pragma once
 
 /* These numbers must be updated when we add new events in the event table */
--- driver/modern_bpf/programs/attached/events/sched_process_exit.bpf.c
+++ driver/modern_bpf/programs/attached/events/sched_process_exit.bpf.c
@@ -1,3 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0-only OR MIT
+/*
+ * Copyright (C) 2023 The Falco Authors.
+ *
+ * This file is dual licensed under either the MIT or GPL 2. See MIT.txt
+ * or GPL2.txt for full copies of the license.
+ */
+
 #include <helpers/interfaces/variable_size_event.h>
 #include <driver/systype_compat.h>
 #include <helpers/interfaces/attached_programs.h>
