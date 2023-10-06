54c2f43cd9b8ffa47bc08a5965795b2e0fc671a5
Federico Di Pierro <nierro92@gmail.com>
2023-10-06 09:11:20 +0200

    chore(test/drivers): added new tests for ia32 to check pushed params.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- test/drivers/helpers/ia32.c
+++ test/drivers/helpers/ia32.c
@@ -5,13 +5,24 @@
  */
 
 #include <unistd.h>
+#include <fcntl.h>
+#include <linux/openat2.h>  /* Definition of RESOLVE_* constants */
 #include <sys/syscall.h>
 #include <sys/types.h>
+#include <sys/socket.h>
 #include <linux/net.h>        /* Definition of SYS_* constants */
 
 int main() {
-	syscall(__NR_close, -1);
+	struct open_how how;
+	how.flags = O_RDWR;
+	how.mode = 0;
+	how.resolve = RESOLVE_BENEATH | RESOLVE_NO_MAGICLINKS;
+	syscall(__NR_openat2, 11, "mock_path", &how, sizeof(struct open_how));
+	syscall(__NR_write, 17, NULL, 1013);
 	unsigned long args[3] = {0};
+	args[0] = AF_INET;
+	args[1] = SOCK_RAW;
+	args[2] = PF_INET;
 	syscall(__NR_socketcall, SYS_SOCKET, args);
 	syscall(__NR_socketcall, SYS_ACCEPT4, args);
 	syscall(__NR_socketcall, SYS_SEND, args);
