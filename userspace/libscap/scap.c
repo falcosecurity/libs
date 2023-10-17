d440f436eb212fda548cdb4f4d85d28a90562703
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2023-10-17 15:43:44 +0200

    cleanup(scap): move scap_alloc_proclist_info to scap_procs.c
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

--- userspace/libscap/scap.c
+++ userspace/libscap/scap.c
@@ -464,39 +464,6 @@ const char* scap_get_host_root()
 	return env_str;
 }
 
-bool scap_alloc_proclist_info(struct ppm_proclist_info **proclist_p, uint32_t n_entries, char* error)
-{
-	uint32_t memsize;
-
-	if(n_entries >= SCAP_DRIVER_PROCINFO_MAX_SIZE)
-	{
-		snprintf(error, SCAP_LASTERR_SIZE, "driver process list too big");
-		return false;
-	}
-
-	memsize = sizeof(struct ppm_proclist_info) +
-		sizeof(struct ppm_proc_info) * n_entries;
-
-	struct ppm_proclist_info *procinfo = (struct ppm_proclist_info*) realloc(*proclist_p, memsize);
-	if(procinfo == NULL)
-	{
-		free(*proclist_p);
-		*proclist_p = NULL;
-		snprintf(error, SCAP_LASTERR_SIZE, "driver process list allocation error");
-		return false;
-	}
-
-	if(*proclist_p == NULL)
-	{
-		procinfo->n_entries = 0;
-	}
-
-	procinfo->max_entries = n_entries;
-	*proclist_p = procinfo;
-
-	return true;
-}
-
 uint64_t scap_ftell(scap_t *handle)
 {
 	if(handle->m_vtable->savefile_ops)
