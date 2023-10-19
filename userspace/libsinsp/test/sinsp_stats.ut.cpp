de038b98e2aa2f67c0a745e73f583350c2c5fb6c
Melissa Kilby <melissa.kilby.oss@gmail.com>
2023-10-19 05:59:28 +0000

    new(test): add sinsp_stats unit test
    Signed-off-by: Melissa Kilby <melissa.kilby.oss@gmail.com>

---
+++ userspace/libsinsp/test/sinsp_stats.ut.cpp
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: Apache-2.0
+/*
+Copyright (C) 2023 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+#include <gtest/gtest.h>
+#include "utils.h"
+#include "sinsp_with_test_input.h"
+#include "test_utils.h"
+#include "scap_stats_v2.h"
+
+
+TEST_F(sinsp_with_test_input, sinsp_stats_v2_resource_utilization)
+{
+#ifdef __linux__
+
+	open_inspector(SCAP_MODE_LIVE);
+	const scap_agent_info* agent_info = m_inspector.get_agent_info();
+	
+	uint32_t nstats = 0;
+	int32_t rc;
+	const scap_stats_v2* sinsp_stats_v2_snapshot;
+	auto buffer = m_inspector.get_sinsp_stats_v2_buffer();
+	uint32_t flags = PPM_SCAP_STATS_RESOURCE_UTILIZATION;
+	sinsp_stats_v2_snapshot = libsinsp::stats::get_sinsp_stats_v2(flags, agent_info, buffer, &nstats, &rc);
+    /* Extra call */
+    sinsp_stats_v2_snapshot = libsinsp::stats::get_sinsp_stats_v2(flags, agent_info, buffer, &nstats, &rc);
+
+    ASSERT_EQ(nstats, SINSP_RESOURCE_UTILIZATION_FDS_TOTAL_HOST + 1);
+    ASSERT_EQ(rc, SCAP_SUCCESS);
+
+    /* These names should always be available */
+	std::unordered_set<std::string> minimal_stats_names = {"cpu_usage_perc", "memory_rss", "memory_vsz", "memory_pss", "container_memory_used", "cpu_usage_perc_total_host", "procs_running_host", "memory_used_host", "open_fds_host"};
+
+	uint32_t i = 0;
+	for(const auto& stat_name : minimal_stats_names)
+	{
+		for(i = 0; i < nstats; i++)
+		{
+			if(stat_name.compare(sinsp_stats_v2_snapshot[i].name) == 0)
+			{
+				break;
+			}
+		}
+
+		if(i == nstats)
+		{
+			FAIL() << "unable to find stat '" << stat_name << "' in the sinsp_stats_v2 buffer";
+		}
+	}
+
+    /* Assert values are greater than 0 */
+	ASSERT_GT(sinsp_stats_v2_snapshot[SINSP_RESOURCE_UTILIZATION_CPU_PERC].value.d, 0);
+	ASSERT_GT(sinsp_stats_v2_snapshot[SINSP_RESOURCE_UTILIZATION_MEMORY_RSS].value.u32, 0);
+    ASSERT_GT(sinsp_stats_v2_snapshot[SINSP_RESOURCE_UTILIZATION_MEMORY_VSZ].value.u32, 0);
+	ASSERT_GT(sinsp_stats_v2_snapshot[SINSP_RESOURCE_UTILIZATION_CPU_PERC_TOTAL_HOST].value.d, 0);	
+	ASSERT_GT(sinsp_stats_v2_snapshot[SINSP_RESOURCE_UTILIZATION_MEMORY_TOTAL_HOST].value.u64, 0);
+    ASSERT_GT(sinsp_stats_v2_snapshot[SINSP_RESOURCE_UTILIZATION_PROCS_HOST].value.u32, 0);	
+    ASSERT_GT(sinsp_stats_v2_snapshot[SINSP_RESOURCE_UTILIZATION_FDS_TOTAL_HOST].value.u64, 0);
+
+    /* Empty call */
+    nstats = 0;
+    flags = 0;
+    sinsp_stats_v2_snapshot = libsinsp::stats::get_sinsp_stats_v2(flags, agent_info, buffer, &nstats, &rc);
+    ASSERT_EQ(nstats, 0);
+    ASSERT_EQ(rc, SCAP_SUCCESS);
+
+#endif
+}
