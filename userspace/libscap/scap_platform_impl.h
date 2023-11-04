d24085c9f5f717356f1fb6ef0f3545f12204ba3e
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2023-11-04 23:16:24 +0100

    cleanup(scap): remove proclist from vtable->get_proc
    It was always unclear (to me) what the proclist is used for, exactly.
    It turns out it's just a holder for the callback which was ignored
    in the previous implementation and now is replaced by a completely
    different temporary proclist.
    
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

--- userspace/libscap/scap_platform_impl.h
+++ userspace/libscap/scap_platform_impl.h
@@ -58,7 +58,7 @@ struct scap_platform_vtable
 	// XXX this is Linux-specific
 	uint32_t (*get_device_by_mount_id)(struct scap_platform*, const char *procdir, unsigned long requested_mount_id);
 
-	struct scap_threadinfo* (*get_proc)(struct scap_platform*, struct scap_proclist* proclist, int64_t tid, bool scan_sockets);
+	struct scap_threadinfo* (*get_proc)(struct scap_platform*, int64_t tid, bool scan_sockets);
 
 	int32_t (*refresh_proc_table)(struct scap_platform*, struct scap_proclist* proclist);
 	bool (*is_thread_alive)(struct scap_platform*, int64_t pid, int64_t tid, const char* comm);
