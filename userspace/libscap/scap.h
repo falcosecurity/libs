fbc2432a99275e9b6c6a9497c5c26071c72babb9
Andrea Terzolo <andreaterzolo3@gmail.com>
2023-11-21 11:02:54 +0100

    chore: bump API and SCHEMA version
    Signed-off-by: Andrea Terzolo <andreaterzolo3@gmail.com>

--- userspace/libscap/scap.h
+++ userspace/libscap/scap.h
@@ -101,7 +101,7 @@ struct scap_vtable;
 // call `scap_get_driver_api_version()` and/or `scap_get_driver_schema_version()`
 // and handle the result
 //
-#define SCAP_MINIMUM_DRIVER_API_VERSION PPM_API_VERSION(6, 0, 0)
+#define SCAP_MINIMUM_DRIVER_API_VERSION PPM_API_VERSION(7, 0, 0)
 #define SCAP_MINIMUM_DRIVER_SCHEMA_VERSION PPM_API_VERSION(2, 0, 0)
 
 // 
