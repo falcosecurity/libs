95ee0e7a0f94621a0c99f5fd1a7ac254a4c70ff5
Melissa Kilby <melissa.kilby.oss@gmail.com>
2023-12-01 02:26:26 +0000

    update(libsinsp/test): add unit tests for concatenate_paths
    Signed-off-by: Melissa Kilby <melissa.kilby.oss@gmail.com>

--- userspace/libsinsp/test/sinsp_utils.ut.cpp
+++ userspace/libsinsp/test/sinsp_utils.ut.cpp
@@ -19,8 +19,153 @@ limitations under the License.
 #include <gtest/gtest.h>
 #include "utils.h"
 
-// copy_and_sanitize_path is not exported in utils.h
+// Following helpers are not exported in utils.h
 void copy_and_sanitize_path(char* target, char* targetbase, const char* path, char separator);
+void rewind_to_parent_path(char* targetbase, char** tc, const char** pc, uint32_t delta);
+
+TEST(sinsp_utils_test, concatenate_paths)
+{
+	char fullpath[SCAP_MAX_PATH_SIZE];
+	std::string path1;
+	std::string path2;
+	bool res;
+
+	/*
+	 * SUCCESS concatenate_paths
+	*/
+
+	path2 = "dir/term";
+	res = sinsp_utils::concatenate_paths(fullpath, SCAP_MAX_PATH_SIZE,
+									"\0",
+									0,
+									path2.c_str(),
+									(uint32_t)path2.length());
+	EXPECT_EQ(path2, std::string(fullpath));
+	ASSERT_TRUE(res);
+
+	path1 = "//";
+	path2 = "dir/term";
+	res = sinsp_utils::concatenate_paths(fullpath, SCAP_MAX_PATH_SIZE,
+									path1.c_str(),
+									(uint32_t)path1.length(),
+									path2.c_str(),
+									(uint32_t)path2.length());
+	EXPECT_EQ("//dir/term", std::string(fullpath));
+	ASSERT_TRUE(res);
+
+	path1 = "////";
+	path2 = "dir/term/";
+	res = sinsp_utils::concatenate_paths(fullpath, SCAP_MAX_PATH_SIZE,
+									path1.c_str(),
+									(uint32_t)path1.length(),
+									path2.c_str(),
+									(uint32_t)path2.length());
+	EXPECT_EQ("////dir/term", std::string(fullpath));
+	ASSERT_TRUE(res);
+
+	path1 = "///../.../../";
+	path2 = "dir/term/";
+	res = sinsp_utils::concatenate_paths(fullpath, SCAP_MAX_PATH_SIZE,
+									path1.c_str(),
+									(uint32_t)path1.length(),
+									path2.c_str(),
+									(uint32_t)path2.length());
+	EXPECT_EQ("///../.../../dir/term", std::string(fullpath));
+	ASSERT_TRUE(res);
+
+	path1 = "///../.../";
+	path2 = "dir/./././///./term/";
+	res = sinsp_utils::concatenate_paths(fullpath, SCAP_MAX_PATH_SIZE,
+									path1.c_str(),
+									(uint32_t)path1.length(),
+									path2.c_str(),
+									(uint32_t)path2.length());
+	EXPECT_EQ("///../.../dir/term", std::string(fullpath));
+	ASSERT_TRUE(res); // only path2 gets sanitized and ./ or multiple //// removed
+
+	path1 = "../.../";
+	path2 = "dir/././././../../.../term/";
+	res = sinsp_utils::concatenate_paths(fullpath, SCAP_MAX_PATH_SIZE,
+									path1.c_str(),
+									(uint32_t)path1.length(),
+									path2.c_str(),
+									(uint32_t)path2.length());
+	EXPECT_EQ("../.../term", std::string(fullpath));
+	ASSERT_TRUE(res); // only path2 gets sanitized and ./ removed and directory traversed up
+
+	path1 = "././";
+	path2 = "./dir/term";
+	res = sinsp_utils::concatenate_paths(fullpath, SCAP_MAX_PATH_SIZE,
+									path1.c_str(),
+									(uint32_t)path1.length(),
+									path2.c_str(),
+									(uint32_t)path2.length());
+	EXPECT_EQ("././dir/term", std::string(fullpath));
+	ASSERT_TRUE(res); // only path2 gets sanitized and ./ removed
+
+
+	/*
+	 * FAILED concatenate_paths
+	*/
+
+	res = sinsp_utils::concatenate_paths(fullpath, SCAP_MAX_PATH_SIZE,
+									"\0",
+									0,
+									"\0",
+									0);
+	EXPECT_EQ("\0", std::string(fullpath));
+	ASSERT_FALSE(res); // nothing to concat as path2 is empty
+
+	path1 = "//";
+	res = sinsp_utils::concatenate_paths(fullpath, SCAP_MAX_PATH_SIZE,
+									path1.c_str(),
+									(uint32_t)path1.length(),
+									"\0",
+									 0);
+	EXPECT_EQ("\0", std::string(fullpath));
+	ASSERT_FALSE(res); // nothing to concat as path2 is empty
+
+	path2 = "/dir/term";
+	res = sinsp_utils::concatenate_paths(fullpath, SCAP_MAX_PATH_SIZE,
+									"\0",
+									0,
+									path2.c_str(),
+									(uint32_t)path2.length());
+	EXPECT_EQ(path2, std::string(fullpath));
+	ASSERT_FALSE(res); // because path2 is absolute
+
+	path1 = "//";
+	path2 = "/dir/term";
+	res = sinsp_utils::concatenate_paths(fullpath, SCAP_MAX_PATH_SIZE,
+									path1.c_str(),
+									(uint32_t)path1.length(),
+									path2.c_str(),
+									(uint32_t)path2.length());
+	EXPECT_EQ("/dir/term", std::string(fullpath));
+	ASSERT_FALSE(res); // because path2 is absolute
+
+	path1 = "//";
+	path2 = "////dir/../../././term";
+	res = sinsp_utils::concatenate_paths(fullpath, SCAP_MAX_PATH_SIZE,
+									path1.c_str(),
+									(uint32_t)path1.length(),
+									path2.c_str(),
+									(uint32_t)path2.length());
+	EXPECT_EQ("/term", std::string(fullpath));
+	ASSERT_FALSE(res); // because path2 is absolute, path2 gets directory traversed up
+
+	path1 = "//";
+	path2 = "////";
+	res = sinsp_utils::concatenate_paths(fullpath, SCAP_MAX_PATH_SIZE,
+									path1.c_str(),
+									(uint32_t)path1.length(),
+									path2.c_str(),
+									(uint32_t)path2.length());
+	EXPECT_EQ("/", std::string(fullpath));
+	ASSERT_FALSE(res); // because not actually paths here just repeated separators
+
+}
+
 
 TEST(sinsp_utils_test, copy_and_sanitize_path)
 {
