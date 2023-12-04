9d572b91fa4ab15334adc73ed2db6aa64637bc22
Melissa Kilby <melissa.kilby.oss@gmail.com>
2023-12-04 23:39:38 +0000

    new(libsinsp/test): test container filterchecks w/ mock API input data
    Signed-off-by: Melissa Kilby <melissa.kilby.oss@gmail.com>

--- userspace/libsinsp/test/container_engine/container_parser_cri_containerd.ut.cpp
+++ userspace/libsinsp/test/container_engine/container_parser_cri_containerd.ut.cpp
@@ -838,5 +838,78 @@ TEST_F(sinsp_with_test_input, container_parser_cri_containerd)
 	ASSERT_EQ(1073741824, container.m_memory_limit);
 	ASSERT_EQ(50000, container.m_cpu_quota);
 
+    /* In addition test sinsp filterchecks, similar to spawn_process_container test. */
+	add_default_init_thread();
+	open_inspector();
+	sinsp_evt *evt = NULL;
+
+	uint64_t parent_pid = 1, parent_tid = 1, child_pid = 20, child_tid = 20;
+	uint64_t fdlimit = 1024, pgft_maj = 0, pgft_min = 1;
+	uint64_t exe_ino = 242048, ctime = 1676262698000004577, mtime = 1676262698000004588;
+	uint32_t loginuid = UINT32_MAX, euid = UINT32_MAX;
+
+	scap_const_sized_buffer empty_bytebuf = {.buf = nullptr, .size = 0};
+
+	add_event_advance_ts(increasing_ts(), parent_tid, PPME_SYSCALL_CLONE_20_E, 0);
+	std::vector<std::string> cgroups = {
+		"cgroups=cpuset=/k8s.io/3ad7b26ded6d8e7b23da7d48fe889434573036c27ae5a74837233de441c3601e",
+		"cpu=/k8s.io/3ad7b26ded6d8e7b23da7d48fe889434573036c27ae5a74837233de441c3601e", "cpuacct=/",
+		"blkio=/k8s.io/3ad7b26ded6d8e7b23da7d48fe889434573036c27ae5a74837233de441c3601e",
+		"memory=/k8s.io/3ad7b26ded6d8e7b23da7d48fe889434573036c27ae5a74837233de441c3601e"};
+	std::string cgroupsv = test_utils::to_null_delimited(cgroups);
+	std::vector<std::string> env = {"SHELL=/bin/bash", "PWD=/home/user", "HOME=/home/user"};
+	std::string envv = test_utils::to_null_delimited(env);
+	std::vector<std::string> args = {"-c", "'echo aGVsbG8K | base64 -d'"};
+	std::string argsv = test_utils::to_null_delimited(args);
+
+	std::string container_json = m_inspector.m_container_manager.container_to_json(container);
+	// {"container":{"Mounts":[{"Destination":"/host/boot","Mode":"","Propagation":"private","RW":false,"Source":"/boot"}],"User":"<NA>","cni_json":"{\"bridge\":{\"IPConfigs\":null},\"eth0\":{\"IPConfigs\":[{\"Gateway\":\"10.244.0.1\",\"IP\":\"10.244.0.2\"}]}}","cpu_period":100,"cpu_quota":50000,"cpu_shares":102,"cpuset_cpu_count":0,"created_time":-4702111234474983746,"env":[],"full_id":"3ad7b26ded6d8e7b23da7d48fe889434573036c27ae5a74837233de441c3601e","id":"3ad7b26ded6d","image":"docker.io/library/busybox:latest","imagedigest":"sha256:3fbc632167424a6d997e74f52b878d7cc478225cffac6bc977eedfe51c7f4e79","imageid":"","imagerepo":"docker.io/library/busybox","imagetag":"latest","ip":"10.244.0.2","is_pod_sandbox":false,"labels":{"io.kubernetes.container.name":"busybox","io.kubernetes.pod.name":"nginx-sandbox","io.kubernetes.pod.namespace":"default","io.kubernetes.pod.uid":"hdishddjaidwnduw9a43535366368","io.kubernetes.sandbox.id":"63060edc2d3aa803ab559f2393776b151f99fc5b05035b21db66b3b62246ad6a"},"lookup_state":1,"memory_limit":1073741824,"metadata_deadline":0,"name":"busybox","port_mappings":[],"privileged":true,"swap_limit":1073741824,"type":8}}
+
+	add_event_advance_ts(increasing_ts(), parent_tid, PPME_SYSCALL_CLONE_20_X, 20, child_tid, "bash", empty_bytebuf,
+			     (uint64_t)1, (uint64_t)1, (uint64_t)0, "", fdlimit, pgft_maj, pgft_min, (uint32_t)12088,
+			     (uint32_t)7208, (uint32_t)0, "bash",
+			     scap_const_sized_buffer{cgroupsv.data(), cgroupsv.size()},
+			     (uint32_t)(PPM_CL_CLONE_CHILD_CLEARTID | PPM_CL_CLONE_CHILD_SETTID | PPM_CL_CLONE_NEWPID |
+					PPM_CL_CHILD_IN_PIDNS),
+			     (uint32_t)1000, (uint32_t)1000, (uint64_t)parent_tid, (uint64_t)parent_pid);
+	add_event_advance_ts(increasing_ts(), child_tid, PPME_SYSCALL_CLONE_20_X, 20, (uint64_t)0, "bash",
+			     empty_bytebuf, child_tid, child_pid, (uint64_t)1, "", fdlimit, pgft_maj, pgft_min,
+			     (uint32_t)12088, (uint32_t)3764, (uint32_t)0, "bash",
+			     scap_const_sized_buffer{cgroupsv.data(), cgroupsv.size()},
+			     (uint32_t)(PPM_CL_CLONE_CHILD_CLEARTID | PPM_CL_CLONE_CHILD_SETTID | PPM_CL_CLONE_NEWPID |
+					PPM_CL_CHILD_IN_PIDNS),
+			     (uint32_t)1000, (uint32_t)1000, (uint64_t)1, (uint64_t)1);
+	add_event_advance_ts(increasing_ts(), -1, PPME_CONTAINER_JSON_2_E, 1, container_json.c_str());
+
+	add_event_advance_ts(increasing_ts(), child_tid, PPME_SYSCALL_EXECVE_19_E, 1, "/bin/test-exe");
+	evt = add_event_advance_ts(increasing_ts(), child_tid, PPME_SYSCALL_EXECVE_19_X, 27, (int64_t)0,
+				   "/bin/test-exe", scap_const_sized_buffer{argsv.data(), argsv.size()}, child_tid,
+				   child_pid, parent_tid, "", fdlimit, pgft_maj, pgft_min, (uint32_t)29612, (uint32_t)4,
+				   (uint32_t)0, "test-exe", scap_const_sized_buffer{cgroupsv.data(), cgroupsv.size()},
+				   scap_const_sized_buffer{envv.data(), envv.size()}, (int32_t)34818, parent_pid,
+				   loginuid, (int32_t)PPM_EXE_UPPER_LAYER, parent_pid, parent_pid, parent_pid, exe_ino,
+				   ctime, mtime, euid);
+
+	ASSERT_EQ(get_field_as_string(evt, "container.id"), "3ad7b26ded6d");
+	ASSERT_EQ(get_field_as_string(evt, "container.image"), "docker.io/library/busybox:latest");
+	ASSERT_EQ(get_field_as_string(evt, "container.image.repository"), "docker.io/library/busybox");
+	ASSERT_EQ(get_field_as_string(evt, "container.image.tag"), "latest");
+	ASSERT_EQ(get_field_as_string(evt, "container.ip"), "10.244.0.2");
+	ASSERT_EQ(get_field_as_string(evt, "container.cni.json"),
+		  "{\"bridge\":{\"IPConfigs\":null},\"eth0\":{\"IPConfigs\":[{\"Gateway\":\"10.244.0.1\",\"IP\":\"10."
+		  "244.0.2\"}]}}");
+	ASSERT_EQ(get_field_as_string(evt, "container.name"), "busybox");
+	ASSERT_EQ(get_field_as_string(evt, "container.privileged"), "true");
+	ASSERT_EQ(get_field_as_string(evt, "k8s.ns.name"), "default");
+	ASSERT_EQ(get_field_as_string(evt, "k8s.pod.name"), "nginx-sandbox");
+	ASSERT_EQ(get_field_as_string(evt, "container.mounts"), "/boot:/host/boot::false:private");
+	ASSERT_EQ(get_field_as_string(evt, "container.mount.source[0]"), "/boot");
+	ASSERT_EQ(get_field_as_string(evt, "container.mount.dest[0]"), "/host/boot");
+
+	ASSERT_EQ(get_field_as_string(evt, "proc.vpid"), "1");
+	ASSERT_EQ(get_field_as_string(evt, "thread.vtid"), "1");
+	ASSERT_EQ(get_field_as_string(evt, "user.loginuid"), "-1");
+	ASSERT_EQ(get_field_as_string(evt, "proc.is_exe_upper_layer"), "true");
+	ASSERT_EQ(get_field_as_string(evt, "user.uid"), "4294967295");
 }
 #endif // MINIMAL_BUILD
