3c7463448c375507acd6751adb1e0139f23c9731
Melissa Kilby <melissa.kilby.oss@gmail.com>
2023-12-08 07:39:33 +0000

    new(libsinsp/test): simple test for sinsp_dns_manager
    Signed-off-by: Melissa Kilby <melissa.kilby.oss@gmail.com>

---
+++ userspace/libsinsp/test/dns_manager.ut.cpp
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: Apache-2.0
+/*
+Copyright (C) 2023 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+#if defined(HAS_CAPTURE) && !defined(CYGWING_AGENT) && !defined(_WIN32) && !defined(__EMSCRIPTEN__)
+#include "sinsp_with_test_input.h"
+#include <dns_manager.h>
+#include <gtest/gtest.h>
+
+TEST_F(sinsp_with_test_input, simple_dns_manager_invocation) 
+{
+    // Simple dummy test to assert that sinsp_dns_manager is invocated correctly
+    // and not leaking memory
+    const char* name = "bogus";
+    uint64_t ts = 11111111111111;
+    uint32_t addr = 111111;
+    bool result = sinsp_dns_manager::get().match(name, AF_INET, &addr, ts);
+    ASSERT_FALSE(result);
+}
+#endif
