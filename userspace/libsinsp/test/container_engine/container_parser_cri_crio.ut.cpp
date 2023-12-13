969fe1ff55c2997b6b35c8d5895c80517d5324be
Melissa Kilby <melissa.kilby.oss@gmail.com>
2023-12-13 19:38:48 +0000

    feat/fix(cri): send correct k8s.pod.id and introduce k8s.pod.uid
    * k8s.pod.id incorrectly sends the pod uid which is a different identifier. Note that this is not a refactor nor a change intended for semantic adjustments, it is a bug.
    * introduce k8s.pod.uid instead so the uid field is still accesible
    * do not yet refactor the container engine and continue extracting these fields from the container labels
    
    Signed-off-by: Melissa Kilby <melissa.kilby.oss@gmail.com>

--- userspace/libsinsp/test/container_engine/container_parser_cri_crio.ut.cpp
+++ userspace/libsinsp/test/container_engine/container_parser_cri_crio.ut.cpp
@@ -722,8 +722,7 @@ TEST_F(sinsp_with_test_input, container_parser_cri_crio)
 
 	ASSERT_EQ(get_field_as_string(evt, "k8s.ns.name"), "redhat.test.crio");
 	ASSERT_EQ(get_field_as_string(evt, "k8s.pod.name"), "podsandbox1");
-	// todo @incertum fix pod id with next refactor, pod.id is sandbox id not pod uid
-	ASSERT_EQ(get_field_as_string(evt, "k8s.pod.id"), "redhat-test-crio");
+	ASSERT_EQ(get_field_as_string(evt, "k8s.pod.id"), "1f04600dc694");
 	// todo @Andreagit97 refactor pod labels queries to k8s.pod.label[example.label]
 	ASSERT_EQ(get_field_as_string(evt, "k8s.pod.label.example.label/custom"), "mylabel");
 	ASSERT_EQ(get_field_as_string(evt, "k8s.pod.labels"), "app:myapp, example.label/custom:mylabel");
