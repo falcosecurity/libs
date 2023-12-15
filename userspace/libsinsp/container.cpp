a09c9a54c62f8c68db2f8863f6b2df0c83206b11
Luca Guerra <luca@guerra.sh>
2023-12-15 14:47:47 +0000

    fix(libsinsp): fix -WReorder warning
    Signed-off-by: Luca Guerra <luca@guerra.sh>

--- userspace/libsinsp/container.cpp
+++ userspace/libsinsp/container.cpp
@@ -43,8 +43,8 @@ limitations under the License.
 using namespace libsinsp;
 
 sinsp_container_manager::sinsp_container_manager(sinsp* inspector, bool static_container, const std::string static_id, const std::string static_name, const std::string static_image) :
-	m_inspector(inspector),
 	m_last_flush_time_ns(0),
+	m_inspector(inspector),
 	m_static_container(static_container),
 	m_static_id(static_id),
 	m_static_name(static_name),
