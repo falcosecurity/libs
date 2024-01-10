85b08a24e7f78925ff39e121b3d55c97b639d084
Roberto Scolaro <roberto.scolaro21@gmail.com>
2024-01-10 11:35:17 +0000

    fix: address review comments
    Signed-off-by: Roberto Scolaro <roberto.scolaro21@gmail.com>

--- userspace/libsinsp/test/savefile.ut.cpp
+++ userspace/libsinsp/test/savefile.ut.cpp
@@ -74,4 +74,111 @@ TEST(savefile, filter)
 
 	unlink(filtered_scap);
 }
+
+int n_opens = 0;
+int n_closes = 0;
+
+void open_cb()
+{
+	n_opens +=1;
+}
+
+void close_cb()
+{
+	n_closes += 1;
+}
+
+TEST(savefile, cycledumper_num_events)
+{
+	char capture_scap[] = "capture.XXXXXX.scap";
+	int events_per_capture = 100;
+
+	n_opens = 0;
+	n_closes = 0;
+
+	int capture_fd = mkstemps(capture_scap, strlen(".scap"));
+	ASSERT_NE(capture_fd, -1);
+	close(capture_fd);
+
+	{
+		sinsp inspector;
+		inspector.open_savefile(RESOURCE_DIR "/sample.scap");
+
+		auto dumper = std::make_unique<sinsp_cycledumper>(&inspector, capture_scap,
+								  0, 0, 0, events_per_capture, true);
+
+		std::vector<std::function<void()>> open_cbs  = {std::bind(&open_cb)};
+		std::vector<std::function<void()>> close_cbs = {std::bind(&close_cb)};
+
+		dumper->set_callbacks(open_cbs, close_cbs);
+
+		int32_t res;
+		sinsp_evt* evt;
+		do
+		{
+			res = inspector.next(&evt);
+			EXPECT_NE(res, SCAP_FAILURE);
+			if(res != SCAP_EOF)
+			{
+				dumper->dump(evt);
+			}
+		}
+		while(res != SCAP_EOF);
+
+		dumper->close();
+		inspector.close();
+	}
+
+	ASSERT_EQ(n_opens, 5);
+	ASSERT_EQ(n_closes, 6); // a autodump_stop is called before starting.
+
+	unlink(capture_scap);
+}
+
+TEST(savefile, cycledumper_seconds)
+{
+	char capture_scap[] = "capture.XXXXXX.scap";
+	int seconds_per_capture = 1;
+
+	n_opens = 0;
+	n_closes = 0;
+
+	int capture_fd = mkstemps(capture_scap, strlen(".scap"));
+	ASSERT_NE(capture_fd, -1);
+	close(capture_fd);
+
+	{
+		sinsp inspector;
+		inspector.open_savefile(RESOURCE_DIR "/sample.scap");
+
+		auto dumper = std::make_unique<sinsp_cycledumper>(&inspector, capture_scap,
+								  0, seconds_per_capture, 0,  0, true);
+
+		std::vector<std::function<void()>> open_cbs  = {std::bind(&open_cb)};
+		std::vector<std::function<void()>> close_cbs = {std::bind(&close_cb)};
+
+		dumper->set_callbacks(open_cbs, close_cbs);
+
+		int32_t res;
+		sinsp_evt* evt;
+		do
+		{
+			res = inspector.next(&evt);
+			EXPECT_NE(res, SCAP_FAILURE);
+			if(res != SCAP_EOF)
+			{
+				dumper->dump(evt);
+			}
+		}
+		while(res != SCAP_EOF);
+
+		dumper->close();
+		inspector.close();
+	}
+
+	ASSERT_EQ(n_opens, 1);
+	ASSERT_EQ(n_closes, 2); // a autodump_stop is called before starting.
+
+	unlink(capture_scap);
+}
 #endif
