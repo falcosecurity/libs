8d6ef79c4a918d8d35d09a4a180ca02092156f81
Melissa Kilby <melissa.kilby.oss@gmail.com>
2024-01-20 02:53:56 +0000

    cleanup(libsinsp): adjust setting max_thread_table_size
    Signed-off-by: Melissa Kilby <melissa.kilby.oss@gmail.com>

--- userspace/libsinsp/threadinfo.h
+++ userspace/libsinsp/threadinfo.h
@@ -883,7 +883,7 @@ VISIBILITY_PRIVATE
 	uint64_t m_last_flush_time_ns;
 	// Increased legacy default of 131072 in January 2024 to prevent
 	// possible drops due to full threadtable on more modern servers
-	const uint32_t m_thread_table_absolute_max_size = 262144;
+	const uint32_t m_thread_table_default_size = 262144;
 	uint32_t m_max_thread_table_size;
 	int32_t m_n_proc_lookups = 0;
 	uint64_t m_n_proc_lookups_duration_ns = 0;
