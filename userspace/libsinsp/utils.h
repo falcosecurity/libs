ff39fd3d4b39a4450116b0b2f5db2eba67e3a3d2
Federico Di Pierro <nierro92@gmail.com>
2024-01-25 16:58:26 +0100

    fix(userspace/libsinsp): revert to old `concatenate_paths` helper function for perf reasons.
    Also, added some small fixes and lots of new tests.
    
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- userspace/libsinsp/utils.h
+++ userspace/libsinsp/utils.h
@@ -95,10 +95,11 @@ public:
 
 	//
 	// Concatenate posix-style path1 and path2 up to max_len in size, normalizing the result.
+	// path1 MUST be '/' terminated and is not sanitized.
 	// If path2 is absolute, the result will be equivalent to path2.
 	// If the result would be too long, the output will contain the string "/PATH_TOO_LONG" instead.
 	//
-	static std::string concatenate_paths(std::string_view path1, std::string_view path2, size_t max_len=SCAP_MAX_PATH_SIZE-1);
+	static std::string concatenate_paths(std::string_view path1, std::string_view path2);
 
 	//
 	// Determines if an IPv6 address is IPv4-mapped
