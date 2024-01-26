b24089d788bfa399522c5d29f5594afbf7550c55
Roberto Scolaro <roberto.scolaro21@gmail.com>
2024-01-26 14:03:30 +0000

    fix: remove some relative paths leftovers
    Signed-off-by: Roberto Scolaro <roberto.scolaro21@gmail.com>

--- userspace/libscap/engine/test_input/test_input_public.h
+++ userspace/libscap/engine/test_input/test_input_public.h
@@ -14,7 +14,7 @@ limitations under the License.
 
 #pragma once
 
-#include "scap_test.h"
+#include <libscap/engine/test_input/scap_test.h>
 #include <libscap/scap_procs.h>
 
 #define TEST_INPUT_ENGINE "test_input"
