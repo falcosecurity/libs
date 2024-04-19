135f67a259d4ebc21f6e727962143395f4f5a189
Federico Di Pierro <nierro92@gmail.com>
2024-04-19 11:07:16 +0200

    fix(test/drivers): fixed sendmsgX_fail and socketcall_sendmsgX_fail tests.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- test/drivers/test_suites/syscall_exit_suite/sendmsg_x.cpp
+++ test/drivers/test_suites/syscall_exit_suite/sendmsg_x.cpp
@@ -175,23 +175,7 @@ TEST(SyscallExit, sendmsgX_fail)
 
 	evt_test->disable_capture();
 
-#ifdef __s390x__
-	/* For some reason this test is successful also with old bpf probe on s390x */
-	if(evt_test->is_modern_bpf_engine()	|| evt_test->is_bpf_engine())
-#else
-	if(evt_test->is_modern_bpf_engine())
-#endif
-	{
-		evt_test->assert_event_presence();
-	}
-	else
-	{
-		/* we need to rewrite the logic in old drivers to support this partial collection
-		 * right now we drop the entire event.
-		 */
-		evt_test->assert_event_absence();
-		GTEST_SKIP() << "[SENDMSG_X]: what we receive is correct but we need to reimplement it, see the code" << std::endl;
-	}
+	evt_test->assert_event_presence();
 
 	if(HasFatalFailure())
 	{
