2c0e943c933ff68699165cca7fdf536c068c7751
Jason Dellaluce <jasondellaluce@gmail.com>
2024-05-06 16:38:22 +0000

    fix(userspace/libsinsp): consistently check storage size when parsing value string
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- userspace/libsinsp/value_parser.cpp
+++ userspace/libsinsp/value_parser.cpp
@@ -26,6 +26,20 @@ limitations under the License.
 #include <netdb.h>
 #endif
 
+static inline void check_storage_size(
+	const char* str, std::string::size_type storage_len, std::string::size_type used_len)
+{
+	if (used_len > storage_len) {
+		throw sinsp_exception(
+			+ "filter parameter too long (used="
+			+ std::to_string(used_len)
+			+ ", available="
+			+ std::to_string(storage_len)
+			+ "):"
+			+ std::string(str));
+	}
+}
+
 size_t sinsp_filter_value_parser::string_to_rawval(const char* str, uint32_t len, uint8_t *storage, std::string::size_type max_len, ppm_param_type ptype)
 {
 	size_t parsed_len;
@@ -33,20 +47,24 @@ size_t sinsp_filter_value_parser::string_to_rawval(const char* str, uint32_t len
 	switch(ptype)
 	{
 		case PT_INT8:
+			check_storage_size(str, max_len, sizeof(int8_t));
 			*(int8_t*)storage = sinsp_numparser::parsed8(str);
 			parsed_len = sizeof(int8_t);
 			break;
 		case PT_INT16:
+			check_storage_size(str, max_len, sizeof(int16_t));
 			*(int16_t*)storage = sinsp_numparser::parsed16(str);
 			parsed_len = sizeof(int16_t);
 			break;
 		case PT_INT32:
+			check_storage_size(str, max_len, sizeof(int32_t));
 			*(int32_t*)storage = sinsp_numparser::parsed32(str);
 			parsed_len = sizeof(int32_t);
 			break;
 		case PT_INT64:
 		case PT_FD:
 		case PT_ERRNO:
+			check_storage_size(str, max_len, sizeof(int64_t));
 			*(int64_t*)storage = sinsp_numparser::parsed64(str);
 			parsed_len = sizeof(int64_t);
 			break;
@@ -54,11 +72,13 @@ size_t sinsp_filter_value_parser::string_to_rawval(const char* str, uint32_t len
 		case PT_FLAGS8:
 		case PT_UINT8:
 		case PT_ENUMFLAGS8:
+			check_storage_size(str, max_len, sizeof(uint8_t));
 			*(uint8_t*)storage = sinsp_numparser::parseu8(str);
 			parsed_len = sizeof(int8_t);
 			break;
 		case PT_PORT:
 		{
+			check_storage_size(str, max_len, sizeof(uint16_t));
 			std::string in(str);
 
 			if(in.empty())
@@ -93,6 +113,7 @@ size_t sinsp_filter_value_parser::string_to_rawval(const char* str, uint32_t len
 		case PT_FLAGS16:
 		case PT_UINT16:
 		case PT_ENUMFLAGS16:
+			check_storage_size(str, max_len, sizeof(uint16_t));
 			*(uint16_t*)storage = sinsp_numparser::parseu16(str);
 			parsed_len = sizeof(uint16_t);
 			break;
@@ -100,15 +121,18 @@ size_t sinsp_filter_value_parser::string_to_rawval(const char* str, uint32_t len
 		case PT_UINT32:
 		case PT_MODE:
 		case PT_ENUMFLAGS32:
+			check_storage_size(str, max_len, sizeof(uint32_t));
 			*(uint32_t*)storage = sinsp_numparser::parseu32(str);
 			parsed_len = sizeof(uint32_t);
 			break;
 		case PT_UINT64:
+			check_storage_size(str, max_len, sizeof(uint64_t));
 			*(uint64_t*)storage = sinsp_numparser::parseu64(str);
 			parsed_len = sizeof(uint64_t);
 			break;
 		case PT_RELTIME:
 		case PT_ABSTIME:
+			check_storage_size(str, max_len, sizeof(uint64_t));
 			*(uint64_t*)storage = sinsp_numparser::parseu64(str);
 			parsed_len = sizeof(uint64_t);
 			break;
@@ -119,10 +143,7 @@ size_t sinsp_filter_value_parser::string_to_rawval(const char* str, uint32_t len
 		case PT_FSRELPATH:
 			{
 				len = (uint32_t)strlen(str);
-				if(len >= max_len)
-				{
-					throw sinsp_exception("filter parameter too long:" + std::string(str));
-				}
+				check_storage_size(str, max_len, len + 1);
 
 				memcpy(storage, str, len);
 				*(uint8_t*)(&storage[len]) = 0;
@@ -130,6 +151,7 @@ size_t sinsp_filter_value_parser::string_to_rawval(const char* str, uint32_t len
 			}
 			break;
 		case PT_BOOL:
+			check_storage_size(str, max_len, sizeof(uint32_t));
 			parsed_len = sizeof(uint32_t);
 			if(std::string(str) == "true")
 			{
@@ -156,15 +178,17 @@ size_t sinsp_filter_value_parser::string_to_rawval(const char* str, uint32_t len
 			}
 
 			break;
-	        case PT_IPV4ADDR:
+	    case PT_IPV4ADDR:
+			check_storage_size(str, max_len, sizeof(struct in_addr));
 			if(inet_pton(AF_INET, str, storage) != 1)
 			{
 				throw sinsp_exception("unrecognized IPv4 address " + std::string(str));
 			}
 			parsed_len = sizeof(struct in_addr);
 			break;
-	        case PT_IPV6ADDR:
+	    case PT_IPV6ADDR:
 		{
+			check_storage_size(str, max_len, sizeof(ipv6addr));
 			new (storage) ipv6addr(str);
 			parsed_len = sizeof(ipv6addr);
 			break;
@@ -178,10 +202,10 @@ size_t sinsp_filter_value_parser::string_to_rawval(const char* str, uint32_t len
 			{
 				return string_to_rawval(str, len, storage, max_len, PT_IPV6NET);
 			}
-
 			break;
 		case PT_IPV4NET:
 		{
+			check_storage_size(str, max_len, sizeof(ipv4net));
 			std::stringstream ss(str);
 			std::string ip, mask;
 			ipv4net* net = (ipv4net*)storage;
@@ -221,6 +245,7 @@ size_t sinsp_filter_value_parser::string_to_rawval(const char* str, uint32_t len
 		}
 		case PT_IPV6NET:
 		{
+			check_storage_size(str, max_len, sizeof(ipv6net));
 			new (storage) ipv6net(str);
 			parsed_len = sizeof(ipv6net);
 			break;
