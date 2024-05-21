ed7cf7a94535d686b0724c106ab7b47131820d81
Roberto Scolaro <roberto.scolaro21@gmail.com>
2024-05-21 08:10:12 +0000

    fix(libsinsp_e2e): increase tcp_client_server* tests stability
    Signed-off-by: Roberto Scolaro <roberto.scolaro21@gmail.com>

--- test/libsinsp_e2e/tcp_client_server.cpp
+++ test/libsinsp_e2e/tcp_client_server.cpp
@@ -408,7 +408,10 @@ TEST_F(sys_call_test, tcp_client_server_with_connection_before_capturing_starts)
 	server.wait_till_ready();
 	client.wait_till_ready();
 
-	ASSERT_NO_FATAL_FAILURE({ event_capture::run(test, callback, filter); });
+	ASSERT_NO_FATAL_FAILURE({event_capture::run(test, callback, filter, event_capture::do_nothing,
+							event_capture::do_nothing, event_capture::always_continue, 131072,
+							(uint64_t)60 * 1000 * 1000 * 1000, (uint64_t)60 * 1000 * 1000 * 1000,
+							SINSP_MODE_LIVE, 3, false); });
 	ASSERT_EQ(1, state);
 }
 
