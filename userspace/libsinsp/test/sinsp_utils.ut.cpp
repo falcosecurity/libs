de8991df6100c65b0665c6f45755df280f5fae60
Federico Di Pierro <nierro92@gmail.com>
2024-08-28 08:08:07 +0200

    wip
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- userspace/libsinsp/test/sinsp_utils.ut.cpp
+++ userspace/libsinsp/test/sinsp_utils.ut.cpp
@@ -25,12 +25,6 @@ TEST(sinsp_utils_test, concatenate_paths)
 	// Some tests were motivated by this resource:
 	// https://pubs.opengroup.org/onlinepubs/000095399/basedefs/xbd_chap04.html#tag_04_11
 
-	// PLEASE NOTE:
-	// * current impl does not support unicode.
-	// * current impl does not sanitize path1
-	// * current impl expects path1 to end with '/'
-	// * current impl skips path1 altogether if path2 is absolute
-
 	std::string path1, path2, res;
 
 	res = sinsp_utils::concatenate_paths("", "");
@@ -39,57 +33,12 @@ TEST(sinsp_utils_test, concatenate_paths)
 	path1 = "";
 	path2 = "../";
 	res = sinsp_utils::concatenate_paths(path1, path2);
-	EXPECT_EQ("", res);
-
-	path1 = "";
-	path2 = "..";
-	res = sinsp_utils::concatenate_paths(path1, path2);
-	EXPECT_EQ("", res);
-
-	path1 = "/";
-	path2 = "../";
-	res = sinsp_utils::concatenate_paths(path1, path2);
-	EXPECT_EQ("/", res);
-
-	path1 = "a";
-	path2 = "../";
-	res = sinsp_utils::concatenate_paths(path1, path2);
-	EXPECT_EQ("a..", res); // since the helper does not add any "/" between path1 and path2, we end up with this.
-
-	path1 = "a/";
-	path2 = "../";
-	res = sinsp_utils::concatenate_paths(path1, path2);
-	EXPECT_EQ("", res);
-
-	path1 = "";
-	path2 = "/foo";
-	res = sinsp_utils::concatenate_paths(path1, path2);
-	EXPECT_EQ("/foo", res);
-
-	path1 = "foo/";
-	path2 = "..//a";
-	res = sinsp_utils::concatenate_paths(path1, path2);
-	EXPECT_EQ("a", res); // path2 has been sanitized, plus we moved up a folder because of ".."
-
-	path1 = "/foo/";
-	path2 = "..//a";
-	res = sinsp_utils::concatenate_paths(path1, path2);
-	EXPECT_EQ("/a", res); // path2 has been sanitized, plus we moved up a folder because of ".."
-
-	path1 = "heolo";
-	path2 = "w////////////..//////.////////r.|";
-	res = sinsp_utils::concatenate_paths(path1, path2);
-	EXPECT_EQ("r.|", res); // since the helper does not add any "/" between path1 and path2, we end up with this.
-
-	path1 = "heolo";
-	path2 = "w/////////////..//"; // heolow/////////////..// > heolow/..// -> /
-	res = sinsp_utils::concatenate_paths(path1, path2);
-	EXPECT_EQ("", res); // since the helper does not add any "/" between path1 and path2, we end up with this, ie a folder up from "heolow/"
+	EXPECT_EQ("..", res);
 
 	path1 = "";
 	path2 = "./";
 	res = sinsp_utils::concatenate_paths(path1, path2);
-	EXPECT_EQ("", res);
+	EXPECT_EQ(".", res);
 
 	path1 = "";
 	path2 = "dir/term";
@@ -144,28 +93,27 @@ TEST(sinsp_utils_test, concatenate_paths)
 	path1 = "./app";
 	path2 = "custom/term";
 	res = sinsp_utils::concatenate_paths(path1, path2);
-	EXPECT_EQ("./appcustom/term", res); // since path1 is not '/' terminated, we expect a string concat without further path fields
+	EXPECT_EQ("app/custom/term", res);
 
 	path1 = "/app";
 	path2 = "custom/term";
 	res = sinsp_utils::concatenate_paths(path1, path2);
-	EXPECT_EQ("/appcustom/term", res); // since path1 is not '/' terminated, we expect a string concat without further path fields
+	EXPECT_EQ("/app/custom/term", res);
 
 	path1 = "app";
 	path2 = "custom/term";
 	res = sinsp_utils::concatenate_paths(path1, path2);
-	EXPECT_EQ("appcustom/term", res); // since path1 is not '/' terminated, we expect a string concat without further path fields
+	EXPECT_EQ("app/custom/term", res);
 
-	path1 = "app/";
+	path1 = "app//";
 	path2 = "custom/term";
 	res = sinsp_utils::concatenate_paths(path1, path2);
 	EXPECT_EQ("app/custom/term", res);
 
-	// We don't support sanitizing path1
 	path1 = "app/////";
 	path2 = "custom////term";
 	res = sinsp_utils::concatenate_paths(path1, path2);
-	EXPECT_EQ("app/////custom/term", res);
+	EXPECT_EQ("app/custom/term", res);
 
 	path1 = "/";
 	path2 = "/app/custom/dir/././././../../term/";
@@ -177,7 +125,6 @@ TEST(sinsp_utils_test, concatenate_paths)
 	res = sinsp_utils::concatenate_paths(path1, path2);
 	EXPECT_EQ("/app", res);
 
-	/* No unicode support
 	path1 = "/root/";
 	path2 = "../😉";
 	res = sinsp_utils::concatenate_paths(path1, path2);
@@ -196,7 +143,7 @@ TEST(sinsp_utils_test, concatenate_paths)
 	path1 = "/root";
 	path2 = "c:/hello/world/";
 	res = sinsp_utils::concatenate_paths(path1, path2);
-	EXPECT_EQ("/root/c:/hello/world", res); */
+	EXPECT_EQ("/root/c:/hello/world", res);
 }
 
 TEST(sinsp_utils_test, sinsp_split)
