005f39704f9628e0c96ea1d6f06eaa8c101b61c4
Federico Di Pierro <nierro92@gmail.com>
2024-09-03 11:02:10 +0200

    chore(cmake): honor CMAKE_BUILD_TYPE while building bundled grpc and protobuf.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- cmake/modules/protobuf.cmake
+++ cmake/modules/protobuf.cmake
@@ -48,7 +48,11 @@ else()
 		else()
 			set(PROTOBUF_PIC_OPTION "--with-pic=yes")
 		endif()
-
+		if (CMAKE_BUILD_TYPE STREQUAL "Release")
+			set(PROTOBUF_CXXFLAGS "-O2 -std=c++11 -DNDEBUG")
+		else()
+			set(PROTOBUF_CXXFLAGS "-g -std=c++11")
+		endif()
 		message(STATUS "Using bundled protobuf in '${PROTOBUF_SRC}'")
 		ExternalProject_Add(protobuf
 			PREFIX "${PROJECT_BINARY_DIR}/protobuf-prefix"
@@ -56,7 +60,7 @@ else()
 			URL "https://github.com/protocolbuffers/protobuf/releases/download/v3.20.3/protobuf-cpp-3.20.3.tar.gz"
 			URL_HASH "SHA256=e51cc8fc496f893e2a48beb417730ab6cbcb251142ad8b2cd1951faa5c76fe3d"
 			# TODO what if using system zlib?
-			CONFIGURE_COMMAND CPPFLAGS=-I${ZLIB_INCLUDE} LDFLAGS=-L${ZLIB_SRC} ./configure --with-zlib ${PROTOBUF_CONFIGURE_FLAGS} ${PROTOBUF_PIC_OPTION} --prefix=${PROTOBUF_INSTALL_DIR}
+			CONFIGURE_COMMAND CPPFLAGS=-I${ZLIB_INCLUDE} LDFLAGS=-L${ZLIB_SRC} ./configure CXXFLAGS=${PROTOBUF_CXXFLAGS} --with-zlib ${PROTOBUF_CONFIGURE_FLAGS} ${PROTOBUF_PIC_OPTION} --prefix=${PROTOBUF_INSTALL_DIR}
 			BUILD_COMMAND make
 			BUILD_IN_SOURCE 1
 			BUILD_BYPRODUCTS ${PROTOC} ${PROTOBUF_INCLUDE} ${PROTOBUF_LIB}
