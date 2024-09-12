92aebc81787e570f240fbaef4b93bf0496b20030
Andrea Terzolo <andreaterzolo3@gmail.com>
2024-09-12 17:06:08 +0200

    fix: fix some warnings as errors
    Signed-off-by: Andrea Terzolo <andreaterzolo3@gmail.com>

--- test/drivers/test_suites/generic_tracepoints_suite/sched_process_exec.cpp
+++ test/drivers/test_suites/generic_tracepoints_suite/sched_process_exec.cpp
@@ -42,7 +42,7 @@ TEST(GenericTracepoints, sched_proc_exec) {
 	/* We need to use `SIGCHLD` otherwise the parent won't receive any signal
 	 * when the child terminates.
 	 */
-	clone_args cl_args = {0};
+	clone_args cl_args = {};
 	cl_args.exit_signal = SIGCHLD;
 	pid_t ret_pid = syscall(__NR_clone3, &cl_args, sizeof(cl_args));
 
@@ -182,7 +182,7 @@ TEST(GenericTracepoints, sched_proc_exec_success_memfd) {
 	/* We need to use `SIGCHLD` otherwise the parent won't receive any signal
 	 * when the child terminates.
 	 */
-	clone_args cl_args = {0};
+	clone_args cl_args = {};
 	cl_args.exit_signal = SIGCHLD;
 	pid_t ret_pid = syscall(__NR_clone3, &cl_args, sizeof(cl_args));
 
