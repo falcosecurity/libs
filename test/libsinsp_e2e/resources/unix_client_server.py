377d260b335e928458aaaa77684a9191feda47b7
Federico Di Pierro <nierro92@gmail.com>
2024-09-17 09:14:37 +0200

    chore(test/libsinsp_e2e,ci): port libsinsp_e2e tests to use python3.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- test/libsinsp_e2e/resources/unix_client_server.py
+++ test/libsinsp_e2e/resources/unix_client_server.py
@@ -1,22 +1,21 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 # coding: utf-8 -*-
 import socket
 import os, os.path
-import time
 import sys
 
 PAYLOAD = "0123456789QWERTYUIOPASDFGHJKLZXCVBNM"
 NAME = "/tmp/python_unix_sockets_example"
- 
+STARTED = "STARTED"
+
 if sys.argv[1] == 'server':
   if os.path.exists(NAME):
     os.remove(NAME)
 
   server = socket.socket( socket.AF_UNIX, socket.SOCK_STREAM )
   server.bind(NAME)
-  sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 
-  print "STARTED"
+  print(STARTED)
   server.listen(5)
 
   connect, address = server.accept()
@@ -27,17 +26,15 @@ if sys.argv[1] == 'server':
   os.remove(NAME)
 
 else:
-  sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
-
   if os.path.exists(NAME):
     client = socket.socket( socket.AF_UNIX, socket.SOCK_STREAM )
     client.connect(NAME)
 
-    print "STARTED"
+    print(STARTED)
 
-    client.send(PAYLOAD)
+    client.send(PAYLOAD.encode())
     resp = client.recv(1024)
     client.close()
 
   else:
-    print >> sys.stderr, "Couldn't Connect!"
+    print("Couldn't Connect!", file=sys.stderr)
