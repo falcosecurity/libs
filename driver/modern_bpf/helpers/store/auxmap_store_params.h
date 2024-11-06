94cc1b0dd05948d2bbbd74602025c7a3272fc3bb
Andrea Terzolo <andreaterzolo3@gmail.com>
2024-11-06 14:34:58 +0100

    cleanup: move `hotplug` logic into sys_exit
    Signed-off-by: Andrea Terzolo <andreaterzolo3@gmail.com>

--- driver/modern_bpf/helpers/store/auxmap_store_params.h
+++ driver/modern_bpf/helpers/store/auxmap_store_params.h
@@ -119,11 +119,12 @@ static __always_inline void auxmap__finalize_event_header(struct auxiliary_map *
  * @param auxmap pointer to the auxmap in which we have already written the entire event.
  * @param ctx BPF prog context
  */
+// todo!: we need to remove the context because we don't perform any tail call
 static __always_inline void auxmap__submit_event(struct auxiliary_map *auxmap, void *ctx) {
 	struct ringbuf_map *rb = maps__get_ringbuf_map();
 	if(!rb) {
-		bpf_tail_call(ctx, &extra_event_prog_tail_table, T1_HOTPLUG_E);
-		bpf_printk("failed to tail call into the 'hotplug' prog");
+		// this should never happen because we check it in sys_enter/sys_exit
+		bpf_printk("FAILURE: unable to obtain the ring buffer");
 		return;
 	}
 
