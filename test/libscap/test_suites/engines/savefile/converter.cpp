777575e9e263875c181047978b20427b103cb678
Andrea Terzolo <andreaterzolo3@gmail.com>
2024-11-29 16:38:32 +0100

    new: add READ_X/PREAD_X conversion
    Signed-off-by: Andrea Terzolo <andreaterzolo3@gmail.com>

--- test/libscap/test_suites/engines/savefile/converter.cpp
+++ test/libscap/test_suites/engines/savefile/converter.cpp
@@ -13,11 +13,161 @@ limitations under the License.
 */
 #include "convert_event_test.h"
 
-// todo!: remove it when we will add the first example.
-TEST_F(convert_event_test, tmp_example) {
+////////////////////////////
+// READ
+////////////////////////////
+
+TEST_F(convert_event_test, PPME_SYSCALL_READ_E_store) {
 	uint64_t ts = 12;
 	int64_t tid = 25;
 
-	// At the moment we don't have event managed by the converter so this should fail.
-	assert_single_conversion_failure(create_safe_scap_event(ts, tid, PPME_SYSCALL_OPEN_E, 0));
+	int64_t fd = 25;
+	uint32_t size = 89;
+
+	auto evt = create_safe_scap_event(ts, tid, PPME_SYSCALL_READ_E, 2, fd, size);
+	assert_single_conversion_skip(evt);
+	assert_event_storage_presence(evt);
+}
+
+TEST_F(convert_event_test, PPME_SYSCALL_READ_X_to_4_params_no_enter) {
+	uint64_t ts = 12;
+	int64_t tid = 25;
+
+	int64_t res = 89;
+	uint8_t read_buf[] = {'h', 'e', 'l', 'l', 'o'};
+
+	// Defaulted to 0
+	int64_t fd = 0;
+	uint32_t size = 0;
+
+	assert_single_conversion_success(
+	        conversion_result::CONVERSION_COMPLETED,
+	        create_safe_scap_event(ts,
+	                               tid,
+	                               PPME_SYSCALL_READ_X,
+	                               2,
+	                               res,
+	                               scap_const_sized_buffer{read_buf, sizeof(read_buf)}),
+	        create_safe_scap_event(ts,
+	                               tid,
+	                               PPME_SYSCALL_READ_X,
+	                               4,
+	                               res,
+	                               scap_const_sized_buffer{read_buf, sizeof(read_buf)},
+	                               fd,
+	                               size));
+}
+
+TEST_F(convert_event_test, PPME_SYSCALL_READ_X__to_4_params_with_enter) {
+	uint64_t ts = 12;
+	int64_t tid = 25;
+
+	int64_t res = 89;
+	uint8_t read_buf[] = {'h', 'e', 'l', 'l', 'o'};
+	int64_t fd = 25;
+	uint32_t size = 36;
+
+	// After the first conversion we should have the storage
+	auto evt = create_safe_scap_event(ts, tid, PPME_SYSCALL_READ_E, 2, fd, size);
+	assert_single_conversion_skip(evt);
+	assert_event_storage_presence(evt);
+
+	assert_single_conversion_success(
+	        conversion_result::CONVERSION_COMPLETED,
+	        create_safe_scap_event(ts,
+	                               tid,
+	                               PPME_SYSCALL_READ_X,
+	                               2,
+	                               res,
+	                               scap_const_sized_buffer{read_buf, sizeof(read_buf)}),
+	        create_safe_scap_event(ts,
+	                               tid,
+	                               PPME_SYSCALL_READ_X,
+	                               4,
+	                               res,
+	                               scap_const_sized_buffer{read_buf, sizeof(read_buf)},
+	                               fd,
+	                               size));
+}
+
+////////////////////////////
+// PREAD
+////////////////////////////
+
+TEST_F(convert_event_test, PPME_SYSCALL_PREAD_E_store) {
+	uint64_t ts = 12;
+	int64_t tid = 25;
+
+	int64_t fd = 25;
+	uint32_t size = 89;
+	uint64_t pos = 7;
+
+	auto evt = create_safe_scap_event(ts, tid, PPME_SYSCALL_PREAD_E, 3, fd, size, pos);
+	assert_single_conversion_skip(evt);
+	assert_event_storage_presence(evt);
+}
+
+TEST_F(convert_event_test, PPME_SYSCALL_PREAD_X_to_4_params_no_enter) {
+	uint64_t ts = 12;
+	int64_t tid = 25;
+
+	int64_t res = 89;
+	uint8_t read_buf[] = {'h', 'e', 'l', 'l', 'o'};
+
+	// Defaulted to 0
+	int64_t fd = 0;
+	uint32_t size = 0;
+	int64_t pos = 0;
+
+	assert_single_conversion_success(
+	        conversion_result::CONVERSION_COMPLETED,
+	        create_safe_scap_event(ts,
+	                               tid,
+	                               PPME_SYSCALL_PREAD_X,
+	                               2,
+	                               res,
+	                               scap_const_sized_buffer{read_buf, sizeof(read_buf)}),
+	        create_safe_scap_event(ts,
+	                               tid,
+	                               PPME_SYSCALL_PREAD_X,
+	                               5,
+	                               res,
+	                               scap_const_sized_buffer{read_buf, sizeof(read_buf)},
+	                               fd,
+	                               size,
+	                               pos));
+}
+
+TEST_F(convert_event_test, PPME_SYSCALL_PREAD_X__to_4_params_with_enter) {
+	uint64_t ts = 12;
+	int64_t tid = 25;
+
+	int64_t res = 89;
+	uint8_t read_buf[] = {'h', 'e', 'l', 'l', 'o'};
+	int64_t fd = 25;
+	uint32_t size = 36;
+	uint64_t pos = 7;
+
+	// After the first conversion we should have the storage
+	auto evt = create_safe_scap_event(ts, tid, PPME_SYSCALL_PREAD_E, 3, fd, size, pos);
+	assert_single_conversion_skip(evt);
+	assert_event_storage_presence(evt);
+
+	assert_single_conversion_success(
+	        conversion_result::CONVERSION_COMPLETED,
+	        create_safe_scap_event(ts,
+	                               tid,
+	                               PPME_SYSCALL_PREAD_X,
+	                               2,
+	                               res,
+	                               scap_const_sized_buffer{read_buf, sizeof(read_buf)}),
+	        create_safe_scap_event(ts,
+	                               tid,
+	                               PPME_SYSCALL_PREAD_X,
+	                               5,
+	                               res,
+	                               scap_const_sized_buffer{read_buf, sizeof(read_buf)},
+	                               fd,
+	                               size,
+	                               pos));
 }
