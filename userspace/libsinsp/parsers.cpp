dc80ee07fcf55f28e393dd289f6f0538adb8491e
Federico Di Pierro <nierro92@gmail.com>
2024-12-05 10:45:08 +0100

    cleanup(userspace/libsinsp): add a `get_container_id` helper method on threadinfo.
    It leverages sinsp state table API to retrieve "container_id" field written by the plugin.
    Use it where needed.
    
    Moreover, user_group_manager cannot subscribe to container changes anymore, since container changes are no more in sinsp.
    Instead, parse ASYNC event "container_removed" to cleanup user_group tables.
    
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- userspace/libsinsp/parsers.cpp
+++ userspace/libsinsp/parsers.cpp
@@ -1823,8 +1823,6 @@ void sinsp_parser::parse_execve_exit(sinsp_evt *evt) {
 	evt->get_tinfo()->m_exe = parinfo->m_val;
 	evt->get_tinfo()->m_lastexec_ts = evt->get_ts();
 
-	auto container_id = evt->get_tinfo()->m_container_id;
-
 	switch(etype) {
 	case PPME_SYSCALL_EXECVE_8_X:
 	case PPME_SYSCALL_EXECVE_13_X:
@@ -4641,9 +4639,6 @@ void sinsp_parser::parse_chroot_exit(sinsp_evt *evt) {
 		} else {
 			evt->get_tinfo()->m_root = resolved_path;
 		}
-		// Root change, let's detect if we are on a container
-
-		auto container_id = evt->get_tinfo()->m_container_id;
 	}
 }
 
