beabd3976d70b78719b31551ec0a7b058c1b5ffa
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2025-01-02 18:34:58 +0100

    test(sinsp/scap_files): add accept events conversion tests
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- userspace/libscap/engine/savefile/converter/table.cpp
+++ userspace/libscap/engine/savefile/converter/table.cpp
@@ -54,6 +54,7 @@ const std::unordered_map<conversion_key, conversion_info> g_conversion_table = {
          conversion_info()
                  .action(C_ACTION_ADD_PARAMS)
                  .instrs({{C_INSTR_FROM_ENTER, 0}, {C_INSTR_FROM_ENTER, 1}})},
+        /*====================== ACCEPT ======================*/
         {conversion_key{PPME_SOCKET_ACCEPT_E, 0}, conversion_info().action(C_ACTION_SKIP)},
         {conversion_key{PPME_SOCKET_ACCEPT_X, 3},
          conversion_info()
