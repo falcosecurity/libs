5df4aec696ba359cfbc6329169738c4847b09c38
Andrea Terzolo <andreaterzolo3@gmail.com>
2025-01-02 15:48:55 +0100

    fix(modern): move args declaration at the beginning
    Signed-off-by: Andrea Terzolo <andreaterzolo3@gmail.com>

--- driver/modern_bpf/programs/tail_called/events/syscall_dispatched_events/listen.bpf.c
+++ driver/modern_bpf/programs/tail_called/events/syscall_dispatched_events/listen.bpf.c
@@ -12,7 +12,9 @@
 
 SEC("tp_btf/sys_enter")
 int BPF_PROG(listen_e, struct pt_regs *regs, long id) {
-	/* Collect parameters at the beginning to manage socketcalls */
+	/* We need to keep this at the beginning of the program because otherwise we alter the state of
+	 * the ebpf registers causing a verifier issue.
+	 */
 	unsigned long args[2] = {0};
 	extract__network_args(args, 2, regs);
 
@@ -46,7 +48,9 @@ int BPF_PROG(listen_e, struct pt_regs *regs, long id) {
 
 SEC("tp_btf/sys_exit")
 int BPF_PROG(listen_x, struct pt_regs *regs, long ret) {
-	/* Collect parameters at the beginning to manage socketcalls */
+	/* We need to keep this at the beginning of the program because otherwise we alter the state of
+	 * the ebpf registers causing a verifier issue.
+	 */
 	unsigned long args[2] = {0};
 	extract__network_args(args, 2, regs);
 
