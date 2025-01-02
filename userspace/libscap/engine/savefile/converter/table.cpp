d11578f8c3e0c9ceb59b75577bae7fa4a9cd6adf
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2025-01-02 17:35:27 +0100

    new: extend SEND_X and SENDTO_X
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- userspace/libscap/engine/savefile/converter/table.cpp
+++ userspace/libscap/engine/savefile/converter/table.cpp
@@ -59,11 +59,25 @@ const std::unordered_map<conversion_key, conversion_info> g_conversion_table = {
          conversion_info()
                  .desired_type(PPME_SOCKET_ACCEPT_5_X)
                  .action(C_ACTION_CHANGE_TYPE)
-                 .instrs({
-                         {C_INSTR_FROM_OLD, 0},
-                         {C_INSTR_FROM_OLD, 1},
-                         {C_INSTR_FROM_OLD, 2},
-                         {C_INSTR_FROM_DEFAULT, 0},
-                         {C_INSTR_FROM_DEFAULT, 0},
-                 })},
-        {conversion_key{PPME_SOCKET_ACCEPT_5_E, 0}, conversion_info().action(C_ACTION_SKIP)}};
+                 .instrs({{C_INSTR_FROM_OLD, 0},
+                          {C_INSTR_FROM_OLD, 1},
+                          {C_INSTR_FROM_OLD, 2},
+                          {C_INSTR_FROM_DEFAULT, 0},
+                          {C_INSTR_FROM_DEFAULT, 0}})},
+        {conversion_key{PPME_SOCKET_ACCEPT_5_E, 0}, conversion_info().action(C_ACTION_SKIP)},
+        /*====================== SEND ======================*/
+        {conversion_key{PPME_SOCKET_SEND_E, 2}, conversion_info().action(C_ACTION_STORE)},
+        {conversion_key{PPME_SOCKET_SEND_X, 2},
+         conversion_info()
+                 .action(C_ACTION_ADD_PARAMS)
+                 .instrs({{C_INSTR_FROM_ENTER, 0},
+                          {C_INSTR_FROM_ENTER, 1},
+                          {C_INSTR_FROM_DEFAULT, 0}})},
+        /*====================== SENDTO ======================*/
+        {conversion_key{PPME_SOCKET_SENDTO_E, 3}, conversion_info().action(C_ACTION_STORE)},
+        {conversion_key{PPME_SOCKET_SENDTO_X, 2},
+         conversion_info()
+                 .action(C_ACTION_ADD_PARAMS)
+                 .instrs({{C_INSTR_FROM_ENTER, 0},
+                          {C_INSTR_FROM_ENTER, 1},
+                          {C_INSTR_FROM_ENTER, 2}})}};
