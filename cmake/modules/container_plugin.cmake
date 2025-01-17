4dd06df7f81f214bc45dc5f3aacd29b11e2fdd57
Federico Di Pierro <nierro92@gmail.com>
2025-01-17 16:23:20 +0100

    update(cmake): bumped container plugin to 0.1.0-beta2.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- cmake/modules/container_plugin.cmake
+++ cmake/modules/container_plugin.cmake
@@ -14,13 +14,21 @@
 #
 
 set(CONTAINER_LIBRARY "${CMAKE_CURRENT_BINARY_DIR}/container_plugin-prefix/src/libcontainer.so")
-set(CONTAINER_VERSION "0.1.0-alpha")
+set(CONTAINER_VERSION "0.1.0-beta2")
+if(${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "x86_64")
+	set(CONTAINER_HASH "3899614434cf462933e71d07a3061446fd93dd4b964befbce78ef90931ddfbc2")
+	set(CONTAINER_ARCH "amd64")
+else() # arm64
+	set(CONTAINER_HASH "c89b94428796fc6690f63982ed51db068ddd83a7048c8163e8ef88bceb22843b")
+	set(CONTAINER_ARCH "arm64")
+endif()
 if(NOT TARGET container_plugin)
 	message(STATUS "Fetching container plugin ${CONTAINER_VERSION} in '${CONTAINER_LIBRARY}'")
 	ExternalProject_Add(
 		container_plugin
-		URL "https://github.com/FedeDP/container_plugin/releases/download/${CONTAINER_VERSION}/libcontainer.so"
-		# URL_HASH "SHA256="
+		URL "https://github.com/FedeDP/container_plugin/releases/download/${CONTAINER_VERSION}/libcontainer_${CONTAINER_ARCH}.so"
+		URL_HASH "SHA256=${CONTAINER_HASH}"
+		DOWNLOAD_NAME "libcontainer.so"
 		DOWNLOAD_NO_EXTRACT 1
 		CONFIGURE_COMMAND ""
 		BUILD_COMMAND ""
