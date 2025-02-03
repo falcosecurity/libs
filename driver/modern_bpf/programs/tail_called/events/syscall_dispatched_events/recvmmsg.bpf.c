f287b80fe046b8616fb9ff8aecf73ce66afcd538
Federico Di Pierro <nierro92@gmail.com>
2025-02-03 15:10:22 +0100

    fix(driver/modern_bpf): avoid calling `extract_network_args` in apply_dynamic_snaplen at each bpf_loop iteration for sendmmsg and recvmmsg.
    This also fixes a verifier issue on clang 14, related to stack length.
    
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- driver/modern_bpf/programs/tail_called/events/syscall_dispatched_events/recvmmsg.bpf.c
+++ driver/modern_bpf/programs/tail_called/events/syscall_dispatched_events/recvmmsg.bpf.c
@@ -38,7 +38,7 @@ int BPF_PROG(recvmmsg_e, struct pt_regs *regs, long id) {
 typedef struct {
 	uint32_t fd;
 	struct mmsghdr *mmh;
-	struct pt_regs *regs;
+	unsigned long *args;
 } recvmmsg_data_t;
 
 static __always_inline long handle_exit(uint32_t index, void *ctx) {
@@ -75,9 +75,10 @@ static __always_inline long handle_exit(uint32_t index, void *ctx) {
 	        .only_port_range = true,
 	        .evt_type = PPME_SOCKET_RECVMMSG_X,
 	        .mmsg_index = index,
+	        .mm_args = data->args,
 	};
 	uint16_t snaplen = maps__get_snaplen();
-	apply_dynamic_snaplen(data->regs, &snaplen, &snaplen_args);
+	apply_dynamic_snaplen(NULL, &snaplen, &snaplen_args);
 	if(snaplen > mmh.msg_len) {
 		snaplen = mmh.msg_len;
 	}
@@ -149,12 +150,12 @@ int BPF_PROG(recvmmsg_x, struct pt_regs *regs, long ret) {
 	}
 
 	/* Collect parameters at the beginning to manage socketcalls */
-	unsigned long args[2];
-	extract__network_args(args, 2, regs);
+	unsigned long args[3];
+	extract__network_args(args, 3, regs);
 	recvmmsg_data_t data = {
 	        .fd = args[0],
 	        .mmh = (struct mmsghdr *)args[1],
-	        .regs = regs,
+	        .args = args,
 	};
 
 	uint32_t nr_loops = ret < 1024 ? ret : 1024;
@@ -200,12 +201,12 @@ int BPF_PROG(recvmmsg_old_x, struct pt_regs *regs, long ret) {
 	}
 
 	/* Collect parameters at the beginning to manage socketcalls */
-	unsigned long args[2];
-	extract__network_args(args, 2, regs);
+	unsigned long args[3];
+	extract__network_args(args, 3, regs);
 	recvmmsg_data_t data = {
 	        .fd = args[0],
 	        .mmh = (struct mmsghdr *)args[1],
-	        .regs = regs,
+	        .args = args,
 	};
 
 	// Send only first message
