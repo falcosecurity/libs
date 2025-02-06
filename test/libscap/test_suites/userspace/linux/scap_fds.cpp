5d4ac671cf0f6c9e897dcd2f102da1b81918c141
Shane Lawrence <shane@lawrence.dev>
2025-02-06 09:05:01 -0500

    Fix formatting.
    Signed-off-by: Shane Lawrence <shane@lawrence.dev>

--- test/libscap/test_suites/userspace/linux/scap_fds.cpp
+++ test/libscap/test_suites/userspace/linux/scap_fds.cpp
@@ -2,10 +2,11 @@
 #include <libscap/scap.h>
 
 extern "C" {
-    int32_t test_time_wait_socket_at_buffer_end(void);
+int32_t test_time_wait_socket_at_buffer_end(void);
 }
 
 TEST(scap_fds, buffer_overflow_test) {
-    int32_t res = test_time_wait_socket_at_buffer_end();
-    ASSERT_EQ(res, SCAP_SUCCESS) << "Expected SCAP_SUCCESS when parsing TIME_WAIT socket at buffer end";
+	int32_t res = test_time_wait_socket_at_buffer_end();
+	ASSERT_EQ(res, SCAP_SUCCESS)
+	        << "Expected SCAP_SUCCESS when parsing TIME_WAIT socket at buffer end";
 }
