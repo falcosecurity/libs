e0eebfb68bdbb3259fb9ec96b91fed2fe5ddf593
Federico Di Pierro <nierro92@gmail.com>
2025-03-24 11:05:24 +0100

    update(cmake): bumped container_plugin.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- cmake/modules/container_plugin.cmake
+++ cmake/modules/container_plugin.cmake
@@ -13,26 +13,29 @@
 # the License.
 #
 
+include(ExternalProject)
+
+string(TOLOWER ${CMAKE_HOST_SYSTEM_NAME} PLUGINS_SYSTEM_NAME)
+
 set(CONTAINER_LIBRARY
 	"${CMAKE_CURRENT_BINARY_DIR}/container_plugin-prefix/src/container_plugin/libcontainer.so"
 )
+
 if(NOT CONTAINER_VERSION)
-	set(CONTAINER_VERSION "0.1.0-rc3")
+	set(CONTAINER_VERSION "0.1.0")
 endif()
-if(${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "x86_64")
-	if(NOT CONTAINER_HASH)
-		set(CONTAINER_HASH "059e8a1f7018d7ead97abc46566a82d8250c03e520d6e8858828bd07c3cb5af7")
-	endif()
-else() # arm64
-	if(NOT CONTAINER_HASH)
-		set(CONTAINER_HASH "5b7892ac01ed109f3da86a3b1603d14d41363755f2362d82946df512e88768cf")
+if(NOT CONTAINER_HASH)
+	if(${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "x86_64")
+		set(CONTAINER_HASH "6e2a47ac472475b580e860bee24f668906cd0fb1aa1cd4d6c2b4d05238bec6e3")
+	else() # arm64
+		set(CONTAINER_HASH "bab588b42e34c726f5536d0d9a1279f9abe4701e8ca89591f48ace066beee844")
 	endif()
 endif()
 if(NOT TARGET container_plugin)
 	message(STATUS "Fetching container plugin ${CONTAINER_VERSION} in '${CONTAINER_LIBRARY}'")
 	ExternalProject_Add(
 		container_plugin
-		URL "https://github.com/FedeDP/container_plugin/releases/download/${CONTAINER_VERSION}/container-${CONTAINER_VERSION}-linux-${CMAKE_HOST_SYSTEM_PROCESSOR}.tar.gz"
+		URL "https://download.falco.org/plugins/stable/container-${CONTAINER_VERSION}-${PLUGINS_SYSTEM_NAME}-${CMAKE_HOST_SYSTEM_PROCESSOR}.tar.gz"
 		URL_HASH "SHA256=${CONTAINER_HASH}"
 		CONFIGURE_COMMAND ""
 		BUILD_COMMAND ""
