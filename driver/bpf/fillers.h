5a3dab1ff65688b6e41c428a167169f06ed78dfa
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2025-06-30 20:20:12 +0200

    feat: add `PPME_SYSCALL_WRITEV_E` params to `PPME_SYSCALL_WRITEV_X`
    Add `PPME_SYSCALL_WRITEV_E` parameters to `PPME_SYSCALL_WRITEV_X`
    event definition and align all 3 kernel drivers to it.
    
    Add new rules to scap file converter table to convert events in old
    scap files to the new layout.
    
    Add/update writev-related drivers, scap converter and sinsp parser
    tests to account the new layout.
    
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- driver/bpf/fillers.h
+++ driver/bpf/fillers.h
@@ -819,8 +819,8 @@ FILLER(sys_writev_e, true) {
 #error Implement this
 #endif
 	/* Parameter 1: fd (type: PT_FD) */
-	int32_t fd = (int32_t)bpf_syscall_get_argument(data, 0);
-	int res = bpf_push_s64_to_ring(data, (int64_t)fd);
+	int64_t fd = (int64_t)(int32_t)bpf_syscall_get_argument(data, 0);
+	int res = bpf_push_s64_to_ring(data, fd);
 	CHECK_RES(res);
 
 	unsigned long iov_pointer = bpf_syscall_get_argument(data, 1);
@@ -833,16 +833,57 @@ FILLER(sys_writev_e, true) {
 	                                  0,
 	                                  PRB_FLAG_PUSH_SIZE | PRB_FLAG_IS_WRITE);
 
-	/* if there was an error we send a size equal to `0`.
-	 * we can improve this in the future but at least we don't lose the whole event.
-	 */
+	/* If there was an error we send a size equal to `0`.
+	 * We can improve this in the future but at least we don't lose the whole event. */
 	if(res == PPM_FAILURE_INVALID_USER_MEMORY) {
 		res = bpf_push_u32_to_ring(data, (uint32_t)0);
 	}
 	return res;
 }
 
-FILLER(sys_writev_pwritev_x, true) {
+FILLER(sys_writev_x, true) {
+	/* Parameter 1: res (type: PT_ERRNO) */
+	long retval = bpf_syscall_get_retval(data->ctx);
+	int res = bpf_push_s64_to_ring(data, retval);
+	CHECK_RES(res);
+
+	/* Parameter 2: data (type: PT_BYTEBUF) */
+	unsigned long iov_pointer = bpf_syscall_get_argument(data, 1);
+	unsigned long iov_cnt = bpf_syscall_get_argument(data, 2);
+	res = bpf_parse_readv_writev_bufs(data,
+	                                  (const struct iovec __user *)iov_pointer,
+	                                  iov_cnt,
+	                                  0,
+	                                  PRB_FLAG_PUSH_DATA | PRB_FLAG_IS_WRITE);
+
+	/* If there was an error we send an empty param.
+	 * We can improve this in the future but at least we don't lose the whole event. */
+	if(res == PPM_FAILURE_INVALID_USER_MEMORY) {
+		res = bpf_push_empty_param(data);
+	}
+	CHECK_RES(res);
+
+	/* Parameter 3: fd (type: PT_FD) */
+	int64_t fd = (int64_t)(int32_t)bpf_syscall_get_argument(data, 0);
+	res = bpf_push_s64_to_ring(data, fd);
+	CHECK_RES(res);
+
+	/* Parameter 4: size (type: PT_UINT32) */
+	res = bpf_parse_readv_writev_bufs(data,
+	                                  (const struct iovec __user *)iov_pointer,
+	                                  iov_cnt,
+	                                  0,
+	                                  PRB_FLAG_PUSH_SIZE | PRB_FLAG_IS_WRITE);
+
+	/* If there was an error we send a size equal to `0`.
+	 * We can improve this in the future but at least we don't lose the whole event. */
+	if(res == PPM_FAILURE_INVALID_USER_MEMORY) {
+		res = bpf_push_u32_to_ring(data, (uint32_t)0);
+	}
+	return res;
+}
+
+FILLER(sys_pwritev_x, true) {
 	unsigned long iovcnt;
 	unsigned long val;
 	long retval;
@@ -4557,9 +4598,8 @@ FILLER(sys_pwritev_e, true) {
 	                                  0,
 	                                  PRB_FLAG_PUSH_SIZE | PRB_FLAG_IS_WRITE);
 
-	/* if there was an error we send a size equal to `0`.
-	 * we can improve this in the future but at least we don't lose the whole event.
-	 */
+	/* If there was an error we send a size equal to `0`.
+	 * We can improve this in the future but at least we don't lose the whole event. */
 	if(res == PPM_FAILURE_INVALID_USER_MEMORY) {
 		res = bpf_push_u32_to_ring(data, 0);
 	}
