9271d1d16cfe3cfdac6cd109a7481489bd6db3b6
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2025-07-07 18:16:31 +0200

    cleanup(libsinsp): switch set_dynamic_field to write_field
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

--- userspace/libsinsp/state/dynamic_struct.h
+++ userspace/libsinsp/state/dynamic_struct.h
@@ -244,18 +244,6 @@ public:
 	virtual ~dynamic_struct() { dynamic_struct::destroy_dynamic_fields(); }
 
 	/**
-	 * @brief Accesses a field with the given accessor and writes its value.
-	 */
-	template<typename T, typename Val = T>
-	inline void set_dynamic_field(const field_accessor<T>& a, const Val& in) {
-		_check_defsptr(a.info(), true);
-		if(a.info().readonly()) {
-			throw sinsp_exception("can't set a read-only dynamic struct field: " + a.info().name());
-		}
-		set_dynamic_field(a.info(), reinterpret_cast<const void*>(&in));
-	}
-
-	/**
 	 * @brief Returns information about all the dynamic fields accessible in a struct.
 	 */
 
@@ -278,21 +266,6 @@ public:
 	}
 
 protected:
-	/**
-	 * @brief Sets the value of a dynamic field by reading it from "in".
-	 * "in" points to a variable having the type of the field_info argument,
-	 * according to the type definitions supported in libsinsp::state::typeinfo.
-	 * For strings, "in" is considered of type const char**.
-	 */
-	virtual void set_dynamic_field(const field_info& i, const void* in) {
-		auto* buf = _access_dynamic_field_for_write(i.m_index);
-		if(i.info().type_id() == SS_PLUGIN_ST_STRING) {
-			*((std::string*)buf) = *((const char**)in);
-		} else {
-			memcpy(buf, in, i.info().size());
-		}
-	}
-
 	/**
 	 * @brief Destroys all the dynamic field values currently allocated
 	 */
@@ -401,19 +374,3 @@ private:
 };
 
 };  // namespace libsinsp::state
-
-// specializations for string types
-template<>
-inline void libsinsp::state::dynamic_struct::set_dynamic_field<std::string, const char*>(
-        const field_accessor<std::string>& a,
-        const char* const& in) {
-	_check_defsptr(a.info(), true);
-	set_dynamic_field(a.info(), reinterpret_cast<const void*>(&in));
-}
-
-template<>
-inline void libsinsp::state::dynamic_struct::set_dynamic_field<std::string, std::string>(
-        const field_accessor<std::string>& a,
-        const std::string& in) {
-	set_dynamic_field(a, in.c_str());
-}
