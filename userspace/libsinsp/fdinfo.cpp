722a9c4b2d8d9e3d57a8a05bef372adac4d4c0db
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2025-07-14 13:49:29 +0200

    cleanup(libsinsp): make table_entry pure virtual
    Note: this does cause uninitialized memory access when calling
    static_fields() on a table that doesn't have any, so Don't Do That
    for now. We'll fix it by removing static_fields() soon.
    
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

--- userspace/libsinsp/fdinfo.cpp
+++ userspace/libsinsp/fdinfo.cpp
@@ -120,7 +120,7 @@ const char* sinsp_fdinfo::get_typestring() const {
 }
 
 sinsp_fdinfo::sinsp_fdinfo(const std::shared_ptr<libsinsp::state::dynamic_field_infos>& dyn_fields):
-        table_entry(dyn_fields) {}
+        extensible_struct(dyn_fields) {}
 
 libsinsp::state::extensible_struct::field_infos sinsp_fdinfo::static_fields() const {
 	return get_static_fields();
