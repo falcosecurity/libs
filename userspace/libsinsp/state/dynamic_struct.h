04cef6b18b4e06bc5cb7b8a71b9528e08b0619f4
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2025-07-18 15:16:49 +0200

    cleanup(libsinsp): rename state_struct to table_entry
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

--- userspace/libsinsp/state/dynamic_struct.h
+++ userspace/libsinsp/state/dynamic_struct.h
@@ -18,7 +18,7 @@ limitations under the License.
 
 #pragma once
 
-#include <libsinsp/state/state_struct.h>
+#include <libsinsp/state/table_entry.h>
 #include <libsinsp/state/type_info.h>
 
 #include <string>
@@ -33,7 +33,7 @@ namespace libsinsp::state {
  * @brief A base class for classes and structs that allow dynamic programming
  * by being extensible and allowing adding and accessing new data fields at runtime.
  */
-class dynamic_struct : virtual public state_struct {
+class dynamic_struct : virtual public table_entry {
 public:
 	template<typename T>
 	class field_accessor;
