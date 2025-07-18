a2d9730865967ca1b0c2796440b72a337ee4a75a
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2025-07-18 16:46:26 +0200

    cleanup(libsinsp): introduce a base class for state structs
    The two obvious implementations are `static_struct`
    and `dynamic_struct`, but there's another one hiding inside the STL
    table adapter support code.
    
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

--- userspace/libsinsp/state/state_struct.h
+++ userspace/libsinsp/state/state_struct.h
@@ -39,4 +39,80 @@ public:
 	typed_accessor(): accessor(typeinfo::of<T>()) {}
 };
 
+class state_struct {
+public:
+	virtual ~state_struct() = default;
+
+	template<typename T>
+	T read_field(const typed_accessor<T>& a) const {
+		auto out = static_cast<const T*>(this->raw_read_field(a));
+		if(out == nullptr) {
+			return {};
+		}
+		return *out;
+	}
+
+	template<typename T, typename Val = T>
+	void read_field(const typed_accessor<T>& a, Val& out) const {
+		out = this->read_field(a);
+	}
+
+	template<typename T, typename Val = T>
+	void write_field(const typed_accessor<T>& a, const Val& in) {
+		// TODO: we could use a direct assignment of const char* to strings
+		//       but we'd have to handle it deep down in each individual
+		//       implementation of raw_write_field
+		T in_val = in;
+		this->raw_write_field(a, &in_val);
+	}
+
+protected:
+	[[nodiscard]] virtual const void* raw_read_field(const accessor& a) const = 0;
+	virtual void raw_write_field(const accessor& a, const void* in) = 0;
+};
+
+template<>
+inline void state_struct::read_field(const typed_accessor<std::string>& a, const char*& out) const {
+	auto out_ptr = static_cast<const std::string*>(this->raw_read_field(a));
+	if(out_ptr) {
+		out = out_ptr->c_str();
+	} else {
+		out = "";
+	}
+}
+
+template<typename F, typename... Args>
+auto dispatch_lambda(ss_plugin_state_type st, F&& f, Args&&... args) {
+	switch(st) {
+	case SS_PLUGIN_ST_INT8:
+		return std::forward<F>(f).template operator()<int8_t>(std::forward<Args>(args)...);
+	case SS_PLUGIN_ST_INT16:
+		return std::forward<F>(f).template operator()<int16_t>(std::forward<Args>(args)...);
+	case SS_PLUGIN_ST_INT32:
+		return std::forward<F>(f).template operator()<int32_t>(std::forward<Args>(args)...);
+	case SS_PLUGIN_ST_INT64:
+		return std::forward<F>(f).template operator()<int64_t>(std::forward<Args>(args)...);
+	case SS_PLUGIN_ST_UINT8:
+		return std::forward<F>(f).template operator()<uint8_t>(std::forward<Args>(args)...);
+	case SS_PLUGIN_ST_UINT16:
+		return std::forward<F>(f).template operator()<uint16_t>(std::forward<Args>(args)...);
+	case SS_PLUGIN_ST_UINT32:
+		return std::forward<F>(f).template operator()<uint32_t>(std::forward<Args>(args)...);
+	case SS_PLUGIN_ST_UINT64:
+		return std::forward<F>(f).template operator()<uint64_t>(std::forward<Args>(args)...);
+	case SS_PLUGIN_ST_STRING:
+		return std::forward<F>(f).template operator()<std::string>(std::forward<Args>(args)...);
+	case SS_PLUGIN_ST_TABLE:
+		return std::forward<F>(f).template operator()<base_table*>(std::forward<Args>(args)...);
+	case SS_PLUGIN_ST_BOOL:
+		return std::forward<F>(f).template operator()<bool>(std::forward<Args>(args)...);
+	default:
+#ifdef _MSC_VER
+		_assume(0);
+#else
+		__builtin_unreachable();
+#endif
+	}
+}
+
 }  // namespace libsinsp::state
