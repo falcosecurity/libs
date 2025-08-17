537a4a86ee858a8b1070d775e166c2177407f933
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2025-08-17 12:48:27 +0200

    feat!: isolate modern probe `openat` TOCTOU mitigation logic
    Current TOCTOU mitigation implementation leverages enter events
    generated by eBPF programs which are tail-called by the `sys_enter`
    dispatcher. As the architecture is moving towards dropping enter
    events generation and collection, the `sys_enter` dispatcher will be
    ultimately removed. This requires to isolate TOCTOU mitigation
    handling logic in order to make it independent from `sys_enter`
    dispatcher removal.
    
    This patch moves TOCTOU mitigation handling logic for `openat`
    syscall into a seprate tracepoint program and two separate kprobe
    programs:
    - `openat_e` - tracepoint program attached to
      `syscall/sys_enter_openat`; it provides support for 64-bit system
      calls.
    - `ia32_compat_openat_e`/`ia32_openat_e` - mutually exclusive kprobe
      programs attached to `__ia32_compat_sys_openat` and
      `__ia32_sys_openat`, respectively. They provide support for ia-32
      emulated system calls. Just one of the two programs is attached,
      based on the availability of the correspoding kernel symbol. The
      compat version takes precedence over the other one.
    
    As the tracepoint attachment procedure generates an `openat` exit
    event on `/sys/kernel/tracing/events/.../id` that would pollute the
    stream of events read by the probe, the implementation takes care
    of attaching it before attaching the `sys_exit` dispatcher.
    
    Notice that the new logic make the TOCTOU mitigation programs
    attachment dependent on the presence of the `sys_exit` dispatcher.
    
    BREAKING CHANGE: change prerequisites for attaching some enter progs
    
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- driver/modern_bpf/programs/tail_called/events/syscall_dispatched_events/openat.bpf.c
+++ driver/modern_bpf/programs/tail_called/events/syscall_dispatched_events/openat.bpf.c
@@ -8,49 +8,6 @@
 
 #include <helpers/interfaces/variable_size_event.h>
 
-/*=============================== ENTER EVENT ===========================*/
-
-SEC("tp_btf/sys_enter")
-int BPF_PROG(openat_e, struct pt_regs *regs, long id) {
-	struct auxiliary_map *auxmap = auxmap__get();
-	if(!auxmap) {
-		return 0;
-	}
-
-	auxmap__preload_event_header(auxmap, PPME_SYSCALL_OPENAT_2_E);
-
-	/*=============================== COLLECT PARAMETERS  ===========================*/
-
-	/* Parameter 1: dirfd (type: PT_FD) */
-	int32_t dirfd = (int32_t)extract__syscall_argument(regs, 0);
-	if(dirfd == AT_FDCWD) {
-		dirfd = PPM_AT_FDCWD;
-	}
-	auxmap__store_s64_param(auxmap, (int64_t)dirfd);
-
-	/* Parameter 2: name (type: PT_FSRELPATH) */
-	unsigned long path_pointer = extract__syscall_argument(regs, 1);
-	auxmap__store_charbuf_param(auxmap, path_pointer, MAX_PATH, USER);
-
-	/* Parameter 3: flags (type: PT_FLAGS32) */
-	uint32_t flags = (uint32_t)extract__syscall_argument(regs, 2);
-	auxmap__store_u32_param(auxmap, open_flags_to_scap(flags));
-
-	/* Parameter 4: mode (type: PT_UINT32) */
-	unsigned long mode = extract__syscall_argument(regs, 3);
-	auxmap__store_u32_param(auxmap, open_modes_to_scap(flags, mode));
-
-	/*=============================== COLLECT PARAMETERS  ===========================*/
-
-	auxmap__finalize_event_header(auxmap);
-
-	auxmap__submit_event(auxmap);
-
-	return 0;
-}
-
-/*=============================== ENTER EVENT ===========================*/
-
 /*=============================== EXIT EVENT ===========================*/
 
 SEC("tp_btf/sys_exit")
--- userspace/libpman/include/libpman.h
+++ userspace/libpman/include/libpman.h
@@ -265,6 +265,20 @@ int pman_attach_creat_toctou_mitigation_progs(void);
 int pman_detach_creat_toctou_mitigation_progs(void);
 
 /**
+ * @brief Attach only the openat TOCTOU mitigation programs.
+ *
+ * @return `0` on success, `errno` in case of error.
+ */
+int pman_attach_openat_toctou_mitigation_progs(void);
+
+/**
+ * @brief Detach only the openat TOCTOU mitigation programs.
+ *
+ * @return `0` on success, `errno` in case of error.
+ */
+int pman_detach_openat_toctou_mitigation_progs(void);
+
+/**
  * @brief Attach only the openat2 TOCTOU mitigation programs.
  *
  * @return `0` on success, `errno` in case of error.
