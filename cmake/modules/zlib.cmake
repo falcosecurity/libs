1ef222ee90c9678b6e38f43981a33c6a14a6521e
Benjamin Leggett <benjamin@edera.io>
2025-08-18 12:07:48 -0400

    Revert "Use native cmake build for zlib to avoid quoting issues"
    This reverts commit a2b528f44567d0b4c453d3402bbacc3b32f5d02e.
    
    Signed-off-by: Benjamin Leggett <benjamin@edera.io>

--- cmake/modules/zlib.cmake
+++ cmake/modules/zlib.cmake
@@ -27,7 +27,23 @@ elseif(NOT USE_BUNDLED_ZLIB)
 	endif()
 else()
 	set(ZLIB_SRC "${PROJECT_BINARY_DIR}/zlib-prefix/src/zlib")
-	set(ZLIB_INCLUDE "${ZLIB_SRC}/include")
+	set(ZLIB_INCLUDE "${ZLIB_SRC}")
+	set(ZLIB_HEADERS "")
+	list(
+		APPEND
+		ZLIB_HEADERS
+		"${ZLIB_INCLUDE}/crc32.h"
+		"${ZLIB_INCLUDE}/deflate.h"
+		"${ZLIB_INCLUDE}/gzguts.h"
+		"${ZLIB_INCLUDE}/inffast.h"
+		"${ZLIB_INCLUDE}/inffixed.h"
+		"${ZLIB_INCLUDE}/inflate.h"
+		"${ZLIB_INCLUDE}/inftrees.h"
+		"${ZLIB_INCLUDE}/trees.h"
+		"${ZLIB_INCLUDE}/zconf.h"
+		"${ZLIB_INCLUDE}/zlib.h"
+		"${ZLIB_INCLUDE}/zutil.h"
+	)
 	if(NOT TARGET zlib)
 		# Match both release and relwithdebinfo builds
 		if(CMAKE_BUILD_TYPE MATCHES "[R,r]el*")
@@ -35,26 +51,31 @@ else()
 		else()
 			set(ZLIB_CFLAGS "-g")
 		endif()
+		if(ENABLE_PIC)
+			set(ZLIB_CFLAGS "${ZLIB_CFLAGS} -fPIC")
+		endif()
 
 		message(STATUS "Using bundled zlib in '${ZLIB_SRC}'")
 		if(NOT WIN32)
 			if(BUILD_SHARED_LIBS)
 				set(ZLIB_LIB_SUFFIX ${CMAKE_SHARED_LIBRARY_SUFFIX})
+				set(ZLIB_CONFIGURE_FLAGS)
 			else()
 				set(ZLIB_LIB_SUFFIX ${CMAKE_STATIC_LIBRARY_SUFFIX})
+				set(ZLIB_CONFIGURE_FLAGS "--static")
 			endif()
-			set(ZLIB_LIB "${ZLIB_SRC}/lib/libz${ZLIB_LIB_SUFFIX}")
+			set(ZLIB_LIB "${ZLIB_SRC}/libz${ZLIB_LIB_SUFFIX}")
 			ExternalProject_Add(
 				zlib
 				PREFIX "${PROJECT_BINARY_DIR}/zlib-prefix"
 				URL "https://github.com/madler/zlib/releases/download/v1.3.1/zlib-1.3.1.tar.gz"
 				URL_HASH "SHA256=9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df23"
-				CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-						-DCMAKE_INSTALL_PREFIX=${ZLIB_SRC}
-						-DCMAKE_C_FLAGS=${ZLIB_CFLAGS}
-						-DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}
-						-DCMAKE_POSITION_INDEPENDENT_CODE=${ENABLE_PIC}
+				CONFIGURE_COMMAND env "CFLAGS=${ZLIB_CFLAGS}" ./configure --prefix=${ZLIB_SRC}
+								  ${ZLIB_CONFIGURE_FLAGS}
+				BUILD_COMMAND make
+				BUILD_IN_SOURCE 1
 				BUILD_BYPRODUCTS ${ZLIB_LIB}
+				INSTALL_COMMAND ""
 			)
 			install(
 				FILES "${ZLIB_LIB}"
@@ -62,7 +83,7 @@ else()
 				COMPONENT "libs-deps"
 			)
 			install(
-				FILES ${ZLIB_INCLUDE}
+				FILES ${ZLIB_HEADERS}
 				DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/${LIBS_PACKAGE_NAME}/zlib"
 				COMPONENT "libs-deps"
 			)
@@ -94,7 +115,7 @@ else()
 				COMPONENT "libs-deps"
 			)
 			install(
-				DIRECTORY ${ZLIB_INCLUDE}
+				FILES ${ZLIB_HEADERS}
 				DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/${LIBS_PACKAGE_NAME}/zlib"
 				COMPONENT "libs-deps"
 			)
