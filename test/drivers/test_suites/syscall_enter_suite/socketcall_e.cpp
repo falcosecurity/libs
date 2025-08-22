09fd3b1ab0e1d047b82746ed55e3df598c80f694
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2025-08-22 11:09:46 +0200

    feat!: drop `getpeername` enter events gen, testing and parsing code
    As the `getpeername` exit events contain all the information needed,
    drop `getpeername` enter events generation from all drivers, and all
    related testing and parsing code.
    
    BREAKING CHANGE: drop `getpeername` enter events generation and parsing
    
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- test/drivers/test_suites/syscall_enter_suite/socketcall_e.cpp
+++ test/drivers/test_suites/syscall_enter_suite/socketcall_e.cpp
@@ -992,50 +992,6 @@ TEST(SyscallEnter, socketcall_recvE) {
 }
 #endif
 
-#ifdef __NR_getpeername
-TEST(SyscallEnter, socketcall_getpeernameE) {
-	auto evt_test = get_syscall_event_test(__NR_getpeername, ENTER_EVENT);
-
-	evt_test->enable_capture();
-
-	/*=============================== TRIGGER SYSCALL  ===========================*/
-
-	int32_t mock_fd = -1;
-	void *usockaddr = NULL;
-	int *usockaddr_len = NULL;
-
-	unsigned long args[3]{};
-	args[0] = mock_fd;
-	args[1] = (unsigned long)usockaddr;
-	args[2] = (unsigned long)usockaddr_len;
-	assert_syscall_state(SYSCALL_FAILURE,
-	                     "getpeername",
-	                     syscall(__NR_socketcall, SYS_GETPEERNAME, args));
-
-	/*=============================== TRIGGER SYSCALL  ===========================*/
-
-	evt_test->disable_capture();
-
-	evt_test->assert_event_presence();
-
-	if(HasFatalFailure()) {
-		return;
-	}
-
-	evt_test->parse_event();
-
-	evt_test->assert_header();
-
-	/*=============================== ASSERT PARAMETERS  ===========================*/
-
-	// Here we have no parameters to assert.
-
-	/*=============================== ASSERT PARAMETERS  ===========================*/
-
-	evt_test->assert_num_params_pushed(0);
-}
-#endif
-
 TEST(SyscallEnter, socketcall_wrong_code_socketcall_interesting) {
 	// We send a wrong code so the event will be dropped
 	auto evt_test = get_syscall_event_test(__NR_socketcall, ENTER_EVENT);
