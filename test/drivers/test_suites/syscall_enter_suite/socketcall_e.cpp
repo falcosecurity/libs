0b72c720ebcb4cd820c503db0498b7eaf6d8223a
Tero Kauppinen <tero.kauppinen@est.tech>
2025-09-05 14:52:20 +0300

    feat!: drop `sendmmsg` enter events gen, testing and parsing code
    As the `sendmmsg` exit events contain all the information needed, drop
    `sendmmsg` enter events generation from all drivers, and all related
    testing and parsing code.
    
    BREAKING CHANGE: drop `sendmmsg` enter events generation and parsing
    
    Signed-off-by: Tero Kauppinen <tero.kauppinen@est.tech>

--- test/drivers/test_suites/syscall_enter_suite/socketcall_e.cpp
+++ test/drivers/test_suites/syscall_enter_suite/socketcall_e.cpp
@@ -216,50 +216,6 @@ TEST(SyscallEnter, socketcall_recvmmsgE) {
 }
 #endif
 
-#ifdef __NR_sendmmsg
-TEST(SyscallEnter, socketcall_sendmmsgE) {
-	auto evt_test = get_syscall_event_test(__NR_sendmmsg, ENTER_EVENT);
-
-	evt_test->enable_capture();
-
-	/*=============================== TRIGGER SYSCALL  ===========================*/
-
-	int32_t mock_fd = -1;
-	struct msghdr *msg = NULL;
-	uint32_t vlen = 0;
-	int flags = 0;
-
-	unsigned long args[4]{};
-	args[0] = mock_fd;
-	args[1] = (unsigned long)msg;
-	args[2] = vlen;
-	args[3] = flags;
-	assert_syscall_state(SYSCALL_FAILURE, "sendmmsg", syscall(__NR_socketcall, SYS_SENDMMSG, args));
-
-	/*=============================== TRIGGER SYSCALL  ===========================*/
-
-	evt_test->disable_capture();
-
-	evt_test->assert_event_presence();
-
-	if(HasFatalFailure()) {
-		return;
-	}
-
-	evt_test->parse_event();
-
-	evt_test->assert_header();
-
-	/*=============================== ASSERT PARAMETERS  ===========================*/
-
-	// Here we have no parameters to assert.
-
-	/*=============================== ASSERT PARAMETERS  ===========================*/
-
-	evt_test->assert_num_params_pushed(0);
-}
-#endif
-
 #if defined(__NR_accept) || defined(__s390x__)
 
 TEST(SyscallEnter, socketcall_acceptE) {
