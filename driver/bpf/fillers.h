54f3ab3c31d4ad1c09c37c3314a82ccdd4da95bd
Tero Kauppinen <tero.kauppinen@est.tech>
2025-09-05 15:06:40 +0300

    feat!: drop `recvmsg` enter events gen, testing and parsing code
    As the `recvmsg` exit events contain all the information needed, drop
    `recvmsg` enter events generation from all drivers, and all related
    testing and parsing code.
    
    BREAKING CHANGE: drop `recvmsg` enter events generation and parsing
    
    Signed-off-by: Tero Kauppinen <tero.kauppinen@est.tech>

--- driver/bpf/fillers.h
+++ driver/bpf/fillers.h
@@ -4618,12 +4618,6 @@ FILLER(sys_listen_x, true) {
 	return bpf_push_s32_to_ring(data, (int32_t)backlog);
 }
 
-FILLER(sys_recvmsg_e, true) {
-	/* Parameter 1: fd (type: PT_FD) */
-	int32_t fd = (int32_t)bpf_syscall_get_argument(data, 0);
-	return bpf_push_s64_to_ring(data, (int64_t)fd);
-}
-
 FILLER(sys_recvmsg_x, true) {
 	/* Parameter 1: res (type: PT_ERRNO) */
 	long retval = bpf_syscall_get_retval(data->ctx);
--- test/drivers/test_suites/syscall_enter_suite/socketcall_e.cpp
+++ test/drivers/test_suites/syscall_enter_suite/socketcall_e.cpp
@@ -514,49 +514,6 @@ TEST(SyscallEnter, socketcall_sendtoE) {
 }
 #endif
 
-#ifdef __NR_recvmsg
-TEST(SyscallEnter, socketcall_recvmsgE) {
-	auto evt_test = get_syscall_event_test(__NR_recvmsg, ENTER_EVENT);
-
-	evt_test->enable_capture();
-
-	/*=============================== TRIGGER SYSCALL  ===========================*/
-
-	int32_t mock_fd = -1;
-	struct msghdr *msg = NULL;
-	int flags = 0;
-
-	unsigned long args[3]{};
-	args[0] = mock_fd;
-	args[1] = (unsigned long)msg;
-	args[2] = flags;
-	assert_syscall_state(SYSCALL_FAILURE, "recvmsg", syscall(__NR_socketcall, SYS_RECVMSG, args));
-
-	/*=============================== TRIGGER SYSCALL  ===========================*/
-
-	evt_test->disable_capture();
-
-	evt_test->assert_event_presence();
-
-	if(HasFatalFailure()) {
-		return;
-	}
-
-	evt_test->parse_event();
-
-	evt_test->assert_header();
-
-	/*=============================== ASSERT PARAMETERS  ===========================*/
-
-	/* Parameter 1: fd (type: PT_FD) */
-	evt_test->assert_numeric_param(1, (int64_t)mock_fd);
-
-	/*=============================== ASSERT PARAMETERS  ===========================*/
-
-	evt_test->assert_num_params_pushed(1);
-}
-#endif
-
 #ifdef __NR_send
 
 TEST(SyscallEnter, socketcall_sendE) {
