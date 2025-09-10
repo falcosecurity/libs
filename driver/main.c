ebb15656fdbe7014d010e424563a086332a4bfa4
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2025-09-10 11:40:11 +0200

    feat!: drop `clone3` enter evts gen, testing and parsing code
    As the `clone3` exit events contain all the information needed, drop
    `clone3` enter events generation from all drivers, and all related
    testing and parsing code.
    
    BREAKING CHANGE: drop `clone3` enter evts gen and parsing
    
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- driver/main.c
+++ driver/main.c
@@ -1461,7 +1461,6 @@ static inline void drops_buffer_syscall_categories_counters(
 		ring_info->n_drops_buffer_open_enter++;
 		break;
 	case PPME_SYSCALL_CLONE_20_E:
-	case PPME_SYSCALL_CLONE3_E:
 		ring_info->n_drops_buffer_clone_fork_enter++;
 		break;
 	case PPME_SYSCALL_EXECVE_19_E:
