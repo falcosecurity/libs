74ced937ac67331e8fcf4ba1f7a9b360dd1df8c2
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2025-10-03 10:10:46 +0200

    feat!: mark `{MESOS,TRACER,K8S}_E as old and `TRACER_X` as unused
    Instead of changing the number of parameters for `TRACER_X` to 0, keep
    the it set to 3, even if the event is now marked as unused. This is
    not the perfect solution, but prevents breaking old scap files
    readings: indeed, the savefile engine doesn't expect to read events,
    encoded using a v1 event block, with less (inferred) parameters than
    the ones declared in the event table.
    In the future, we can adapt the engine code and remove any parameter
    from `TRACER_X`. At the moment, the scap converter takes care of
    droppping it regardless of its parameters number, since it is marked
    as unused.
    
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- driver/event_stats.h
+++ driver/event_stats.h
@@ -12,6 +12,6 @@ or GPL2.txt for full copies of the license.
 /* These numbers must be updated when we add new events in the event table */
 #define SYSCALL_EVENTS_NUM 382
 #define TRACEPOINT_EVENTS_NUM 6
-#define METAEVENTS_NUM 20
+#define METAEVENTS_NUM 19
 #define PLUGIN_EVENTS_NUM 1
-#define UNKNOWN_EVENTS_NUM 21
+#define UNKNOWN_EVENTS_NUM 22
