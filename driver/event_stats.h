d31f45162e75d9b835e97b7094b42e8159ea0c8b
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2025-10-03 15:34:13 +0200

    chore!: mark `PPME_SCAPEVENT_X` with `EC_UNKNOWN` and set name to `NA`
    BREAKING CHANGE: change `PPME_SCAPEVENT_X` flags and name
    
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- driver/event_stats.h
+++ driver/event_stats.h
@@ -12,6 +12,6 @@ or GPL2.txt for full copies of the license.
 /* These numbers must be updated when we add new events in the event table */
 #define SYSCALL_EVENTS_NUM 382
 #define TRACEPOINT_EVENTS_NUM 6
-#define METAEVENTS_NUM 19
+#define METAEVENTS_NUM 18
 #define PLUGIN_EVENTS_NUM 1
-#define UNKNOWN_EVENTS_NUM 22
+#define UNKNOWN_EVENTS_NUM 23
