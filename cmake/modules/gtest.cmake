921c0913d52bbd94549ccbe62162e289f896419f
irozzo-1A <iacopo@sysdig.com>
2025-12-06 22:26:19 +0100

    chore(libsinsp): add target to build library for sinsp integration tests
    Signed-off-by: irozzo-1A <iacopo@sysdig.com>

--- cmake/modules/gtest.cmake
+++ cmake/modules/gtest.cmake
@@ -18,64 +18,18 @@ option(USE_BUNDLED_GTEST "Enable building of the bundled gtest" ${USE_BUNDLED_DE
 if(GTEST_INCLUDE_DIR)
 	# we already have gtest
 elseif(NOT USE_BUNDLED_GTEST)
-	find_path(
-		GTEST_INCLUDE_DIR
-		PATH_SUFFIXES gtest
-		NAMES gtest.h
-	)
-	find_library(GTEST_LIB NAMES gtest)
-	find_library(GTEST_MAIN_LIB NAMES gtest_main)
-	if(GTEST_INCLUDE_DIR
-	   AND GTEST_LIB
-	   AND GTEST_MAIN_LIB
-	)
-		message(
-			STATUS
-				"Found gtest: include: ${GTEST_INCLUDE_DIR}, lib: ${GTEST_LIB}, main lib: ${GTEST_MAIN_LIB}"
-		)
-	else()
-		message(FATAL_ERROR "Couldn't find system gtest")
-	endif()
+	find_package(GTest REQUIRED)
 else()
-	# https://github.com/google/googletest/tree/main/googletest#incorporating-into-an-existing-cmake-project
-	# Download and unpack googletest at configure time
-	configure_file(
-		CMakeListsGtestInclude.cmake ${PROJECT_BINARY_DIR}/googletest-download/CMakeLists.txt
+	include(FetchContent)
+	FetchContent_Declare(
+		gtest
+		URL https://github.com/google/googletest/archive/refs/tags/v1.16.0.tar.gz
+		URL_HASH SHA256=78c676fc63881529bf97bf9d45948d905a66833fbfa5318ea2cd7478cb98f399
+		OVERRIDE_FIND_PACKAGE
 	)
-	execute_process(
-		COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
-		RESULT_VARIABLE result
-		WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/googletest-download
-	)
-	if(result)
-		message(FATAL_ERROR "CMake step for googletest failed: ${result}")
-	endif()
-	execute_process(
-		COMMAND ${CMAKE_COMMAND} --build .
-		RESULT_VARIABLE result
-		WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/googletest-download
-	)
-	if(result)
-		message(FATAL_ERROR "Build step for googletest failed: ${result}")
-	endif()
-
-	# Add googletest directly to our build. This defines the gtest and gtest_main targets.
-	add_subdirectory(
-		${PROJECT_BINARY_DIR}/googletest-src ${PROJECT_BINARY_DIR}/googletest-build
-		EXCLUDE_FROM_ALL
-	)
-
-	set(GTEST_INCLUDE_DIR "${gtest_SOURCE_DIR}/include")
-	set(GTEST_MAIN_LIB "gtest_main")
-	install(
-		DIRECTORY "${GTEST_INCLUDE_DIR}"
-		DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/${LIBS_PACKAGE_NAME}"
-		COMPONENT "libs-deps"
-	)
+	FetchContent_MakeAvailable(gtest)
 endif()
 
 if(NOT TARGET gtest)
 	add_custom_target(gtest)
 endif()
-
-include_directories("${GTEST_INCLUDE_DIR}")
