102f750cb4869fc1137e58389ee6cf1dfc95fe47
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2026-02-13 16:52:31 +0100

    test(e2e): be resilient to container dict layout changes
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- test/e2e/tests/commons/sinspqa/docker.py
+++ test/e2e/tests/commons/sinspqa/docker.py
@@ -14,9 +14,19 @@ def get_network_data(container: Container) -> str:
     """
     container.reload()
 
-    ip = container.attrs['NetworkSettings']['IPAddress']
+    settings = container.attrs.get('NetworkSettings') or {}
+
+    ip = settings.get('IPAddress')
+    if not ip:
+        networks = settings.get('Networks', {})
+        for network_name, network_config in networks.items():
+            if network_config.get('IPAddress'):
+                ip = network_config['IPAddress']
+                break
+
+
     # Try and get a single port number
-    ports = container.attrs['NetworkSettings']['Ports']
+    ports = settings.get('Ports') or {}
     ports = list(ports.keys())
 
     port = ports[0].split('/')[0] if len(ports) else None
