956b15bc1bd8b7fbe2363113e4d706964c0d8740
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2026-03-10 17:24:25 +0100

    feat: add support for 32-bits encoded file descriptors
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- driver/ppm_param_helpers.h
+++ driver/ppm_param_helpers.h
@@ -46,6 +46,7 @@ inline int ppm_param_min_len_from_type(const ppm_param_type t, uint32_t *min_len
 	case PT_GID:
 	case PT_MODE:
 	case PT_SIGSET:
+	case PT_FD32:
 		*min_len = 4;
 		return 0;
 
@@ -115,6 +116,7 @@ inline int ppm_param_max_len_from_type(const ppm_param_type t,
 	case PT_GID:
 	case PT_MODE:
 	case PT_SIGSET:
+	case PT_FD32:
 		*max_len = 4;
 		return 0;
 
