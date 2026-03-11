e47990968695674a55693e5b70ff5d4b95b6e271
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2026-03-11 13:06:48 +0100

    chore(userspace/libpman): add BPF iterators debugging support
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- userspace/libpman/src/iterators.c
+++ userspace/libpman/src/iterators.c
@@ -20,6 +20,37 @@ limitations under the License.
 #include <libscap/scap.h>
 #include <libscap/strl.h>
 
+#ifdef BPF_ITERATOR_DEBUG
+
+#if defined(BPF_ITERATOR_DEBUG_RAW) || defined(BPF_ITERATOR_DEBUG_PARSED)
+
+#include <libscap/scap_print.h>
+
+#ifdef BPF_ITERATOR_DEBUG_RAW
+#define DEBUG_PRINT_EVENT(evt_ptr) scap_print_event(evt_ptr, PRINT_FULL)
+#endif  // BPF_ITERATOR_DEBUG_RAW
+
+#ifdef BPF_ITERATOR_DEBUG_PARSED
+#define DEBUG_PRINT_THREADINFO(tinfo_ptr) scap_print_threadinfo(tinfo_ptr)
+#define DEBUG_PRINT_FDINFO(fdinfo_ptr) scap_print_fdinfo(fdinfo_ptr)
+#endif  // BPF_ITERATOR_DEBUG_PARSED
+
+#endif  // defined(BPF_ITERATOR_DEBUG_RAW) || defined(BPF_ITERATOR_DEBUG_PARSED)
+
+#endif  // BPF_ITERATOR_DEBUG
+
+#ifndef DEBUG_PRINT_EVENT
+#define DEBUG_PRINT_EVENT(evt_ptr)
+#endif
+
+#ifndef DEBUG_PRINT_THREADINFO
+#define DEBUG_PRINT_THREADINFO(tinfo_ptr)
+#endif
+
+#ifndef DEBUG_PRINT_FDINFO
+#define DEBUG_PRINT_FDINFO(fdinfo_ptr)
+#endif
+
 int32_t pman_iter_fetch_task(const struct scap_fetch_callbacks *callbacks,
                              const uint32_t tid,
                              scap_threadinfo **tinfo,
