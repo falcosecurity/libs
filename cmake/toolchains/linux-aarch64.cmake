a4b6aa8eac904fb4ffb31f128a69592af382a858
MatthewClarkMay <mcmay.web@gmail.com>
2026-03-12 11:17:16 -0500

    ci: validate bundled arm64 cross-builds
    Signed-off-by: MatthewClarkMay <mcmay.web@gmail.com>

---
+++ cmake/toolchains/linux-aarch64.cmake
@@ -0,0 +1,16 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+
+set(CMAKE_C_COMPILER /usr/bin/aarch64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/aarch64-linux-gnu-g++)
+set(CMAKE_ASM_COMPILER /usr/bin/aarch64-linux-gnu-gcc)
+set(CMAKE_AR /usr/bin/aarch64-linux-gnu-ar)
+set(CMAKE_RANLIB /usr/bin/aarch64-linux-gnu-ranlib)
+set(CMAKE_STRIP /usr/bin/aarch64-linux-gnu-strip)
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+set(CMAKE_FIND_ROOT_PATH /usr/aarch64-linux-gnu)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
