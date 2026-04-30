irozzo-1A/agent-libs

feat(driver): enable support for requiring fixed number of buffers
Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

userspace/libpman/src/configuration.c
@@ -94,8 +94,13 @@ void pman_clear_state() {
 
 int pman_init_state(falcosecurity_log_fn log_fn,
                     unsigned long buf_bytes_dim,
-                    uint16_t cpus_for_each_buffer,
+                    double buffers_num,
                     bool allocate_online_only) {
+	if(buffers_num < 0) {
+		pman_print_error("buffers_num cannot be negative");
+		return -1;
+	}
+
 	char error_message[MAX_ERROR_MESSAGE_LEN];
 
 	/* `LIBBPF_STRICT_ALL` turns on all supported strict features
@@ -132,6 +137,35 @@ int pman_init_state(falcosecurity_log_fn log_fn,
 		pman_print_error("no available cpus");
 		return -1;
 	}
+
+	/* Set the dimension of a single ring buffer */
+	g_state.buffer_bytes_dim = buf_bytes_dim;
+
+	/* These will be used during the ring buffer consumption phase. */
+	g_state.last_ring_read = -1;
+	g_state.last_event_size = 0;
+
+	if(buffers_num > 1) {
+		if(buffers_num != (double)(uint32_t)buffers_num) {
+			pman_print_error("buffers_num must be an integer value");
+			return -1;
+		}
+		g_state.n_required_buffers = (uint32_t)buffers_num;
+		/* The following disables cpus-to-ring-buffers mapping */
+		g_state.cpus_for_each_buffer = 0;
+		return 0;
+	}
+
+	uint16_t cpus_for_each_buffer = 0;
+	if(buffers_num != 0) {
+		double ratio = (double)1 / buffers_num;
+		if(ratio != (double)(uint16_t)ratio) {
+			pman_print_error("1 / buffers_num must be an integer value");
+			return -1;
+		}
+		cpus_for_each_buffer = (uint16_t)ratio;
+	}
+	printf("cpus_for_each_buffer %d\n", cpus_for_each_buffer);
 
 	g_state.allocate_online_only = allocate_online_only;
 
@@ -179,12 +213,7 @@ int pman_init_state(falcosecurity_log_fn log_fn,
 	if((g_state.n_interesting_cpus % g_state.cpus_for_each_buffer) != 0) {
 		g_state.n_required_buffers++;
 	}
-	/* Set the dimension of a single ring buffer */
-	g_state.buffer_bytes_dim = buf_bytes_dim;
 
-	/* These will be used during the ring buffer consumption phase. */
-	g_state.last_ring_read = -1;
-	g_state.last_event_size = 0;
 	return 0;
 }
 

userspace/libpman/src/ringbuffer.c
@@ -55,11 +55,9 @@ static int ringbuf_array_set_inner_map() {
 }
 
 static int ringbuf_array_set_max_entries() {
-	/* We always allocate a number of entries equal to the available CPUs.
-	 * This doesn't mean that we allocate a ring buffer for every available CPU,
-	 * it means only that every CPU will have an associated entry.
-	 */
-	if(bpf_map__set_max_entries(g_state.skel->maps.ringbuf_maps, g_state.n_possible_cpus)) {
+	int16_t buffers_num = pman_is_cpus_to_ringbufs_mapping_disabled() ? g_state.n_required_buffers
+	                                                                  : g_state.n_possible_cpus;
+	if(bpf_map__set_max_entries(g_state.skel->maps.ringbuf_maps, buffers_num)) {
 		pman_print_error("unable to set max entries for the ringbuf_array");
 		return errno;
 	}
@@ -128,6 +126,7 @@ int pman_finalize_ringbuf_array_after_loading() {
 		return errno;
 	}
 	bool success = false;
+	const bool is_cpus_to_ringbufs_mapping_disabled = pman_is_cpus_to_ringbufs_mapping_disabled();
 
 	/* We don't need anymore the inner map, close it. */
 	close(g_state.inner_ringbuf_map_fd);
@@ -140,7 +139,7 @@ int pman_finalize_ringbuf_array_after_loading() {
 		if(ringbufs_fds[i] <= 0) {
 			snprintf(error_message,
 			         MAX_ERROR_MESSAGE_LEN,
-			         "failed to create the ringbuf map for CPU '%d'. (If you get memory allocation "
+			         "failed to create the ringbuf [%d] map. (If you get memory allocation "
 			         "errors try to reduce the buffer dimension)",
 			         i);
 			pman_print_error((const char *)error_message);
@@ -163,7 +162,7 @@ int pman_finalize_ringbuf_array_after_loading() {
 		if(ring_buffer__add(g_state.rb_manager, ringbufs_fds[i], NULL, NULL)) {
 			snprintf(error_message,
 			         MAX_ERROR_MESSAGE_LEN,
-			         "failed to add the ringbuf map for CPU %d into the manager",
+			         "failed to add the ringbuf [%d] map into the manager",
 			         i);
 			pman_print_error((const char *)error_message);
 			goto clean_percpu_ring_buffers;
@@ -175,6 +174,21 @@ int pman_finalize_ringbuf_array_after_loading() {
 	if(ringubuf_array_fd <= 0) {
 		pman_print_error("failed to get the ringubuf_array");
 		return errno;
+	}
+
+	if(is_cpus_to_ringbufs_mapping_disabled) {
+		for(int i = 0; i < g_state.n_required_buffers; i++) {
+			if(bpf_map_update_elem(ringubuf_array_fd, &i, &ringbufs_fds[i], BPF_ANY)) {
+				snprintf(error_message,
+				         MAX_ERROR_MESSAGE_LEN,
+				         "failed to add ringbuf [%d] to ringbuf map",
+				         i);
+				pman_print_error((const char *)error_message);
+				goto clean_percpu_ring_buffers;
+			}
+		}
+		success = true;
+		goto clean_percpu_ring_buffers;
 	}
 
 	/* We need to associate every CPU to the right ring buffer */
@@ -203,7 +217,7 @@ int pman_finalize_ringbuf_array_after_loading() {
 		if(bpf_map_update_elem(ringubuf_array_fd, &i, &ringbufs_fds[ringbuf_id], BPF_ANY)) {
 			snprintf(error_message,
 			         MAX_ERROR_MESSAGE_LEN,
-			         "failed to add the ringbuf map for CPU '%d' to ringbuf '%d'",
+			         "failed to add the ringbuf map for CPU [%d] to ringbuf [%d]",
 			         i,
 			         ringbuf_id);
 			pman_print_error((const char *)error_message);

falcosecurity/libs

userspace/libpman/src/configuration.c
@@ -102,8 +102,13 @@ void pman_clear_state() {
 
 int pman_init_state(falcosecurity_log_fn log_fn,
                     unsigned long buf_bytes_dim,
-                    uint16_t cpus_for_each_buffer,
+                    double buffers_num,
                     bool allocate_online_only) {
+	if(buffers_num < 0) {
+		pman_print_errorf("buffers_num cannot be negative");
+		return -1;
+	}
+
 	/* `LIBBPF_STRICT_ALL` turns on all supported strict features
 	 * of libbpf to simulate libbpf v1.0 behavior.
 	 * `libbpf_set_strict_mode` returns always 0.
@@ -137,6 +142,34 @@ int pman_init_state(falcosecurity_log_fn log_fn,
 	if(g_state.n_possible_cpus <= 0) {
 		pman_print_errorf("no available cpus");
 		return -1;
+	}
+
+	/* Set the dimension of a single ring buffer */
+	g_state.buffer_bytes_dim = buf_bytes_dim;
+
+	/* These will be used during the ring buffer consumption phase. */
+	g_state.last_ring_read = -1;
+	g_state.last_event_size = 0;
+
+	if(buffers_num > 1) {
+		if(buffers_num != (double)(uint32_t)buffers_num) {
+			pman_print_errorf("buffers_num must be an integer value");
+			return -1;
+		}
+		g_state.n_required_buffers = (uint32_t)buffers_num;
+		/* The following disables cpus-to-ring-buffers mapping */
+		g_state.cpus_for_each_buffer = 0;
+		return 0;
+	}
+
+	uint16_t cpus_for_each_buffer = 0;
+	if(buffers_num != 0) {
+		double ratio = (double)1 / buffers_num;
+		if(ratio != (double)(uint16_t)ratio) {
+			pman_print_errorf("1 / buffers_num must be an integer value");
+			return -1;
+		}
+		cpus_for_each_buffer = (uint16_t)ratio;
 	}
 
 	g_state.allocate_online_only = allocate_online_only;
@@ -182,12 +215,7 @@ int pman_init_state(falcosecurity_log_fn log_fn,
 	if((g_state.n_interesting_cpus % g_state.cpus_for_each_buffer) != 0) {
 		g_state.n_required_buffers++;
 	}
-	/* Set the dimension of a single ring buffer */
-	g_state.buffer_bytes_dim = buf_bytes_dim;
 
-	/* These will be used during the ring buffer consumption phase. */
-	g_state.last_ring_read = -1;
-	g_state.last_event_size = 0;
 	return 0;
 }
 

userspace/libpman/src/ringbuffer.c
@@ -86,7 +86,10 @@ int pman_prepare_ringbuf_array_before_loading() {
 	 * allocate a ring buffer for every available CPU, it means only that every CPU will have an
 	 * associated entry.
 	 */
-	err = err ?: ringbuf_array_set_max_entries(g_state.skel, g_state.n_possible_cpus);
+	const int16_t buffers_num = pman_is_cpus_to_ringbufs_mapping_disabled()
+	                                    ? g_state.n_required_buffers
+	                                    : g_state.n_possible_cpus;
+	err = err ?: ringbuf_array_set_max_entries(g_state.skel, buffers_num);
 	/* Allocate consumer positions and producer positions for the ringbuffer. */
 	err = err ?: allocate_consumer_producer_positions();
 	return err;
@@ -139,6 +142,7 @@ static bool is_cpu_online(uint16_t cpu_id) {
 int pman_finalize_ringbuf_array_after_loading() {
 	int last_errno = EINVAL;
 	bool success = false;
+	const bool is_cpus_to_ringbufs_mapping_disabled = pman_is_cpus_to_ringbufs_mapping_disabled();
 
 	int ringbuf_array_fd;
 
@@ -163,7 +167,7 @@ int pman_finalize_ringbuf_array_after_loading() {
 		if(ringbufs_fds[i] < 0) {
 			last_errno = errno;
 			pman_print_errorf(
-			        "failed to create the ringbuf map for CPU '%d'. (If you get memory allocation "
+			        "failed to create the ringbuf [%d] map. (If you get memory allocation "
 			        "errors try to reduce the buffer dimension)",
 			        i);
 			goto clean_percpu_ring_buffers;
@@ -185,7 +189,7 @@ int pman_finalize_ringbuf_array_after_loading() {
 	for(int i = 1; i < g_state.n_required_buffers; i++) {
 		if(ring_buffer__add(g_state.rb_manager, ringbufs_fds[i], NULL, NULL)) {
 			last_errno = errno;
-			pman_print_errorf("failed to add the ringbuf map for CPU %d into the manager", i);
+			pman_print_errorf("failed to add the ringbuf [%d] map into the manager", i);
 			goto clean_percpu_ring_buffers;
 		}
 	}
@@ -195,6 +199,18 @@ int pman_finalize_ringbuf_array_after_loading() {
 	if(ringbuf_array_fd < 0) {
 		last_errno = errno;
 		pman_print_errorf("failed to get the ringbuf_array");
+		goto clean_percpu_ring_buffers;
+	}
+
+	if(is_cpus_to_ringbufs_mapping_disabled) {
+		for(int i = 0; i < g_state.n_required_buffers; i++) {
+			if(bpf_map_update_elem(ringbuf_array_fd, &i, &ringbufs_fds[i], BPF_ANY)) {
+				last_errno = errno;
+				pman_print_errorf("failed to add ringbuf [%d] to ringbuf map", i);
+				goto clean_percpu_ring_buffers;
+			}
+		}
+		success = true;
 		goto clean_percpu_ring_buffers;
 	}
 
@@ -221,7 +237,7 @@ int pman_finalize_ringbuf_array_after_loading() {
 
 		if(bpf_map_update_elem(ringbuf_array_fd, &i, &ringbufs_fds[ringbuf_id], BPF_ANY)) {
 			last_errno = errno;
-			pman_print_errorf("failed to add the ringbuf map for CPU '%d' to ringbuf '%d'",
+			pman_print_errorf("failed to add the ringbuf map for CPU [%d] to ringbuf [%d]",
 			                  i,
 			                  ringbuf_id);
 			goto clean_percpu_ring_buffers;